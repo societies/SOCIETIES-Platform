75e93d66f2289f248da0e0830385a559d3e7c119
root <root@localhost>
2011-10-20 14:24:04 +0100

    adding initial CSS discovery API

---
+++ css-modules/css-ser-discovery/CSSDiscoveryMgmt/src/main/java/eu/societies/CSSDiscovery/api/ICSSDiscovery.java
@@ -0,0 +1,28 @@
+package eu.societies.CSSDiscovery.api;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface ICSSDiscovery {
+	
+	public boolean setCSSDiscoveryDomain(Object[] CSSDomain);
+	
+	public Collection<Object> searchAllCSS();
+	
+	public Collection<Object> searchAllCSS(Object[] cisGroup);
+	
+	public boolean registerForJoinCSSAlert(Object handler, Object[] cisGroupFilter);
+	
+	public boolean registerForLeftCSSAlert(Object handler, Object[] cisGroupFilter);
+	
+	public boolean unregisterJoinCSSAlert(Object handler, Object[] cisGroupFilter);
+	
+	public boolean unregisterLeftCSSAlert(Object handler, Object[] cisGroupFilter);
+	
+	public Object findCSS(Object CSSID);
+	
+	public boolean isCSSExists();
+	
+	public List<Object> getCISMembership(Object CSSID);  
+
+}
