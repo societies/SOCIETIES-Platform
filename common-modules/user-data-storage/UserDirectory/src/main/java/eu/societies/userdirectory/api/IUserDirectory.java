9141926dfa90619e5963a466cc978bb5b0087897
root <root@localhost>
2011-10-20 13:14:47 +0100

    adding initial api for userdirectory

---
+++ common-modules/user-data-storage/UserDirectory/src/main/java/eu/societies/userdirectory/api/IUserDirectory.java
@@ -0,0 +1,19 @@
+package eu.societies.userdirectory.api;
+
+import java.util.Collection;
+
+public interface IUserDirectory {
+	
+	public Object createNewUserContact();
+	
+	public boolean insertUser(IUserProfile contact);
+	
+	public boolean updateUser(IUserProfile contact);
+	
+	public Collection<Object> queryUser(Object Parameter);
+	
+	public boolean isUserExists(Object parameter);
+	
+	public Collection<Object> retrieveAllUser();
+
+}
---
+++ common-modules/user-data-storage/UserDirectory/src/main/java/eu/societies/userdirectory/api/IUserProfile.java
@@ -0,0 +1,45 @@
+/**
+ * This is an initial interface for persisting user information 
+ */
+package eu.societies.userdirectory.api;
+/**
+ * 
+ * 
+ */
+
+public interface IUserProfile {		
+	/**
+	 * 
+	 * @param jid unique XMPP communication id of an user
+	 */	
+	public void setJabberID(Object jid);	
+	/**
+	 * @param cssid
+	 */
+	public void addCSSID(Object cssid);	
+	/**User
+	 * @param memberCIS
+	 */
+	public void addCISMembershipGroup(Object memberCIS);
+	/**
+	 * @param fullname
+	 */
+	public void setName(Object fullname);
+	/**
+	 * @param note
+	 */
+	public void setContent(Object note);
+	/**
+	 * @param email
+	 */
+	public void addEmailAddress(Object email);
+	/**
+	 * @param phone
+	 */
+	public void addPhoneNumber(Object phone);
+	/**
+	 * @param address
+	 */
+	public void addStructuredPostalAddress(Object address);	
+	
+}
