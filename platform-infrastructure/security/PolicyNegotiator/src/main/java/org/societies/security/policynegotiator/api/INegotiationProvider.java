138efd5b236269d144e631762e4200fffb9c4874
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-10-25 16:57:21 +0200

    Initial API for generic secure policy negotiator.

---
+++ platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/api/INegotiationProvider.java
@@ -0,0 +1,27 @@
+package org.societies.security.policynegotiator.api;
+
+public interface INegotiationProvider {
+
+	/**
+	 * Get final SLA.
+	 * 
+	 * @param policySignedByRequestor The option accepted by the requestor side.
+	 * Includes requestor identity and signature.
+	 * 
+	 * @return Final SLA, signed by both parties.
+	 */
+	public String getFinalPolicy(String policySignedByRequestor);
+	
+	/**
+	 * Get all available options for SLA.
+	 * 
+	 * @return All available options embedded in a single XML document
+	 */
+	public String getPolicyOptions();
+	
+	/**
+	 * Reject all options and terminate negotiation.
+	 */
+	public void reject();
+
+}
---
+++ platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/api/INegotiationRequester.java
@@ -0,0 +1,26 @@
+package org.societies.security.policynegotiator.api;
+
+public interface INegotiationRequuster {
+
+	/**
+	 * Accept given policy option.
+	 * 
+	 * @param policyOptionId ID of the option accepted by the requestor side.
+	 */
+	public void acceptPolicy(int policyOptionId);
+	
+	/**
+	 * Get all available options for SLA.
+	 * 
+	 * @param callback The callback to be invoked to return the result.
+	 * 
+	 * @return All available options embedded in a single XML document
+	 */
+	public void getPolicyOptions(Object callback);
+	
+	/**
+	 * Reject all options and terminate negotiation.
+	 */
+	public void reject();
+
+}
