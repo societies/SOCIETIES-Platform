8e959f84aa264c16c60ca7919d8eb0bc87bbe737
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-11-10 14:48:53 +0100

    Initial empty APIs for QoS Monitor

---
+++ platform-infrastructure/slm/QoSMonitor/src/main/java/org/societies/slm/qosmonitor/api/IQoSReporter.java
@@ -0,0 +1,12 @@
+package org.societies.slm.qosmonitor.api;
+
+/**
+ * Interface for invoking the Quality of Service (QoS) Reporter.
+ * To be used by QoS Monitor.
+ * 
+ * @author Mitja Vardjan
+ *
+ */
+public interface IQoSReporter {
+	
+}
