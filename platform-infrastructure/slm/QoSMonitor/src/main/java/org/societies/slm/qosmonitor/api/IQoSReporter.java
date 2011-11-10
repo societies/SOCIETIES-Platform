45597d469e26537ffff004df02750db0842f5f13
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-11-10 15:54:14 +0100

    IQoSReporter split to interfaces that will be used (invoked) by 3rd party monitor and local monitor.

--- platform-infrastructure/slm/QoSMonitor/src/main/java/org/societies/slm/qosmonitor/api/IQoSReporter.java
+++ platform-infrastructure/slm/QoSMonitor/src/main/java/org/societies/slm/qosmonitor/api/IQoSReporter.java
@@ -2,25 +2,12 @@ package org.societies.slm.qosmonitor.api;
 
 /**
  * Interface for invoking the Quality of Service (QoS) Reporter.
- * To be used by QoS Monitor.
+ * To be used by 3rd party QoS Monitor.
  * 
  * @author Mitja Vardjan
  *
  */
 public interface IQoSReporter {
-	
-	/**
-	 * Notifies the 3rd party monitor that QoS is below promised level and
-	 * requests evaluation of QoS by the 3rd party monitor.
-	 * The 3rd party monitor is specified in Service License Agreement (SLA).
-	 * 
-	 * @param sla XML-formatted Service License Agreement
-	 * 
-	 * @param violations References to the QoS parameters to be investigated.
-	 * Given as array of XPath expressions that point to locations in SLA where
-	 * the QoS parameters are defined.
-	 */
-	public void notifyLowQoS(String sla, String[] violations);
 	
 	/**
 	 * Get additional community data required by 3rd party monitor in order to
