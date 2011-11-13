1c49e4a59dcef90d6bbc6f5eca23c316f1a56610
pkosmides <pkosmidi@central.ntua.gr>
2011-11-13 21:10:13 +0200

    1st Try - Added src/test/java packet and ICommunityCtxBroker (in test)
    Change-Id: I10a2ada52ea27a12c8d8e116673ab8783b808a8e

---
+++ context-management/CM_API/src/test/java/org/societies/context/broker/ICommunityCtxBroker.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package org.societies.context.broker;
+
+/**
+ * @author Paul
+ *
+ */
+public interface ICommunityCtxBroker {
+
+	/**
+	 * 
+	 * @param community
+	 * @param callback
+	 */
+	public void retrieveAdministratingCSS(ContextEntityIdentifier community, ICommunityCtxBrokerCallback callback);
+
+	/**
+	 * Retrieves the context attribute(s) that acts as a bond of the community of
+	 * entities specified by the CtxEntityIdentifier.
+	 * 
+	 * @param community
+	 * @param callback
+	 */
+	public void retrieveBonds(ContextEntityIdentifier community, ICommunityCtxBrokerCallback callback);
+
+	/**
+	 * This applies for Community hierarchies. Retrieves the child communities
+	 * (subcommunities of CtxEntities) of the specified parent CtxEntity
+	 * 
+	 * @param community
+	 * @param callback
+	 */
+	public void retrieveChildCommunities(ContextEntityIdentifier community, ICommunityCtxBrokerCallback callback);
+
+	/**
+	 * Retrievies a list of Individual Context Entities that are members of the
+	 * specified community Entity.
+	 * 
+	 * @param community
+	 * @param callback
+	 */
+	public void retrieveCommunityMembers(ContextEntityIdentifier community, ICommunityCtxBrokerCallback callback);
+
+	/**
+	 * 
+	 * @param community
+	 * @param callback
+	 */
+	public void retrieveParentCommunities(ContextEntityIdentifier community, ICommunityCtxBrokerCallback callback);
+
+}
