2d9ae4f813b4de5303ef869d24fe5e7753fd7ccc
FRBlackmun <frb4@hw.ac.uk>
2011-11-14 13:51:45 +0000

    Rewrote CIS rule - I'd been doing it wrong it seems!

--- intelligent-community-orchestration/API/src/main/java/org/societies/comorch/api/ICISRule.java
+++ intelligent-community-orchestration/API/src/main/java/org/societies/comorch/api/ICISRule.java
@@ -1,9 +1,8 @@
 
 /**
- * This class represents a CIS subscription record.
- * (don't you mean CIS rule? - Fraser)
+ * This class represents a CIS rule, which applies for a specific, existing CIS
  * 
- * @author Babak Farshchian
+ * @author Fraser Blackmun
  * @version 0
  * 
  */
@@ -12,86 +11,76 @@ package org.societies.comorch.api;
 
 public interface ICISRule {
     
-	// The current setting for notifying users on deleted CISs.
-    private boolean notificationSetting;
-    // The statuses of all CIS members.
-    private char[] cssStatus;
-    // The time interval that passes before the trigger to perform actions is invoked.
-    private double intervalTrigger;
-    // The possibility of each CIS member to alter the permission settings of the CIS.
-    private boolean[] permissionSettings;
-    // The CIS that the CISRule applies for.
-    private CIS cis;
-    // The current permissions for the CIS.
-    private Permissions permission;
+	//States the type of the rule, which is one of the following three:
+	//  Lifecycle:  Relates to the creation, configuration,
+	//              and deletion of the CIS that the rule applies for
+	//  Membership: Relates to criteria for CIS membership and user 
+	//              recommendation for joining
+	//  Merge:      Specifies if and/or when a CIS should be merged with other CISs.
+	private String ruleType;
+	
+	//The rule embodied by the CISRule object that this interface represents.
+	//The 'keys' in the HashMap may be the following (still in progress):
+	//  "CIS deletion criteria" - value = array of conditions for a CIS to be deleted,
+	//                                    where each element of the array is a sufficient
+	//                                    condition for deletion.
+	//  "CIS configuration criteria" - value = array of conditions for a CIS to be configured,
+	//                                         and details on the resultant configuration for 
+	//                                         each array element
+	//  "CIS merging criteria" - value = array of conditions for a CIS to be merged with another
+	//  "CIS membership criteria" - value = the membership criteria for a CIS
+	//  
+	private HashMap ruleDetails;
 
     /*
-     * Constructur for ICISRule.
-     */
+     * Constructor for ICISRule.
+     * 
+	 * Description: The constructor creates a new CISRule with the details
+	 *              passed to it.
+	 * Parameters: 
+	 * 				1) ruleType - The kind of rule being created, which
+	 *                            is one of: Lifecycle, Membership, Merge.
+	 * 				2) ruleToCreate - The details on the rule that are needed
+	 *                                in order to create it. 
+	 */
+	
+	public ICISRule(String ruleType, HashMap ruleToCreate);
+	
+	/*
+	 * Description: The modifyRule method alters the rule that the CISRule 
+	 *              object represents, using the details in the input HashMap.
+	 * Parameters: 
+	 * 				1) changesToRule - The changes that are to be made to the CIS rule.
+	 * Returns:
+	 * 				* True if the method was able to modify the CIS rule.
+	 *				* False if the method was unable to modify the CIS rule.
+	 */
+	
+    public boolean modifyRule(HashMap changesToRule);
+	
+    /*
+	 * Description: The modifyRule method alters the rule that the CISRule 
+	 *              object represents, using the information of the input
+	 *              CISRule.
+	 * Parameters: 
+	 * 				1) CISRule - The new rule that is replacing the existing one
+	 * Returns:
+	 * 				* True if the method was able to modify the CIS rule.
+	 *				* False if the method was unable to modify the CIS rule.
+	 */
     
-	public ICISRule();
-
-	/*
-	 * Description: The setDeletedCISsNotification setting changes the current
-	 *              setting for whether to notify  CIS members when relevant CISs are deleted(?),
-	 *              based on the input.
-	 * Parameters: 
-	 * 				1) boolean - the new notification setting.
-	 * Returns:
-	 * 				* True if the method was able to set the deleted CISs notification setting.
-	 *				* False if the method was unable to set the deleted CISs notification setting.
-	 */
-	
-	public boolean setDeletedCISsNotification(boolean notificationSetting);
+	public boolean modifyRule(CISRule replacingRule);
 	
 	/*
-	 * Description: The setCSSStatus method changes the current status of a particular CSS
-	 *              that is a member of the relevant CIS, based on the input.
+	 * Description: The deleteRule method causes the CISRule object
+	 *              that the method is invokved on to delete itself.
 	 * Parameters: 
-	 * 				1) char - the new status of a CSS.
+	 * 				none
 	 * Returns:
-	 * 				* True if the method was able to set the CSS status.
-	 *				* False if the method was unable to set the CSS status.
+	 * 				* True if the method was able to delete the CIS rule.
+	 *				* False if the method was unable to delete the CIS rule.
 	 */
 	
-	public boolean setCSSStatus(char cssStatus);
-	
-	/*
-	 * Description: The setIntervalTrigger method changes the current time interval
-	 *              that must pass before a trigger is activated(?)
-	 * Parameters: 
-	 * 				1) boolean - the new interval trigger.
-	 * Returns:
-	 * 				* True if the method was able to set the interval trigger.
-	 *				* False if the component was unable to set the interval trigger.
-	 */
-	
-	public boolean setIntervalTrigger(double intervalTrigger);
-	
-	/*
-	 * Description: The setPermissions method changes the current permission
-	 *              settings for the CIS, based on the input.
-	 * Parameters: 
-	 * 				1) double - the new permission settings.
-	 * Returns:
-	 * 				* True if the method was able to set the permissions.
-	 *				* False if the method was unable to set the permissions.
-	 */
-	
-	public boolean setPermissions(boolean permissionSettings);
-	
-	/*
-	 * Description: The setDeletedCISsNotification method invokes the configureCIS method 
-	 * 				inside the Community Lifecycle Management component 
-	 * 				in order to change parameters of a specific CIS.
-	 * Parameters: 
-	 * 				1) CIS - The CIS for which the setting is being changed.
-	 * 				2) permission - The permission data necessary to carry out the operation.
-	 * Returns:
-	 * 				* True if the method was able to set the permissions.
-	 *              * False if the method was unable to set the permissions.
-	 */
-	
-	public boolean setDeletedCISsNotification(CIS cis, Permissions permission);
+	public boolean deleteRule();
 
 }
