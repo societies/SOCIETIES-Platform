42409820ee2a4dccf8847dd73e2e9cbbdef437b2
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-11-14 12:53:27 +0100

    Renamed ...Local to ...Callback

--- platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/api/INegotiationRequesterCallback.java
+++ platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/api/INegotiationRequesterCallback.java
@@ -8,30 +8,30 @@ package org.societies.security.policynegotiator.api;
  * @author Mitja Vardjan
  *
  */
-public interface INegotiationRequesterInternal {
+public interface INegotiationRequesterCallback {
 
 	/**
 	 * Async return for
-	 * {@link INegotiationRequester#getPolicyOptions(INegotiationRequesterInternal)}.
+	 * {@link INegotiationRequester#getPolicyOptions(INegotiationRequesterCallback)}.
 	 * 
 	 * @param sops All available options for policy, embedded in a single XML document.
 	 */
-	public void returnSignedPolicyOptions(String sops);
+	public void onSignedPolicyOptions(String sops);
 	
 	/**
 	 * Async return for
-	 * {@link INegotiationRequester#negotiatePolicy(int, ResponsePolicy, INegotiationRequesterInternal)}.
+	 * {@link INegotiationRequester#negotiatePolicy(int, ResponsePolicy, INegotiationRequesterCallback)}.
 	 * 
 	 * @param modifiedPolicy Policy possibly modified by provider side.
 	 * Based on the policy sent before by the requester side.
 	 */
-	public void returnNegotiationResponse(ResponsePolicy modifiedPolicy);
+	public void onNegotiationResponse(ResponsePolicy modifiedPolicy);
 	
 	/**
 	 * Async return for
-	 * {@link INegotiationRequester#acceptPolicy(int, ResponsePolicy, INegotiationRequesterInternal)}.
+	 * {@link INegotiationRequester#acceptPolicy(int, ResponsePolicy, INegotiationRequesterCallback)}.
 	 * 
 	 * @param policy XML-based final policy signed by both parties.
 	 */
-	public void returnFinalPolicy(String policy);
+	public void onFinalPolicy(String policy);
 }
