ea97753419c117ef8de7b472ebb609e2abc51db8
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-11-14 12:35:40 +0100

    Initial API for ServiceDeployer

---
+++ css-modules/css-mgmt/ServiceDeployer/src/main/java/org/societies/cssmgmt/servicedeployer/api/IServiceDeployer.java
@@ -0,0 +1,21 @@
+package org.societies.cssmgmt.servicedeployer.api;
+
+/**
+ * Interface for invoking Service Deployer.
+ * The asynchronous methods return via {@link IServiceDeployerCallback}.
+ * 
+ * @author Mitja Vardjan
+ */
+public interface IServiceDeployer {
+
+	/**
+	 * Deploys given service to local CSS.
+	 * 
+	 * @param serviceId Service ID
+	 * 
+	 * @param nodeId CSS node ID.
+	 * 
+	 * @param callback The callback to be invoked after method is finished.
+	 */
+	public void deployServiceToNode(String serviceId, String nodeId, IServiceDeployerCallback callback);
+}
