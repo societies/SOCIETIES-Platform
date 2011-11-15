baa24b0e976f93db08dbdf49a96536eb0bd70917
Alan Walsh <awalsh@tssg.org>
2011-11-15 13:46:25 +0000

    Added Native option menu capability

--- client-framework/MasterGUI/src/org/societies/cft/MasterGUIActivity.java
+++ client-framework/MasterGUI/src/org/societies/cft/MasterGUIActivity.java
@@ -2,6 +2,9 @@ package org.societies.cft;
 
 import com.phonegap.DroidGap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MasterGUIActivity extends DroidGap {
 /** Called when the activity is first created. */
@@ -11,4 +14,24 @@ public class MasterGUIActivity extends DroidGap {
 //          setContentView(R.layout.main);
       super.loadUrl("file:///android_asset/www/index.html");
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+    @Override 
+    /**
+     * Workaround required since PhoneGap 1.0.0
+     */
+    public boolean onKeyDown(int keyCode,KeyEvent event){ 
+            if (keyCode == KeyEvent.KEYCODE_MENU) { 
+                 return false; 
+           }else{ 
+               return super.onKeyDown(keyCode, event); 
+           } 
+    }
+
 }
