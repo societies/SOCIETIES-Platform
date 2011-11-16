20d0dab9d3688ca2d2b288c656fe1cc385f9f22a
Alan Walsh <awalsh@tssg.org>
2011-11-16 16:15:49 +0000

    Sample custom theme and preferences example

--- client-framework/MasterGUI/src/org/societies/cft/MasterGUIActivity.java
+++ client-framework/MasterGUI/src/org/societies/cft/MasterGUIActivity.java
@@ -1,10 +1,13 @@
 package org.societies.cft;
 
 import com.phonegap.DroidGap;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MasterGUIActivity extends DroidGap {
 /** Called when the activity is first created. */
@@ -33,5 +36,19 @@ public class MasterGUIActivity extends DroidGap {
                return super.onKeyDown(keyCode, event); 
            } 
     }
-
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.about:
+            return true;
+        case R.id.preference:
+        	Intent prefIntent = new Intent(this, MasterPreferences.class);
+        	this.startActivity(prefIntent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
