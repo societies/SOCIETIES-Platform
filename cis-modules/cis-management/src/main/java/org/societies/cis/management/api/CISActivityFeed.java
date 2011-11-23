18cba9e17652b287134fa65e05759258c3b39217
Babak Farshchian <babak.farshchian@sintef.no>
2011-11-23 14:15:13 +0100

    Added references to Activity Streams and added some preliminary methods.
    Signed-off-by: Babak Farshchian <babak.farshchian@sintef.no>

--- cis-modules/cis-management/src/main/java/org/societies/cis/management/api/CISActivityFeed.java
+++ cis-modules/cis-management/src/main/java/org/societies/cis/management/api/CISActivityFeed.java
@@ -1,11 +1,17 @@
 /**
  * Is a feed of activities that are collected from CIS members and their shared services.
  * 
- * @author Babak Farshchian
+ * @link CISActivity
+ * @author Babak.Farshchian@sintef.no
  * @version 0
  */
 package org.societies.cis.management.api;
 
 public class CISActivityFeed {
+	public CISActivity[] activities;
+	public void getActivities(String CssId, String timePeriod){};
+	public void getActivities(String CssId, String query, String timePeriod){};
+	public void addCISActivity(CISActivity activity){};
+	public void cleanupFeed(String criteria){};
 
 }
