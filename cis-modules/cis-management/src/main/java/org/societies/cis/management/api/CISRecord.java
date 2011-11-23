cbbf15ac2c7f1a244618390d842fad58943c550f
Babak Farshchian <babak.farshchian@sintef.no>
2011-11-23 14:47:10 +0100

    Adding first version of CISRecord.

--- cis-modules/cis-management/src/main/java/org/societies/cis/management/api/CISRecord.java
+++ cis-modules/cis-management/src/main/java/org/societies/cis/management/api/CISRecord.java
@@ -7,6 +7,15 @@
 package org.societies.cis.management.api;
 
 public class CISRecord {
+	public CISActivityFeed feed;
+	public String ownerCss;
+	public String membershipCriteria;
+	/**
+	 * permaLink is a permanent URL to this CIS. A type of CIS homepage.
+	 */
+	public String permaLink;
+	public String[] membersCss;
+	private String password;
 	
 
 }
