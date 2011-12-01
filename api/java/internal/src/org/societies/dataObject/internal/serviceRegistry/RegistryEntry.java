1967a1c6be9e2e7838a690df94aaaae476d1d20e
apanazzolo-sn <apanazzolo@soluta.net>
2011-12-01 17:55:59 +0100

    add a first RegistryEntry class.

---
+++ api/java/internal/src/org/societies/dataObject/internal/serviceRegistry/RegistryEntry.java
@@ -0,0 +1,20 @@
+package org.societies.dataObject.internal.serviceRegistry;
+
+/**
+ * @author apanazzolo
+ * @version 1.0
+ * @created 01-dic-2011 17.53.53
+ */
+public class RegistryEntry {
+
+	private long id;
+
+	public RegistryEntry(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
