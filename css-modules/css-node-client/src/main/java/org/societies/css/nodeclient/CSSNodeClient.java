144eea6cbde8ab77b1b2ac8869d3f5c94b676f64
Alan Walsh <awalsh@tssg.org>
2011-12-02 14:46:40 +0000

    licence and springdm project

---
+++ css-modules/css-node-client/src/main/java/org/societies/css/nodeclient/CSSNodeClient.java
@@ -0,0 +1,45 @@
+package org.societies.css.nodeclient;
+
+import java.util.List;
+
+import org.societies.css.api.nodeclient.ICSSNodeClient;
+
+public class CSSNodeClient implements ICSSNodeClient {
+
+	@Override
+	public List checkDeviceConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List cssNodeDiscovery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List respondToCSSNodeDiscovery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List registerCSSNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean unregisterCSSNode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean changeCSSNodeStatus() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
