ea027725343accde994eaf478b5be7f713f00b1a
guyf <guyf@GUYF-TP.haifa.ibm.com>
2011-12-07 12:13:30 +0200

    Test

--- css-modules/device-mgmt/src/main/java/org/societies/device/api/GpsDeviceAPI.java
+++ css-modules/device-mgmt/src/main/java/org/societies/device/api/GpsDeviceAPI.java
@@ -3,7 +3,7 @@ package org.societies.device.api;
 
 /**
  * 
- * Interface for a GPS OS device API. The methods represents the basic functionalities any GPS device offers.  
+ * Interface for a GPS OS device API.  The methods represents the basic functionalities any GPS device offers.  
  * The implementor will use the OS device driver to produce the results. 
  *
  */
