adeaa4a468b101735d44d0493a2a122ee60ddec1
Guido Spadotto <gspadotto@soluta.net>
2011-12-09 16:57:42 +0100

    Fixed import declarations

--- api/java/external/src/main/java/org/societies/api/qosmonitor/IQoSReporterCallback.java
+++ api/java/external/src/main/java/org/societies/api/qosmonitor/IQoSReporterCallback.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.societies.api.slm.qosmonitor;
+package org.societies.api.qosmonitor;
 
 import java.io.Serializable;
 
