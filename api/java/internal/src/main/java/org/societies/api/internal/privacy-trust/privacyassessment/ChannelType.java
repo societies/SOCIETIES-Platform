7887a2a92581098240e19b0b476fb34106d0eba5
mvardjan <mitja.vardjan@setcce.si>
2011-12-09 15:56:16 +0100

    Data types changed to String or dedicated types

--- api/java/internal/src/main/java/org/societies/api/internal/privacy-trust/privacyassessment/ChannelType.java
+++ api/java/internal/src/main/java/org/societies/api/internal/privacy-trust/privacyassessment/ChannelType.java
@@ -1,5 +1,10 @@
 /**
- * Copyright (c) 2011, SOCIETIES Consortium
+ * Copyright (c) 2011, SOCIETIES Consortium (WATERFORD INSTITUTE OF TECHNOLOGY (TSSG), HERIOT-WATT UNIVERSITY (HWU), SOLUTA.NET 
+ * (SN), GERMAN AEROSPACE CENTRE (Deutsches Zentrum fuer Luft- und Raumfahrt e.V.) (DLR), Zavod za varnostne tehnologije
+ * informacijske družbe in elektronsko poslovanje (SETCCE), INSTITUTE OF COMMUNICATION AND COMPUTER SYSTEMS (ICCS), LAKE
+ * COMMUNICATIONS (LAKE), INTEL PERFORMANCE LEARNING SOLUTIONS LTD (INTEL), PORTUGAL TELECOM INOVAÇÃO, SA (PTIN), IBM Corp., 
+ * INSTITUT TELECOM (ITSUD), AMITEC DIACHYTI EFYIA PLIROFORIKI KAI EPIKINONIES ETERIA PERIORISMENIS EFTHINIS (AMITEC), TELECOM 
+ * ITALIA S.p.a.(TI),  TRIALOG (TRIALOG), Stiftelsen SINTEF (SINTEF), NEC EUROPE LTD (NEC))
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -17,30 +22,18 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.societies.api.internal.privacytrust.privacyassessment;
 
-import java.util.Date;
-
-import org.societies.api.internal.privacytrust.privacyassessment.ChannelType;
-
 /**
- * Interface for appending to log
- * 
+ * Type of communication channel. Examples: facebook, facebook wall post.
+ *
  * @author Mitja Vardjan
+ *
  */
-public interface IPrivacyLogAppend {
+public enum ChannelType {
 
-		/**
-		 * Log any outgoing information
-		 * 
-		 * @param dataType
-		 * @param time
-		 * @param sentToGroup    True if multicast
-		 * @param receiverId    CSS ID of the receiver
-		 * @param senderId    CSS ID of the sender
-		 * @param channelId    ID of the channel. Examples: facebook, facebook wall post,
-		 * twitter, XMPP
-		 */
-		public void log(String dataType, Date time, boolean sentToGroup, String receiverId, String senderId, ChannelType channelId);
+	FACEBOOK,
+	FACEBOOK_WALL_POST,
+	TWITTER,
+	XMPP
 }
