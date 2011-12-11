04ceff9ff7dfb643b206737ea123779d17709317
nikosk <nikosk@cn.ntua.gr>
2011-12-11 16:06:39 +0200

    to be removed

--- context-management/CM_platform_API/src/main/java/org/societies/context/user/db/api/platform/IUserCtxDBMgrCallback.java
+++ context-management/CM_platform_API/src/main/java/org/societies/context/user/db/api/platform/IUserCtxDBMgrCallback.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.societies.api.internal.context.user.db;
+package org.societies.context.user.db.api.platform;
 
 import java.util.List;
 
