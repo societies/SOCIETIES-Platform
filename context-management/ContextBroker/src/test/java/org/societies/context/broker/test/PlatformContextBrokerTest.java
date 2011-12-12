d90d174800260234ca37664188eee7e7fc275410
jpapai <jpapai@cn.ntua.gr>
2011-12-12 16:56:29 +0200

    Platform Context Broker test skeleton

--- context-management/ContextBroker/src/test/java/org/societies/context/broker/test/PlatformContextBrokerTest.java
+++ context-management/ContextBroker/src/test/java/org/societies/context/broker/test/PlatformContextBrokerTest.java
@@ -1,18 +1,58 @@
 package org.societies.context.broker.test;
 
+import org.societies.api.context.broker.IUserCtxBrokerCallback;
+import org.societies.context.broker.impl.PlatformContextBroker;
+import org.societies.api.mock.EntityIdentifier;
+
 public class PlatformContextBrokerTest {
 
+	private static PlatformContextBroker platformCtxBroker = null;
+	//private static IUserCtxDBMgr userDB;
+	
 	//Constructor
 	PlatformContextBrokerTest(){
-		//TODO
-			
+		
+		//here the user DB of the broker should be set using the setter: setUserDB(IUserCtxDBMgr userDB)
+		//setUserDB(userDB);
+		System.out.println("-- start of testing --");
+		testCreateCtxEntity();
+		testCreateCtxAssociation();
+		
 	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new PlatformContextBrokerTest();
+	}
+
+	
+	private void testCreateCtxEntity(){
+
+		System.out.println("---- test CreateCtxEntity external");
+		IUserCtxBrokerCallback exCallback = null;
+		EntityIdentifier identifier = null;
+		platformCtxBroker.createEntity(identifier, "person", exCallback);
+		
+		System.out.println("---- test CreateCtxEntity internal");
+		org.societies.api.internal.context.broker.IUserCtxBrokerCallback inCallback = null;
+		platformCtxBroker.createEntity("person", inCallback);
+		
+	}
+	
+	private void testCreateCtxAssociation(){
+		
+		System.out.println("---- test testCreateCtxAssociation external");
+		IUserCtxBrokerCallback exCallback = null;
+		EntityIdentifier identifier = null;
+		platformCtxBroker.createAssociation(identifier, "person", exCallback);
+		
+		System.out.println("---- test testCreateCtxAssociation internal");
+		org.societies.api.internal.context.broker.IUserCtxBrokerCallback inCallback = null;
+		platformCtxBroker.createAssociation("person", inCallback);
+		
 	}
 
 }
