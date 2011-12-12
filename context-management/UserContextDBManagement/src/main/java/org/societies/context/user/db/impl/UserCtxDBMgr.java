5197987b7e7262e511db7d17a2acd707b5ee5302
NicolasLiampotis <nicolas.liampotis@cn.ntua.gr>
2011-12-12 16:26:23 +0000

    First attempt to play with spring

--- context-management/UserContextDBManagement/src/main/java/org/societies/context/user/db/impl/UserCtxDBMgr.java
+++ context-management/UserContextDBManagement/src/main/java/org/societies/context/user/db/impl/UserCtxDBMgr.java
@@ -40,16 +40,17 @@ import org.societies.api.context.model.IndividualCtxEntity;
 import org.societies.api.internal.context.user.db.IUserCtxDBMgr;
 import org.societies.api.internal.context.user.db.IUserCtxDBMgrCallback;
 import org.societies.api.mock.EntityIdentifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserCtxDBMgr implements IUserCtxDBMgr{
 
 	private final Map<CtxIdentifier, CtxModelObject> modelObjects;
 
 	private final EntityIdentifier privateId;
 
-	/**
-	 * Constructs and boots the standalone edition of the UserCtxDBMgr
-	 */
+	@Autowired
 	public UserCtxDBMgr() {
 		this.modelObjects =  new HashMap<CtxIdentifier, CtxModelObject>();
 		this.privateId = new EntityIdentifier();
