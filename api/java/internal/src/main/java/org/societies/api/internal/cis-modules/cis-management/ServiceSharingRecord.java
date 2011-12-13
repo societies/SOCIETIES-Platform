efd60647b29e86199ed5abffaa80dc622888b5bc
Jacqueline Floch <jacqueline.floch@sintef.no>
2011-12-13 17:10:37 +0100

    API moved

---
+++ api/java/internal/src/main/java/org/societies/api/internal/cis-modules/cis-management/ServiceSharingRecord.java
@@ -0,0 +1,11 @@
+/**
+ * Describes how a service is shared in a CIS.
+ * 
+ * @author Babak Farshchian
+ * @version 0
+ */
+package org.societies.cis.management.api;
+
+public class ServiceSharingRecord {
+
+}
