6c01b7a0352282bd370ba723a2737a4dc586875d
Jacqueline Floch <jacqueline.floch@sintef.no>
2011-12-13 17:01:28 +0100

    APIs moved

---
+++ api/java/internal/src/main/java/org/societies/api/internal/cis-modules/cis-directory/CisAdvertisementRecord.java
@@ -0,0 +1,41 @@
+/**
+ * This class defines a data type that is used stored in and managed by a CIS Directory.
+ * 
+ * @author Babak Farshchian
+ * @version 0
+ * 
+ */
+
+package org.societies.cis.discovery.api;
+
+public class CISAdvertisementRecord {
+	private String name, id, uri;
+	
+	public CISAdvertisementRecord(String name, String id, String uri) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.uri = uri;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+
+}
