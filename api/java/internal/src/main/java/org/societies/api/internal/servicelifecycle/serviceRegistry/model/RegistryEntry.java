d30856e6a044d8513577cc7be12ce933715587d8
Massimo Mazzariol <mmazzariol@soluta.net>
2011-12-15 15:44:48 +0100

    Change RegistryEntry and set getters and setters

--- api/java/internal/src/main/java/org/societies/api/internal/servicelifecycle/serviceRegistry/model/RegistryEntry.java
+++ api/java/internal/src/main/java/org/societies/api/internal/servicelifecycle/serviceRegistry/model/RegistryEntry.java
@@ -3,9 +3,6 @@ package org.societies.api.internal.servicelifecycle.serviceRegistry.model;
 import java.net.URI;
 import java.util.List;
 
-//TODO : temporary mock package import to solve missing package in API folder
-import org.societies.api.internal.mock.EndPoint;
-
 /**
  * This is the Class accepted by the ServiceRegistry when a service wants to
  * register.
@@ -18,31 +15,126 @@ import org.societies.api.internal.mock.EndPoint;
  */
 public class RegistryEntry {
 
+	private long id;
+
+	
 	private String serviceName;
 	private String version;
 	private String serviceDescription;
-	// BJB : remove reference to org.societies.comm.xmpp
-	private List<EndPoint> sharedWithCSS;
-	private List<EndPoint> sharedWithCIS;
-	private EndPoint CSSInstalled;
-	private EndPoint CSSNodeInstalled;
-	/* old
-	private List<org.societies.comm.xmpp.Endpoint> sharedWithCSS;
-	private List<org.societies.comm.xmpp.Endpoint> sharedWithCIS;
-	private org.societies.comm.xmpp.Endpoint CSSInstalled;
-	private org.societies.comm.xmpp.Endpoint CSSNodeInstalled;
-	end old */
+	// private List<Endpoint> sharedWithCSS;
+	// private List<Endpoint> sharedWithCIS;
+	// private Endpoint CSSInstalled;
+	// private Endpoint CSSNodeInstalled;
 	private URI serviceURI;
 	private String organizationId;
-	private ServiceResourceIdentifier serviceIdentifier;
+	// private ServiceResourceIdentifier serviceIdentifier;
 	private String authorSignature;
 
-	public RegistryEntry(){
+	public RegistryEntry() {
 
 	}
 
 	public void finalize() throws Throwable {
 
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getServiceDescription() {
+		return serviceDescription;
+	}
+
+	public void setServiceDescription(String serviceDescription) {
+		this.serviceDescription = serviceDescription;
+	}
+
+//	public List<Endpoint> getSharedWithCSS() {
+//		return sharedWithCSS;
+//	}
+
+//	public void setSharedWithCSS(
+//			List<Endpoint> sharedWithCSS) {
+//		this.sharedWithCSS = sharedWithCSS;
+//	}
+
+//	public List<Endpoint> getSharedWithCIS() {
+//		return sharedWithCIS;
+//	}
+
+//	public void setSharedWithCIS(
+//			List<Endpoint> sharedWithCIS) {
+//		this.sharedWithCIS = sharedWithCIS;
+//	}
+
+//	public Endpoint getCSSInstalled() {
+//		return CSSInstalled;
+//	}
+
+//	public void setCSSInstalled(Endpoint cSSInstalled) {
+//		CSSInstalled = cSSInstalled;
+//	}
+
+//	public Endpoint getCSSNodeInstalled() {
+//		return CSSNodeInstalled;
+//	}
+
+//	public void setCSSNodeInstalled(
+//			Endpoint cSSNodeInstalled) {
+//		CSSNodeInstalled = cSSNodeInstalled;
+//	}
+
+	public URI getServiceURI() {
+		return serviceURI;
+	}
+
+	public void setServiceURI(URI serviceURI) {
+		this.serviceURI = serviceURI;
+	}
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+//	public ServiceResourceIdentifier getServiceIdentifier() {
+//		return serviceIdentifier;
+//	}
+
+//	public void setServiceIdentifier(ServiceResourceIdentifier serviceIdentifier) {
+//		this.serviceIdentifier = serviceIdentifier;
+//	}
+
+	public String getAuthorSignature() {
+		return authorSignature;
+	}
+
+	public void setAuthorSignature(String authorSignature) {
+		this.authorSignature = authorSignature;
 	}
 
 }
