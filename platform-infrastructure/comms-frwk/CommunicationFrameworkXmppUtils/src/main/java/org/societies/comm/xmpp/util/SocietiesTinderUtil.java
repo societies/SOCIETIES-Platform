e092b162ef885053eeb96f218c8c3186842d0ac3
Miquel Martin <contact@miquelmartin.org>
2011-12-18 18:40:37 +0100

    - renamed namespaceextensions to featureservers to make the api more understandable and less xml specific
    - First try: Made the FeatureServers specify the type of message they expect using genericsso they don't have to cast their payloads to whatever type they need.
    - Second try: understood that that will never fly because of type erasure http://docs.oracle.com/javase/tutorial/java/generics/erasure.html
    
    - Reorganized the (now called commons) interface project since it now has  common datatypes, exceptions and interfaces
    
    - Javadocing around
    
    - Extracted the core functionality of the communication manager implementation in to an Helper class, so that the External Component and the Smack client can use it.
    
    - Tweaked naming: Pojo -> Bean
    
    - Moved Tinder utils to static factories in Stanza and Endpoint and eliminated the TinderUtils project
    
    - Added exception handling (return IQ errors on exception or at least log)
    
    - Heavily refactored the CommHelper to avoid code duplication when handling the different messsages
    
    -  SocietiesTinderUtil.toPacket(Stanza s) was not implemented. I have no idea how Joao plans to go from proprietary Societies endpoints to JID. Implementation's pending at the Stanza class
    
    - Created the project for the client version of the comm mgr

--- platform-infrastructure/comms-frwk/CommunicationFrameworkXmppUtils/src/main/java/org/societies/comm/xmpp/util/SocietiesTinderUtil.java
+++ platform-infrastructure/comms-frwk/CommunicationFrameworkXmppUtils/src/main/java/org/societies/comm/xmpp/util/SocietiesTinderUtil.java
@@ -1,29 +1,18 @@
 package org.societies.comm.xmpp.util;
 
-import org.societies.comm.identity.Identity;
-import org.societies.comm.identity.Identity.IdentityType;
-import org.societies.comm.xmpp.Endpoint;
-import org.societies.comm.xmpp.Stanza;
+import org.societies.comm.xmm.datatypes.Endpoint;
+import org.societies.comm.xmm.datatypes.Identity;
+import org.societies.comm.xmm.datatypes.Identity.IdentityType;
+import org.societies.comm.xmm.datatypes.Stanza;
 import org.xmpp.packet.JID;
 import org.xmpp.packet.Packet;
 
 public abstract class SocietiesTinderUtil {
-	
+
 	public static Packet toPacket(Stanza stanza) {
-		return null; // TODO not implemented!
+		throw new UnsupportedOperationException("Not yet implemented!");
 	}
-	
-	public static Stanza toStanzaInfo(Packet packet) {
-		Endpoint to = toEndpoint(packet.getTo());
-		Endpoint from = toEndpoint(packet.getFrom());
-		Stanza returnStanza = new Stanza(packet.getID(), from, to);
-		return returnStanza;
-	}
-	
-	public static Endpoint toEndpoint(JID jid) {
-		Identity i = new Identity(IdentityType.CSS,jid.getNode(),jid.getDomain()); // TODO hardcoded for now
-		Endpoint e = new Endpoint(i, jid.getResource());
-		return e;
-	}
+
+
 
 }
--- platform-infrastructure/comms-frwk/XCCommunicationFrameworkBundle/readme.txt
+++
@@ -1,4 +0,0 @@
-To use:
-	Copy xc.properties (XMPP configuration) to the Virgo pickup directory
-	Build using Maven (mvn package)
-	Copy the resulting jar to the Virgo pickup directory
