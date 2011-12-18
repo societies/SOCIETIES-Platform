115b2f3d527d7dc39411b02ca2ad290c580aecb4
Miquel Martin <contact@miquelmartin.org>
2011-12-18 18:44:01 +0100

    - renamed namespaceextensions to featureservers to make the api more understandable and less xml specific
    - First try: Made the FeatureServers specify the type of message they expect using genericsso they don't have to cast their payloads to whatever type they need.
    - Second try: understood that that will never fly because of type erasure http://docs.oracle.com/javase/tutorial/java/generics/erasure.html
    
    - Reorganized the (now called commons) interface project since it now has  common datatypes, exceptions and interfaces
    
    - Javadocing around
    
    - Extracted the core functionality of the communication manager implementation in to an Helper class, so that the External Component and the Smack client can use it.
    
    - Tweaked naming: Pojo -> Bean
    
    - Moved Tinder utils to static factories in Stanza and Endpoint and eliminated the TinderUtils project
    
    - Added exception handling (return IQ errors on exception or at least log)
    
    - Heavily refactored the CommHelper to avoid code duplication when handling the different messsages
    
    -  SocietiesTinderUtil.toPacket(Stanza s) was not implemented. I have no idea how Joao plans to go from proprietary Societies endpoints to JID. Implementation's pending at the Stanza class
    
    - Created the project for the client version of the comm mgr

---
+++ platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/XCCommunicationMgr.java
@@ -0,0 +1,117 @@
+package org.societies.comm.xmpp.xc.impl;
+
+import org.jivesoftware.whack.ExternalComponentManager;
+import org.societies.comm.xmpp.common.CommManagerHelper;
+import org.societies.comm.xmpp.datatypes.Stanza;
+import org.societies.comm.xmpp.exceptions.CommunicationException;
+import org.societies.comm.xmpp.interfaces.CommCallback;
+import org.societies.comm.xmpp.interfaces.CommManager;
+import org.societies.comm.xmpp.interfaces.FeatureServer;
+import org.xmpp.component.AbstractComponent;
+import org.xmpp.component.ComponentException;
+import org.xmpp.packet.IQ;
+import org.xmpp.packet.Message;
+
+public class XCCommunicationMgr extends AbstractComponent implements
+		CommManager {
+
+	private final CommManagerHelper helper;
+
+	private String host;
+	private String subDomain;
+	private String secretKey;
+	private ExternalComponentManager manager;
+
+	public XCCommunicationMgr(String host, String subDomain,
+			String secretKey) {
+		this.helper = new CommManagerHelper();
+		this.host = host;
+		this.subDomain = subDomain;
+		this.secretKey = secretKey;
+
+		initWhackCommManager();
+	}
+
+	private void initWhackCommManager() {
+		manager = new ExternalComponentManager(host);
+		manager.setSecretKey(subDomain, secretKey);
+
+		log.info("Connected!");
+		try {
+			manager.addComponent(subDomain, this);
+		} catch (ComponentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Implementation of AbstractComponent methods
+	 */
+
+	@Override
+	public String getDescription() {
+		return "this external component acts as a dispatcher to "
+				+ "facilitate the communication among Societies components";
+	}
+
+	@Override
+	public String getName() {
+		return "Societies Communication Manager";
+	}
+
+	@Override
+	protected String[] discoInfoFeatureNamespaces() {
+		return helper.getSupportedNamespaces();
+	}
+
+	@Override
+	protected IQ handleIQGet(IQ iq) {
+		log.debug("IQ Received");
+		IQ response = helper.forwardIQ(iq);
+		return response;
+	}
+
+	@Override
+	protected IQ handleIQSet(IQ iq) throws Exception {
+		return handleIQGet(iq);
+	}
+
+	@Override
+	protected void handleIQResult(IQ iq) {
+		log.debug("IQ Result received");
+		helper.forwardIQResult(iq);
+	}
+
+	@Override
+	protected void handleIQError(IQ iq) {
+		helper.forwardIQError(iq);
+	}
+
+	@Override
+	protected void handleMessage(Message message) {
+		helper.forwardMessage(message);
+	}
+
+	/*
+	 * Implementation of CommunicationManager methods
+	 */
+
+	// TODO test thread.getclassloader and Async
+	@Override
+	public void register(FeatureServer fs) throws CommunicationException,
+			ClassNotFoundException {
+		helper.register(fs);
+	}
+
+	@Override
+	public void sendIQ(Stanza stanza, IQ.Type type, Object payload,
+			CommCallback callback) throws CommunicationException {
+		helper.sendIQ(stanza, type, payload, callback);
+	}
+
+	@Override
+	public void sendMessage(Stanza stanza, Message.Type type, Object payload)
+			throws CommunicationException {
+		helper.sendMessage(stanza, type, payload);
+	}
+}
