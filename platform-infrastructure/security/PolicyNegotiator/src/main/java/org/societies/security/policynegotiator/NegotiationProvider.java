a242525a08fda0281c2aa0918d1e9779c5342f5a
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-12-22 17:17:12 +0100

    PoliciyNegotiator uses Sign component. It doesn't work yet.

--- platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/NegotiationProvider.java
+++ platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/NegotiationProvider.java
@@ -3,9 +3,28 @@ package org.societies.security.policynegotiator;
 import org.societies.api.internal.privacytrust.privacyprotection.model.privacypolicy.ResponsePolicy;
 import org.societies.api.internal.security.policynegotiator.INegotiationProvider;
 import org.societies.api.internal.security.policynegotiator.INegotiationRequesterCallback;
+import org.societies.api.security.sign.ISign;
 
 public class NegotiationProvider implements INegotiationProvider {
 
+	private final String TAG = NegotiationProvider.class.getName();
+	
+	private ISign sign;
+	
+	public NegotiationProvider() {
+		System.out.println(TAG + ", " + "NegotiationProvider()");
+	}
+	
+	public void setSign(ISign sign) {
+		System.out.println(TAG + ", " + "setSign()");
+		this.sign = sign;
+	}
+	
+	public ISign getSign() {
+		System.out.println(TAG + ", " + "getSign()");
+		return sign;
+	}
+	
 	@Override
 	public void getPolicyOptions(INegotiationRequesterCallback callback) {
 		// TODO Auto-generated method stub
