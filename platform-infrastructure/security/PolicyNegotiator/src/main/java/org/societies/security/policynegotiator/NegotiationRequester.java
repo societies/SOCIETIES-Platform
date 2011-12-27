c2af885ed1071bb97688b63d534c7db523af200b
Mitja Vardjan <mitja.vardjan@setcce.si>
2011-12-27 13:42:13 +0100

    Same approach applied to NegotiationRequester

--- platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/NegotiationRequester.java
+++ platform-infrastructure/security/PolicyNegotiator/src/main/java/org/societies/security/policynegotiator/NegotiationRequester.java
@@ -2,8 +2,27 @@ package org.societies.security.policynegotiator;
 
 import org.societies.api.internal.security.policynegotiator.INegotiationProviderCallback;
 import org.societies.api.internal.security.policynegotiator.INegotiationRequester;
+import org.societies.api.security.sign.ISign;
 
 public class NegotiationRequester implements INegotiationRequester, INegotiationProviderCallback {
+
+	private final String TAG = NegotiationProvider.class.getName();
+	
+	private ISign sign;
+	
+	public NegotiationRequester() {
+		System.out.println(TAG + ", " + "NegotiationRequester()");
+	}
+	
+	public void setSign(ISign sign) {
+		System.out.println(TAG + ", " + "setSign()");
+		this.sign = sign;
+	}
+	
+	public ISign getSign() {
+		System.out.println(TAG + ", " + "getSign()");
+		return sign;
+	}
 
 	@Override
 	public void onGetPolicyOptions(int sessionId, String sops) {
