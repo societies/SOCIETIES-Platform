e3892f2a97307684ba610a4b0210417a7d948f19
nikosk <nikosk@cn.ntua.gr>
2012-01-27 14:57:04 +0200

    CAUI discovery component added CAUI model updated

---
+++ personalisation/CAUIDiscovery/src/main/java/org/societies/personalisation/CAUIDiscovery/impl/CAUIDiscovery.java
@@ -0,0 +1,13 @@
+package org.societies.personalisation.CAUIDiscovery.impl;
+
+import org.societies.personalisation.CAUI.api.CAUIDiscovery.ICAUIDiscovery;
+
+public class CAUIDiscovery implements ICAUIDiscovery{
+
+	@Override
+	public void generateNewUserModel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
