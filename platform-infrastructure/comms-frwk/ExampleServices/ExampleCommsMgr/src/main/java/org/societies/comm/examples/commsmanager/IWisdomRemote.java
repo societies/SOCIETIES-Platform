15b86d95c2902944f1741d356c4afe93d7a4c0c5
aleckey <alec.leckey@gmail.com>
2012-02-02 12:46:17 +0000

    Cleaned up example for tutorial

--- platform-infrastructure/comms-frwk/ExampleServices/ExampleCommsMgr/src/main/java/org/societies/comm/examples/commsmanager/IWisdomRemote.java
+++ platform-infrastructure/comms-frwk/ExampleServices/ExampleCommsMgr/src/main/java/org/societies/comm/examples/commsmanager/IWisdomRemote.java
@@ -24,24 +24,14 @@
  */
 package org.societies.comm.examples.commsmanager;
 
-import java.util.concurrent.Future;
+import org.societies.comm.examples.fortunecookie.datatypes.Cookie;
 
 /**
- * Calculator Client interface. Each method requires a callback to receive the result
- * to showcase asynchronous calls.
+ * Describe your class here...
  *
  * @author aleckey
  *
  */
-public interface ICalcRemote {
-	/**Adds 2 numbers together and returns total
-	 */
-    public int Add(int a, int b);
-    
-    /**Subtracts 2 numbers and returns total
-     */
-    public int Subtract(int a, int b);
-
-    /**Adds 2 numbers together and returns total */
-    public void AddAsync(int a, int b, IExamplesCallback callback);
+public interface IWisdomRemote {
+	public Cookie getCookie();
 }
