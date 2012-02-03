614d80785f8ea930357ebdb842468db403425be5
pkuppuud <kperumalin@hotmail.com>
2012-02-03 12:13:00 +0000

    initial api for service management is added

---
+++ platform-infrastructure/servicelifcycle/serviceManagement/src/main/java/org/societies/slm/servicemgmt/osgi/ServiceRegistryListener.java
@@ -0,0 +1,21 @@
+package org.societies.slm.servicemgmt.osgi;
+
+import java.util.Map;
+
+import org.springframework.osgi.service.importer.OsgiServiceLifecycleListener;
+
+public class ServiceRegistryListener implements OsgiServiceLifecycleListener {
+
+	@Override
+	public void bind(Object arg0, @SuppressWarnings("rawtypes") Map arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unbind(Object arg0, @SuppressWarnings("rawtypes") Map arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
