476b607fb87ddcf43a885eb9033be31e4d59a3c3
tcarlyle <thomas.vilarinho@sintef.no>
2012-02-03 15:51:46 +0100

    more renaming

--- api/java/internal/src/main/java/org/societies/api/internal/css/discovery/ICssDiscovery.java
+++ api/java/internal/src/main/java/org/societies/api/internal/css/discovery/ICssDiscovery.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.societies.api.internal.css_modules.css_discovery;
+package org.societies.api.internal.css.discovery;
 
 import java.util.Collection;
 import java.util.List;
