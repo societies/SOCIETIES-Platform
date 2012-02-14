0faabdfcb487055babddabfe8369cf227da8cd49
Bruno Jean-Bart <bruno.jean-bart@trialog.com>
2012-02-14 20:28:20 +0100

    Add a junit test of the provider example and a easymock test of the consumer example. Bruno.

--- example/example.service.consumer/src/main/java/org/societies/service/consumer/MathServiceConsumer.java
+++ example/example.service.consumer/src/main/java/org/societies/service/consumer/MathServiceConsumer.java
@@ -16,6 +16,7 @@ public class MathServiceConsumer{
 		System.out.println("numbers from property file :" +num_a+" "+num_b);
 	}
 	
+	// Start Bean mandatory methods 
 	public IMathService getMathService() {
 		return mathService;
 	}
@@ -23,10 +24,25 @@ public class MathServiceConsumer{
 	public void setMathService(IMathService mathService) {
 		this.mathService = mathService;
 	}
+	//End Bean mandatory methods
 		
+	// used to demonstrate how starting an execution of a bundle process
 	public void initMethod() throws Exception {		
 	System.out.println("add result is : "+ 	getMathService().add(num_a, num_b));
 	System.out.println("multiply result is : "+ 	getMathService().multiply(num_a, num_b));
 	System.out.println("subtract result is : "+ 	getMathService().subtract(num_a, num_b));
 	}
+	
+	// used to demonstrate how to test a method calling an interface of another bundle
+	public void collaborationCall(int num_a, int num_b) {
+		System.out.println("add "+ num_a + "+" +num_b);
+		
+		System.out.println("add result is : "+ 	getMathService().add(num_a, num_b));
+		/*
+		System.out.println("multiply result is : "+ 	getMathService().multiply(num_a, num_b));
+		System.out.println("subtract result is : "+ 	getMathService().subtract(num_a, num_b));
+		*/
+	}
+	
+	//
 }
