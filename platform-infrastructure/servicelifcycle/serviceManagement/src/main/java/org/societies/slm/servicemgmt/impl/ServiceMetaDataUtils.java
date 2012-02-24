a122567fdbff9b2b9529c5301d9781ffd1cdd4ec
pkuppuud <kperumalin@hotmail.com>
2012-02-24 09:11:15 +0000

    pushing missing class ServiceMetadataUtils

---
+++ platform-infrastructure/servicelifcycle/serviceManagement/src/main/java/org/societies/slm/servicemgmt/impl/ServiceMetaDataUtils.java
@@ -0,0 +1,30 @@
+package org.societies.slm.servicemgmt.impl;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.societies.api.internal.servicelifecycle.model.Service;
+import org.societies.api.internal.servicelifecycle.model.ServiceResourceIdentifier;
+
+public class ServiceMetaDataUtils {
+
+	public ServiceMetaDataUtils() {
+		
+	}
+
+	public static ServiceResourceIdentifier generateServiceResourceIdentifier(Service service){
+		// ***** To do ********
+		// some logic to map available meta data and xmpp service identity 
+		// and construct serviceResourceIdentity object
+		// then pass return this object
+		ServiceResourceIdentifier serResId=new ServiceResourceIdentifier();		
+		try {
+			serResId.setIdentifier(new URI("http://perumal@societies.org/pkcss/someservice"));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return serResId;
+		
+	}
+}
