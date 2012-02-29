b6be088e410708ea2560a89da2d7bda36ef1309b
Joao M. Goncalves <jmgonc@gmail.com>
2012-02-29 19:31:24 +0000

    implemented basic commsmanager factory and test case

--- platform-infrastructure/comms-frwk/ExampleServices/TestBundle/src/main/java/org/societies/comm/test/TestPubsubClient.java
+++ platform-infrastructure/comms-frwk/ExampleServices/TestBundle/src/main/java/org/societies/comm/test/TestPubsubClient.java
@@ -23,7 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class TestPubsubClient extends Thread implements Subscriber, ApplicationListener<PubsubEvent> {
 	
 	private static Logger LOG = LoggerFactory
@@ -33,7 +33,7 @@ public class TestPubsubClient extends Thread implements Subscriber, ApplicationL
 	private IIdentityManager idm;
 	private ICommManager endpoint;
 	
-	@Autowired
+//	@Autowired
 	public TestPubsubClient(PubsubClient psc, ICommManager endpoint) {
 		this.psc = psc;
 		this.endpoint = endpoint;
