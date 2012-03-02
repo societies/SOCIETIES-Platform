57447f3ec305f054eb9cdd490fd54b88dc19ee5c
Olivier Maridat <olivier.maridat@trialog.com>
2012-03-02 10:40:10 +0100

    Creation of the Societies-XMPP-Maven-Plugin

---
+++ utilities/maven/plugin-consumer/src/main/java/org/societies/example/complexservice/schema/ServiceAMsgBeanResult.java
@@ -0,0 +1,15 @@
+package org.societies.example.complexservice.schema;
+
+public class ServiceAMsgBeanResult {
+
+	private MyComplexBean complexBean;
+
+	public MyComplexBean getComplexBean() {
+		return complexBean;
+	}
+
+	public void setComplexBean(MyComplexBean complexBean) {
+		this.complexBean = complexBean;
+	}
+	
+}
