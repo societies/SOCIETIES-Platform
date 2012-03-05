907ffeb72ffb1e85a9a766b3968bfadcf0601965
Joao M. Goncalves <jmgonc@gmail.com>
2012-03-05 17:48:23 +0000

    changes during t45 call

--- platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
+++ platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
@@ -2,6 +2,7 @@ package org.societies.comm.xmpp.xc.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
 import org.societies.api.identity.IIdentity;
@@ -10,9 +11,11 @@ import org.societies.api.internal.comm.ICISCommunicationMgrFactory;
 public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFactory {
 
 	private HashMap<IIdentity,ICommManager> cisCommManagers;
+	private String genericPassword;
 	
-	public CISCommunicationMgrFactoryImpl() {
+	public CISCommunicationMgrFactoryImpl(ICommManager endpoint, String genericPassword) {
 		cisCommManagers = new HashMap<IIdentity, ICommManager>();
+		this.genericPassword = genericPassword;
 	}
 	
 	@Override
@@ -25,5 +28,15 @@ public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFacto
 	@Override
 	public Map<IIdentity, ICommManager> getAllCISCommMgrs() {
 		return cisCommManagers;
+	}
+
+	@Override
+	public ICommManager getNewCommManager() {
+		String randomCisIdentifier = UUID.randomUUID().toString();
+		// TODO verify if exists
+		IIdentity cisIdentity = null; //TODO
+		XCCommunicationMgr commMgr = new XCCommunicationMgr(cisIdentity.getDomain(), cisIdentity.getJid(), genericPassword);
+		cisCommManagers.put(cisIdentity, commMgr);
+		return commMgr;
 	}
 }
