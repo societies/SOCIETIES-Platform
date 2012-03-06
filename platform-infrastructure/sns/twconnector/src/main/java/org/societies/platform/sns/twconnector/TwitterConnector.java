98ca7ccf88dab2ca6c5d99c1c501ef985fa41f9e
alps <alps@157.159.160.158>
2012-03-06 14:44:34 +0100

    add Twitter Connector initial code

---
+++ platform-infrastructure/sns/twconnector/src/main/java/org/societies/platform/sns/twconnector/TwitterConnector.java
@@ -0,0 +1,15 @@
+package org.societies.platform.sns.twconnector;
+
+
+class TwitterConnector{
+	
+	public TwitterClient twClient = null;
+	
+	public TwitterConnector(){
+		this.twClient = new TwitterClient();
+	}
+	
+	
+	
+	
+}
