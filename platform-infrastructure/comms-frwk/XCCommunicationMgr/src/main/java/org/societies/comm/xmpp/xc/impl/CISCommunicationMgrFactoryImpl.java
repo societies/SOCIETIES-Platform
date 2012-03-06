aefbfe35216945e0c8dd0922a16e78b058486ab6
Joao M. Goncalves <jmgonc@gmail.com>
2012-03-06 14:11:45 +0000

    bugfix in schema pom; 1st implementation of the required method from t45 call

--- platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
+++ platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
@@ -4,18 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
 import org.societies.api.identity.IIdentity;
+import org.societies.api.identity.IIdentityManager;
+import org.societies.api.identity.InvalidFormatException;
 import org.societies.api.internal.comm.ICISCommunicationMgrFactory;
 
 public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFactory {
 
+	private static Logger LOG = LoggerFactory
+			.getLogger(CISCommunicationMgrFactoryImpl.class);
+	
 	private HashMap<IIdentity,ICommManager> cisCommManagers;
 	private String genericPassword;
+	private ICommManager originalEndpoint;
+	private IIdentityManager idm;
+	private String domainName;
 	
 	public CISCommunicationMgrFactoryImpl(ICommManager endpoint, String genericPassword) {
 		cisCommManagers = new HashMap<IIdentity, ICommManager>();
 		this.genericPassword = genericPassword;
+		originalEndpoint = endpoint;
+		idm = originalEndpoint.getIdManager();
+		domainName = idm.getThisNetworkNode().getDomain();
 	}
 	
 	@Override
@@ -32,11 +45,16 @@ public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFacto
 
 	@Override
 	public ICommManager getNewCommManager() {
-		String randomCisIdentifier = UUID.randomUUID().toString();
-		// TODO verify if exists
-		IIdentity cisIdentity = null; //TODO
-		XCCommunicationMgr commMgr = new XCCommunicationMgr(cisIdentity.getDomain(), cisIdentity.getJid(), genericPassword);
-		cisCommManagers.put(cisIdentity, commMgr);
-		return commMgr;
+		try {
+			String randomCisIdentifier = UUID.randomUUID().toString()+"."+domainName;
+			// TODO verify if exists
+			IIdentity cisIdentity = idm.fromJid(randomCisIdentifier);
+			XCCommunicationMgr commMgr = new XCCommunicationMgr(cisIdentity.getDomain(), cisIdentity.getJid(), genericPassword);
+			cisCommManagers.put(cisIdentity, commMgr);
+			return commMgr;
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
