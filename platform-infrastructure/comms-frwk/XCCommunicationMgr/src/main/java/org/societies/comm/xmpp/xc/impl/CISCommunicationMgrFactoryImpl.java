81bb4bb1804f5101d6ab8757542ccd26f73c610f
Joao M. Goncalves <jmgonc@gmail.com>
2012-03-07 11:33:40 +0000

    schema internal builder references to pubsub removed

--- platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
+++ platform-infrastructure/comms-frwk/XCCommunicationMgr/src/main/java/org/societies/comm/xmpp/xc/impl/CISCommunicationMgrFactoryImpl.java
@@ -28,7 +28,6 @@ public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFacto
 		this.genericPassword = genericPassword;
 		originalEndpoint = endpoint;
 		idm = originalEndpoint.getIdManager();
-		domainName = idm.getThisNetworkNode().getDomain();
 	}
 	
 	@Override
@@ -45,6 +44,10 @@ public class CISCommunicationMgrFactoryImpl implements ICISCommunicationMgrFacto
 
 	@Override
 	public ICommManager getNewCommManager() {
+		// TODO move to init method
+		if (domainName==null)
+			domainName = idm.getThisNetworkNode().getDomain();
+		
 		try {
 			String randomCisIdentifier = UUID.randomUUID().toString()+"."+domainName;
 			// TODO verify if exists
