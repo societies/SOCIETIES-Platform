301c22727893ecf4df8d98e62834d6409fe0c290
sanchocsa <sanchocsa@gmail.com>
2012-03-15 18:48:48 +0000

    Refactored Service Management
    Removed the Service Management package, and the API, moved the needed
    classes into Service Control.

--- platform-infrastructure/service-lifecycle/service-mgmt/src/main/java/org/societies/slm/servicecontrol/ServiceRegistryListener.java
+++ platform-infrastructure/service-lifecycle/service-mgmt/src/main/java/org/societies/slm/servicecontrol/ServiceRegistryListener.java
@@ -22,7 +22,7 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.societies.slm.servicemgmt.impl;
+package org.societies.slm.servicecontrol;
 
 import java.util.ArrayList;
 import java.util.List;
