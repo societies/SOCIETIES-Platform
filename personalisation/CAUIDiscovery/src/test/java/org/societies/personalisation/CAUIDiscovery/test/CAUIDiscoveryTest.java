7159fff729d5cfd999b41de4a5b955af7d7b42f6
nikosk <nikosk@cn.ntua.gr>
2012-03-16 11:50:32 +0200

    minor fix

--- personalisation/CAUIDiscovery/src/test/java/org/societies/personalisation/CAUIDiscovery/test/CAUIDiscoveryTest.java
+++ personalisation/CAUIDiscovery/src/test/java/org/societies/personalisation/CAUIDiscovery/test/CAUIDiscoveryTest.java
@@ -1,5 +1,44 @@
 package org.societies.personalisation.CAUIDiscovery.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class CAUIDiscoveryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGenerateNewUserModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDictionary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOccurences() {
+		fail("Not yet implemented");
+	}
 
 }
