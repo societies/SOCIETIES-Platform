a73e35b71dedff9679ac5632c204e5a41794f422
Nicolas Liampotis <nicolas.liampotis@cn.ntua.gr>
2012-03-19 01:49:12 +0200

    Configure sl4j logging in user-context-db-mgmt

--- context-management/UserContextDBManagement/src/main/java/org/societies/context/user/db/impl/UserCtxDBMgr.java
+++ context-management/UserContextDBManagement/src/main/java/org/societies/context/user/db/impl/UserCtxDBMgr.java
@@ -29,6 +29,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.societies.api.context.model.CtxAssociation;
 import org.societies.api.context.model.CtxAttribute;
 import org.societies.api.context.model.CtxAttributeIdentifier;
@@ -52,6 +54,9 @@ import org.springframework.stereotype.Service;
  */
 @Service("userCtxDBMgr")
 public class UserCtxDBMgr implements IUserCtxDBMgr {
+	
+	/** The logging facility. */
+	private static final Logger LOG = LoggerFactory.getLogger(UserCtxDBMgr.class);
 
 	private final Map<CtxIdentifier, CtxModelObject> modelObjects;
 
