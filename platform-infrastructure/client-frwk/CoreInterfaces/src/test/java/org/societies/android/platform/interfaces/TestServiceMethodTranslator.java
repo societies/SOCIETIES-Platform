39d2b2be7217018163a12a2d01b09ef8eafa1fb6
Alan Walsh <awalsh@tssg.org>
2012-03-20 08:44:48 +0000

    Allow for parcelable

--- platform-infrastructure/client-frwk/CoreInterfaces/src/test/java/org/societies/android/platform/interfaces/TestServiceMethodTranslator.java
+++ platform-infrastructure/client-frwk/CoreInterfaces/src/test/java/org/societies/android/platform/interfaces/TestServiceMethodTranslator.java
@@ -7,12 +7,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestServiceMethodTranslator {
-	private static String methodsArray [] = {"getGreeting()", "getGreeting(String appendToMessage)", "getAnotherGreeting(String appendToMessage, int index)"};
+	private static String methodsArray [] = {"getGreeting()", 
+												"getGreeting(String appendToMessage)",
+												"getAnotherGreeting(String appendToMessage, int index)",
+												"getParcelable(String value, int number, org.societies.android.TestParcel parcel)"};
 	private static String METHOD_1 = "getGreeting";
 	private static String METHOD_2 = "getAnotherGreeting";
+	private static String METHOD_3 = "getParcelable";
 	private static String METHOD_NON_EXIST = "doesNotExist()";
 	private static String PARAM_1_NAME = "appendToMessage";
 	private static String PARAM_2_NAME = "index";
+	private static String PARAM_3_NAME = "value";
+	private static String PARAM_4_NAME = "number";
+	private static String PARAM_5_NAME = "parcel";
 	private static String PARAM_1_TYPE = "String";
 	private static String PARAM_2_TYPE = "int";
 	private static String PARAM_3_TYPE = "long";
@@ -22,6 +29,7 @@ public class TestServiceMethodTranslator {
 	private static String PARAM_7_TYPE = "char";
 	private static String PARAM_8_TYPE = "boolean";
 	private static String PARAM_9_TYPE = "short";
+	private static String PARAM_10_TYPE = "org.societies.android.TestParcel";
 	
 	private static String PARAM_1_TYPE_CAP = "String";
 	private static String PARAM_2_TYPE_CAP = "Int";
@@ -41,6 +49,7 @@ public class TestServiceMethodTranslator {
 		assertEquals(0, ServiceMethodTranslator.getMethodIndex(methodsArray, methodsArray[0]));
 		assertEquals(1, ServiceMethodTranslator.getMethodIndex(methodsArray, methodsArray[1]));
 		assertEquals(2, ServiceMethodTranslator.getMethodIndex(methodsArray, methodsArray[2]));
+		assertEquals(3, ServiceMethodTranslator.getMethodIndex(methodsArray, methodsArray[3]));
 		assertEquals(-1, ServiceMethodTranslator.getMethodIndex(methodsArray, METHOD_NON_EXIST));
 	}
 
@@ -49,11 +58,12 @@ public class TestServiceMethodTranslator {
 		assertEquals(METHOD_1, ServiceMethodTranslator.getMethodName(methodsArray, 0));
 		assertEquals(METHOD_1, ServiceMethodTranslator.getMethodName(methodsArray, 1));
 		assertEquals(METHOD_2, ServiceMethodTranslator.getMethodName(methodsArray, 2));
+		assertEquals(METHOD_3, ServiceMethodTranslator.getMethodName(methodsArray, 3));
 	}
 	
 	@Test(expected= org.societies.utilities.DBC.PreconditionException.class)
 	public void testGetInvalidMethodString() {
-		assertEquals(METHOD_2, ServiceMethodTranslator.getMethodName(methodsArray, 3));
+		assertEquals(METHOD_3, ServiceMethodTranslator.getMethodName(methodsArray, 4));
 	}
 	
 	@Test
@@ -61,6 +71,7 @@ public class TestServiceMethodTranslator {
 		assertEquals(0, ServiceMethodTranslator.getParameterNumber(methodsArray[0]));
 		assertEquals(1, ServiceMethodTranslator.getParameterNumber(methodsArray[1]));
 		assertEquals(2, ServiceMethodTranslator.getParameterNumber(methodsArray[2]));
+		assertEquals(3, ServiceMethodTranslator.getParameterNumber(methodsArray[3]));
 	}
 
 	
@@ -73,6 +84,10 @@ public class TestServiceMethodTranslator {
 		assertEquals(PARAM_1_NAME, ServiceMethodTranslator.getMethodParameterName(methodsArray[2], 0));
 		assertEquals(PARAM_2_NAME, ServiceMethodTranslator.getMethodParameterName(methodsArray[2], 1));
 		assertEquals(null, ServiceMethodTranslator.getMethodParameterName(methodsArray[2], 2));
+		assertEquals(PARAM_3_NAME, ServiceMethodTranslator.getMethodParameterName(methodsArray[3], 0));
+		assertEquals(PARAM_4_NAME, ServiceMethodTranslator.getMethodParameterName(methodsArray[3], 1));
+		assertEquals(PARAM_5_NAME, ServiceMethodTranslator.getMethodParameterName(methodsArray[3], 2));
+		assertEquals(null, ServiceMethodTranslator.getMethodParameterName(methodsArray[3], 3));
 	}
 
 	
@@ -81,6 +96,7 @@ public class TestServiceMethodTranslator {
 		assertEquals(0, ServiceMethodTranslator.getMethodParameterNames(methodsArray[0]).length);
 		assertEquals(1, ServiceMethodTranslator.getMethodParameterNames(methodsArray[1]).length);
 		assertEquals(2, ServiceMethodTranslator.getMethodParameterNames(methodsArray[2]).length);
+		assertEquals(3, ServiceMethodTranslator.getMethodParameterNames(methodsArray[3]).length);
 	}
 
 	@Test(expected= org.societies.utilities.DBC.PreconditionException.class)
@@ -94,6 +110,7 @@ public class TestServiceMethodTranslator {
 		assertEquals(0, ServiceMethodTranslator.getParameterClasses(methodsArray[0]).length);
 		assertEquals(1, ServiceMethodTranslator.getParameterClasses(methodsArray[1]).length);
 		assertEquals(2, ServiceMethodTranslator.getParameterClasses(methodsArray[2]).length);
+		assertEquals(3, ServiceMethodTranslator.getParameterClasses(methodsArray[3]).length);
 //		Class clazzes [] = ServiceMethodTranslator.getParameterClasses(methodsArray[2]);
 //		for (int i = 0; i < clazzes.length; i++) {
 //			System.out.println("Class: " + clazzes[i].getCanonicalName());
@@ -106,6 +123,7 @@ public class TestServiceMethodTranslator {
 		assertEquals(PARAM_2_TYPE_CAP, ServiceMethodTranslator.capitaliseString(PARAM_2_TYPE));
 		assertEquals(PARAM_3_TYPE_CAP, ServiceMethodTranslator.capitaliseString(PARAM_3_TYPE));
 		assertEquals(PARAM_4_TYPE_CAP, ServiceMethodTranslator.capitaliseString(PARAM_4_TYPE));
+		assertEquals(PARAM_10_TYPE, ServiceMethodTranslator.capitaliseString(PARAM_10_TYPE));
 	}
 	
 	@Test
@@ -113,6 +131,9 @@ public class TestServiceMethodTranslator {
 		assertEquals(PARAM_1_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[1], 0));
 		assertEquals(PARAM_1_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[2], 0));
 		assertEquals(PARAM_2_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[2], 1));
+		assertEquals(PARAM_1_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[3], 0));
+		assertEquals(PARAM_2_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[3], 1));
+		assertEquals(PARAM_10_TYPE, ServiceMethodTranslator.getMethodParameterType(methodsArray[3], 2));
 
 		assertEquals(null, ServiceMethodTranslator.getMethodParameterType(methodsArray[0], 0));
 	}
@@ -121,24 +142,21 @@ public class TestServiceMethodTranslator {
 		assertEquals(PARAM_1_TYPE_CAP, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[1], 0));
 		assertEquals(PARAM_1_TYPE_CAP, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[2], 0));
 		assertEquals(PARAM_2_TYPE_CAP, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[2], 1));
+		assertEquals(PARAM_1_TYPE_CAP, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[3], 0));
+		assertEquals(PARAM_2_TYPE_CAP, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[3], 1));
+		assertEquals(PARAM_10_TYPE, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[3], 2));
 
-		assertEquals(null, ServiceMethodTranslator.getMethodParameterTypeCapitalised(methodsArray[0], 0));
 	}
 	@Test
 	public void testGetMethodParameterTypes() {
 		assertEquals(0, ServiceMethodTranslator.getMethodParameterTypes(methodsArray[0]).length);
 		assertEquals(1, ServiceMethodTranslator.getMethodParameterTypes(methodsArray[1]).length);
 		assertEquals(2, ServiceMethodTranslator.getMethodParameterTypes(methodsArray[2]).length);
+		assertEquals(3, ServiceMethodTranslator.getMethodParameterTypes(methodsArray[3]).length);
 //		String types [] = ServiceMethodTranslator.getMethodParameterTypes(methodsArray[2]);
 //		for (int i = 0; i < types.length; i++) {
 //			System.out.println("Types: " + types[i]);
 //		}
-	}
-	@Test
-	public void testGetMethodParameterTypesCapitalised() {
-		assertEquals(0, ServiceMethodTranslator.getMethodParameterTypesCapitalised(methodsArray[0]).length);
-		assertEquals(1, ServiceMethodTranslator.getMethodParameterTypesCapitalised(methodsArray[1]).length);
-		assertEquals(2, ServiceMethodTranslator.getMethodParameterTypesCapitalised(methodsArray[2]).length);
 	}
 	
 	@Test
