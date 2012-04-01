0c52a1aca6d4e9e206d86522661964570160dbc7
dingqi <yangdinqgi@gmail.com>
2012-04-01 22:25:06 +0200

    add foursquare SNS connector

---
+++ platform-infrastructure/sns/FouraquareConnector/src/main/java/org/societies/platform/FoursquareConnector/FoursquareConnector.java
@@ -0,0 +1,15 @@
+package org.societies.platform.FoursquareConnector;
+
+import org.societies.api.internal.sns.ISocialConnector;
+
+
+public interface FoursquareConnector extends ISocialConnector {
+	
+	public static final String USER_PROFILE = "https://api.foursquare.com/v2/users/self?oauth_token=";
+	public static final String RECENT_CHECKINS = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=";
+	
+	/**
+	 * @return
+	 */
+	String getRecentCheckins();
+}
---
+++ platform-infrastructure/sns/FouraquareConnector/src/main/java/org/societies/platform/FoursquareConnector/exeptions/MissingTokenExeptions.java
@@ -0,0 +1,5 @@
+package org.societies.platform.FoursquareConnector.exeptions;
+
+public class MissingTokenExeptions extends Exception{
+	
+}
