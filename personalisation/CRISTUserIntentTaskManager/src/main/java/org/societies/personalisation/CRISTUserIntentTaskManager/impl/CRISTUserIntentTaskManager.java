e6a112f5c478eed32d81f4ce68786999c28eb714
Zhu WANG <Zhu WANG@157.159.160.201>
2012-04-03 15:13:50 +0200

    Implemented the situation learning algorithm of CRIST

--- personalisation/CRISTUserIntentTaskManager/src/main/java/org/societies/personalisation/CRISTUserIntentTaskManager/impl/CRISTUserIntentTaskManager.java
+++ personalisation/CRISTUserIntentTaskManager/src/main/java/org/societies/personalisation/CRISTUserIntentTaskManager/impl/CRISTUserIntentTaskManager.java
@@ -42,9 +42,12 @@ import org.societies.personalisation.CRIST.api.model.CRISTUserTaskModelData;
 public class CRISTUserIntentTaskManager implements ICRISTUserIntentTaskManager {
 
 	private LinkedHashMap<String, Integer> intentModel = null;
+	private LinkedHashMap<String, Integer> situationModel = null;
+	private ArrayList<String> registeredContext = new ArrayList<String>();
 
 	private HashMap<IIdentity, CRISTUserAction> currentUserActionMap = new HashMap<IIdentity, CRISTUserAction>();
 	private HashMap<IIdentity, CRISTUserSituation> currentUserSituationMap = new HashMap<IIdentity, CRISTUserSituation>();
+	private HashMap<IIdentity, ArrayList<String>> currentUserContextMap = new HashMap<IIdentity, ArrayList<String>>();
 
 	private ICRISTUserIntentPrediction cristPrediction;
 	private ICRISTUserIntentDiscovery cristDiscovery;
@@ -113,52 +116,179 @@ public class CRISTUserIntentTaskManager implements ICRISTUserIntentTaskManager {
 
 		ArrayList<String> historyAction = new ArrayList<String>();
 		ArrayList<String> historySituation = new ArrayList<String>();
-
+		ArrayList<ArrayList<String>> historyContext = new ArrayList<ArrayList<String>>();
+		// Assuming that the following sensors are available: light, sound, temperature, gps
+		this.registeredContext.add("Light");
+		this.registeredContext.add("Sound");
+		this.registeredContext.add("Temperature");
+		this.registeredContext.add("GPS");
+		
+		ArrayList<String> historyContextClique = new ArrayList<String>();
+		
 		// Mock history data
 		historyAction.add("Turn on MP3 Player");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn down volume");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Switch songs");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn off MP3 Player");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Start the GPS navigator");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Input a location name");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Close the GPS navigator");
 		historySituation.add("Shopping Mall");
+		historyContextClique.add("100");
+		historyContextClique.add("80");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn on MP3 Player");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn up volume");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Switch songs");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn off MP3 Player");
 		historySituation.add("Office");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("26");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Start the GPS navigator");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Input a location name");
 		historySituation.add("Outdoor");
+		historyContextClique.add("120");
+		historyContextClique.add("60");
+		historyContextClique.add("15");
+		historyContextClique.add("55,1.33");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Close the GPS navigator");
 		historySituation.add("Shopping Mall");
+		historyContextClique.add("100");
+		historyContextClique.add("80");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn on MP3 Player");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn down volume");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Switch songs");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn off MP3 Player");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn on MP3 Player");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
+		
 		historyAction.add("Turn down volume");
 		historySituation.add("Study Hall");
+		historyContextClique.add("100");
+		historyContextClique.add("30");
+		historyContextClique.add("22");
+		historyContextClique.add("N/A");
+		historyContext.add(historyContextClique);
 
 		for (int i = 0; i < historyAction.size(); i++) {
 			MockHistoryData currentHisData = new MockHistoryData(
-					historyAction.get(i), historySituation.get(i), null);
+					historyAction.get(i), historySituation.get(i), historyContext.get(i));
 			this.historyList.add(currentHisData);
 		}
 	}
@@ -244,6 +374,15 @@ public class CRISTUserIntentTaskManager implements ICRISTUserIntentTaskManager {
 		return this.currentUserSituationMap.get(entityID);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.societies.personalisation.CRIST.api.CRISTUserIntentTaskManager.ICRISTUserIntentTaskManager#getCurrentUserContext(org.societies.api.identity.IIdentity)
+	 */
+	@Override
+	public ArrayList<String> getCurrentUserContext(IIdentity entityID) {
+		// TODO Auto-generated method stub
+		return this.currentUserContextMap.get(entityID);
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -259,19 +398,63 @@ public class CRISTUserIntentTaskManager implements ICRISTUserIntentTaskManager {
 		// TODO: Identify the user's current situation based on her real-time context
 		// Retrieve the user's real-time context: How to retrieve a given user's context
 		// this.ctxBroker.retrieve();
+		int currentPos = this.historyList.size();
+		ArrayList<String> lastContextClique = this.historyList.get(currentPos).getContext();
+		ArrayList<String> currentContextClique = new ArrayList<String>();
 		
+		// Generate a new context clique based on the newly received ctxAttribute
+		String contextID = ctxAttribute.getSourceId();
+		for (int i=0;i<lastContextClique.size();i++){
+			String oneContextID = this.registeredContext.get(i);
+			if (oneContextID.equals(contextID)){
+				currentContextClique.add(ctxAttribute.getStringValue());
+			}else{
+				currentContextClique.add(lastContextClique.get(i));
+			}
+		}
 		
-		CRISTUserSituation userSituation = new CRISTUserSituation();
+		CRISTUserSituation userSituation = inferUserSituation(currentContextClique);
+		
+		// CRISTUserSituation userSituation = new CRISTUserSituation();
 		// MOCK
-		userSituation.setSituationID("Outdoor");
+		// userSituation.setSituationID("Outdoor");
 		if (this.currentUserSituationMap.containsKey(entityID)){
 			this.currentUserSituationMap.remove(entityID);
 			this.currentUserSituationMap.put(entityID, userSituation);
 		}else{
 			this.currentUserSituationMap.put(entityID, userSituation);
 		}
+		if (this.currentUserContextMap.containsKey(entityID)){
+			this.currentUserContextMap.remove(entityID);
+			this.currentUserContextMap.put(entityID, currentContextClique);
+		}else{
+			this.currentUserContextMap.put(entityID, currentContextClique);
+		}
 	}
 
+	private CRISTUserSituation inferUserSituation(ArrayList<String> contextClique){
+		double currentLight = Double.parseDouble(contextClique.get(1));
+		double currentSound = Double.parseDouble(contextClique.get(2));
+		double currentTemp = Double.parseDouble(contextClique.get(3));
+		String currentGPS = contextClique.get(4);
+		
+		// Currently we simply use predefined rules to infer the situation, a more valid way should 
+		// be constructing a situation-model based on the history context based supervised learning mechanism 
+		CRISTUserSituation situation = new CRISTUserSituation();
+		if (currentLight>80&&currentLight<120&&currentSound<50&&currentTemp>16&&currentTemp<24&&currentGPS.equals("N/A")){
+			situation.setSituationID("Study Hall");
+		}else if(!currentGPS.equals("N/A")){
+			situation.setSituationID("Outdoor");
+		}else if(currentLight>100&&currentSound>60&&currentTemp>22&&currentGPS.equals("N/A")){
+			situation.setSituationID("Shopping Mall");
+		}else if(currentLight>80&&currentSound<50&&currentTemp>20&&currentTemp<28&&currentGPS.equals("N/A")){
+			situation.setSituationID("Office");
+		}else{
+			situation.setSituationID("Home");
+		}
+		
+		return situation;
+	}
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -316,6 +499,11 @@ public class CRISTUserIntentTaskManager implements ICRISTUserIntentTaskManager {
 		}
 
 		CRISTUserSituation currentUserSituation = getCurrentUserSituation(entityID);
+		ArrayList<String> currentContextClique = getCurrentUserContext(entityID);
+		MockHistoryData oneHisData = new MockHistoryData(userAction.getActionID(),
+				currentUserSituation.getSituationID(),currentContextClique);
+		
+		this.historyList.add(oneHisData);
 		// Predict user intent based on one's current action
 		ArrayList<CRISTUserAction> results = getNextActions(entityID,
 				userAction, currentUserSituation);
