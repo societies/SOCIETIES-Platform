23f1379559747fe22b0679ff265e05b8088c2c0b
Alan Walsh <awalsh@tssg.org>
2012-04-03 17:26:55 +0100

    Android Comms documentation

--- platform-infrastructure/client-frwk/MasterGUI/src/org/societies/android/platform/phongegap/PluginCoreServiceMonitor.java
+++ platform-infrastructure/client-frwk/MasterGUI/src/org/societies/android/platform/phongegap/PluginCoreServiceMonitor.java
@@ -1,3 +1,28 @@
+/**
+Copyright (c) 2011, SOCIETIES Consortium (WATERFORD INSTITUTE OF TECHNOLOGY (TSSG), HERIOT-WATT UNIVERSITY (HWU), SOLUTA.NET 
+
+(SN), GERMAN AEROSPACE CENTRE (Deutsches Zentrum fuer Luft- und Raumfahrt e.V.) (DLR), Zavod za varnostne tehnologije
+informacijske družbe in elektronsko poslovanje (SETCCE), INSTITUTE OF COMMUNICATION AND COMPUTER SYSTEMS (ICCS), LAKE
+COMMUNICATIONS (LAKE), INTEL PERFORMANCE LEARNING SOLUTIONS LTD (INTEL), PORTUGAL TELECOM INOVAÇÃO, SA (PTIN), IBM Corp (IBM),
+INSTITUT TELECOM (ITSUD), AMITEC DIACHYTI EFYIA PLIROFORIKI KAI EPIKINONIES ETERIA PERIORISMENIS EFTHINIS (AMITEC), TELECOM 
+ITALIA S.p.a.(TI), TRIALOG (TRIALOG), Stiftelsen SINTEF (SINTEF), NEC EUROPE LTD (NEC))
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.societies.android.platform.phongegap;
 
 import java.util.HashMap;
@@ -42,6 +67,11 @@ public class PluginCoreServiceMonitor extends Plugin {
 	private static final String LOG_TAG = PluginCoreServiceMonitor.class.getName();
 	private static final String CLASSNAME_SEPARATOR = "\\.";
 
+	/**
+	 * Actions required to bind and unbind to any Android service(s) 
+	 * required by this plugin. It is imperative that dependent 
+	 * services are binded to before invoking invoking methods.
+	 */
 	private static final String CONNECT_SERVICE = "connectService";
 	private static final String DISCONNECT_SERVICE = "disconnectService";
 	
@@ -336,7 +366,11 @@ public class PluginCoreServiceMonitor extends Plugin {
     	}
     	return retValue;
     }
-
+    /**
+     * Extract a service or activity from its class name
+     * @param className
+     * @return String
+     */
     private String extractServiceName(String className) {
     	Log.d(LOG_TAG, "extractService for class: " + className );
     	String serviceName = className;
