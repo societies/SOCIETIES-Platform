57422db02fa071991b2475254670ab39e1df8fa4
Guido Spadotto <gspadotto@soluta.net>
2012-04-04 16:02:46 +0200

    Added Annotations for handling required-provided interfaces

--- api/java/external/src/main/java/org/societies/api/identity/IIdentity.java
+++ api/java/external/src/main/java/org/societies/api/identity/IIdentity.java
@@ -1,9 +1,17 @@
 package org.societies.api.identity;
 
+import org.societies.utilities.annotations.SocietiesExternalInterface;
+import org.societies.utilities.annotations.SocietiesExternalInterface.SocietiesInterfaceType;
+
+@SocietiesExternalInterface(type = SocietiesInterfaceType.PROVIDED)
 public interface IIdentity {
 	String getIdentifier();
+
 	String getDomain();
+
 	IdentityType getType();
+
 	String getJid();
+
 	String getBareJid();
 }
--- api/java/external/src/main/java/org/societies/api/mock/ServiceResourceIdentifier.java
+++
@@ -1,8 +0,0 @@
-package org.societies.api.mock;
-/**Use org.societies.api.internal.servicelifecycle.model.ServiceResourceIdentifier*/
-@Deprecated 
-public class ServiceResourceIdentifier {
-	public String toUriString() {
-		return "";
-	}
-}
