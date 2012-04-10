162d8f6b02f9abce7b0b616217f15b22f0c79344
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2012-04-10 09:24:22 +0200

    trying to wire the persistance beans differently

--- cis-modules/cis-persistance/src/main/java/org/societies/cis/persistance/PersistanceManager.java
+++ cis-modules/cis-persistance/src/main/java/org/societies/cis/persistance/PersistanceManager.java
@@ -3,16 +3,20 @@ package org.societies.cis.persistance;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PersistanceManager implements IPersistanceManager {
 	private EntityManager em = null;
 	@Autowired
-	public PersistanceManager(EntityManager em){
-		this.em = em;
+	public PersistanceManager(){
+		LocalContainerEntityManagerFactoryBean lcemfb;
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.societies.cis.persistance.IPersistanceManager#persist(java.lang.Object)
+	 */
 	/* (non-Javadoc)
 	 * @see org.societies.cis.persistance.IPersistanceManager#persist(java.lang.Object)
 	 */
