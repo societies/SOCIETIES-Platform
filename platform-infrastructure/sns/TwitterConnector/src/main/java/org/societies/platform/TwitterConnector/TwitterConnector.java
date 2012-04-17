eafdcb92040e9548f97f61970d362ab0c7aa503b
dingqi <yangdinqgi@gmail.com>
2012-04-17 14:49:03 +0300

    add twitter user profile search api

--- platform-infrastructure/sns/TwitterConnector/src/main/java/org/societies/platform/TwitterConnector/TwitterConnector.java
+++ platform-infrastructure/sns/TwitterConnector/src/main/java/org/societies/platform/TwitterConnector/TwitterConnector.java
@@ -10,9 +10,15 @@ public interface TwitterConnector extends ISocialConnector {
 	public static final String GET_USERINFO_URL = "http://api.twitter.com/1/users/lookup.json?user_id=";
 	public static final String GET_FRIENDS_URL = "https://api.twitter.com/1/friends/ids.json?user_id=";
 	public static final String GET_FOLLOWERS_URL = "https://api.twitter.com/1/followers/ids.json?user_id=";
+	public static final String GET_OTHER_PROFILE_URL = "https://api.twitter.com/1/users/lookup.json?user_id=";
 	
 	/**
 	 * @return
 	 */
 	String getUserFollowers();
+	/**
+	 * @param id
+	 * @return
+	 */
+	String getOtherProfile(String id);
 }
