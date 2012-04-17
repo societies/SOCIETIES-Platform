8ecdcb261003ceee3ef816405ea4dbe9197a5ce4
Joao M. Goncalves <jmgonc@gmail.com>
2012-04-17 16:52:19 +0300

    interface updates based on test cases

---
+++ api/java/internal/src/main/java/org/societies/api/internal/comm/ICommManagerController.java
@@ -0,0 +1,18 @@
+package org.societies.api.internal.comm;
+
+import java.util.Set;
+
+import org.societies.api.comm.xmpp.exceptions.XMPPError;
+import org.societies.api.identity.INetworkNode;
+
+public interface ICommManagerController {
+	// Simple Identity Methods
+	INetworkNode newMainIdentity(String identifier, String domain, String password) throws XMPPError; // TODO this takes no credentials in a private/public key case
+	INetworkNode login(String identifier, String domain, String password);
+	INetworkNode loginFromConfig();
+	boolean logout();
+	boolean destroyMainIdentity();
+	
+	// CSS Node Discovery
+	Set<INetworkNode> getOtherNodes();
+}
