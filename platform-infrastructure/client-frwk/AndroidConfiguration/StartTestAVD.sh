445714150728bcd91d0617ad6da3ed1c96715b44
Alan Walsh <awalsh@tssg.org>
2012-04-18 13:26:49 +0100

    Android scripts

---
+++ platform-infrastructure/client-frwk/AndroidConfiguration/StartTestAVD.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Start curr ent AVD 
+
+testParams() {
+	if [ $1 -eq 1 ]
+	then
+ 		return 0 
+	else
+  		if [ $1 -eq 0 ]
+    		then
+      			echo "AVD name must be specified"
+    		else
+      			echo "Too many parameters"
+  		fi
+		return 1
+	fi
+	
+}
+
+startAVD() {
+	echo ""
+	echo "Start the test emulator: $1"
+	echo "-----------------------"
+	emulator -avd $1 -partition-size 128
+
+	if [ $? -ne 0 ]
+ 	then
+		echo -e "\nIncorrect AVD name"
+		return 1
+        fi
+
+}
+
+testParams $#
+if [ $?  -eq 0 ] 
+ then
+ startAVD $1	
+ if [ $? -ne 0 ]
+  then
+	exit 1
+ fi
+fi
+
+
+exit 0
