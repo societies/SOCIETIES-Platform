c246a3170b4cb7d99e9f18536b260d674796692e
Luca Lamorte <lucasimone@gmail.com>
2012-05-04 14:47:33 +0200

    Commit old update in the Content Provider Table definition in CoreInterface

--- platform-infrastructure/client-frwk/ContentProvider/src/org/societies/clientframework/contentprovider/services/SocietiesCP.java
+++ platform-infrastructure/client-frwk/ContentProvider/src/org/societies/clientframework/contentprovider/services/SocietiesCP.java
@@ -1,8 +1,11 @@
 package org.societies.clientframework.contentprovider.services;
 
 import org.societies.android.platform.interfaces.IContentProvider;
-import org.societies.api.android.internal.model.CredentialTable;
-import org.societies.api.android.internal.model.ServiceTable;
+import org.societies.api.android.internal.tables.CredentialTable;
+import org.societies.api.android.internal.tables.CssNodeTable;
+import org.societies.api.android.internal.tables.CssNodesTable;
+import org.societies.api.android.internal.tables.CssProfileTable;
+import org.societies.api.android.internal.tables.ServiceTable;
 
 import android.content.ContentProvider;
 import android.content.ContentUris;
@@ -35,6 +38,8 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
     
     private static final int CREDENTIAL_ID = 1;
     private static final int SERVICE_ID    = 2;
+    private static final int PROFILE_CSS   = 3;
+    private static final int NODE_CSS	   = 4;
     
 	
 	// Holds the database object
@@ -45,6 +50,8 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
     	sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 		sURIMatcher.addURI(AUTHORITY, CredentialTable.TABLE_NAME, CREDENTIAL_ID);
 		sURIMatcher.addURI(AUTHORITY, ServiceTable.TABLE_NAME, 	  SERVICE_ID);
+		sURIMatcher.addURI(AUTHORITY, CssProfileTable.TABLE_NAME, PROFILE_CSS);
+		sURIMatcher.addURI(AUTHORITY, CssNodeTable.TABLE_NAME, 	  NODE_CSS);
 		
 		// Add other URI to be user to select more tables....
 		// .....
@@ -54,6 +61,8 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 	public synchronized int delete(Uri uri, String where, String[] whereArgs) {
 		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 		
+		
+	
 		int count;
         switch (sURIMatcher.match(uri)) {
         case CREDENTIAL_ID:
@@ -63,7 +72,16 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
         case SERVICE_ID:
             count = db.delete(ServiceTable.TABLE_NAME, where, whereArgs);
             break;
-
+            
+        case PROFILE_CSS:
+        	count = db.delete(CssProfileTable.TABLE_NAME, where, whereArgs);
+        	break;
+        
+        case NODE_CSS:
+        	count = db.delete(CssNodesTable.TABLE_NAME_NODES, where, whereArgs);
+        	
+        	break;
+        	
         default:
             throw new IllegalArgumentException("Unknown URI " + uri);
         }
@@ -76,12 +94,19 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 	@Override
 	public String getType(Uri uri) {
 		 switch (sURIMatcher.match(uri)) {
-	        case CREDENTIAL_ID:
+	      
+		    case CREDENTIAL_ID:
 	            return BASE_PATH + "/" + CredentialTable.TABLE_NAME;
 
 	        case SERVICE_ID:
 	             return BASE_PATH + "/" + ServiceTable.TABLE_NAME;
 
+	        case PROFILE_CSS:
+	        	return BASE_PATH + "/" + CssProfileTable.TABLE_NAME;
+	        	
+	        case NODE_CSS:
+	        	return BASE_PATH + "/" + CssNodeTable.TABLE_NAME;
+	             
 	        default:
 	            throw new IllegalArgumentException("Unknown URI " + uri);
 	        }
@@ -92,7 +117,7 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 		
 		
 		db = mOpenHelper.getWritableDatabase();
-		long rowId;
+		long rowId=0;
 		
 		switch(sURIMatcher.match(uri)){
 		case CREDENTIAL_ID:// Set the table
@@ -101,6 +126,15 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 			case SERVICE_ID:// Set the table
 				rowId = db.insert(ServiceTable.TABLE_NAME, null, values);
 			break;
+				
+			case PROFILE_CSS: 
+				rowId = db.insert(CssProfileTable.TABLE_NAME, null, values);
+			break;
+			case NODE_CSS:
+				rowId = db.insert(CssNodeTable.TABLE_NAME, null, values);
+			
+			break;
+			
 			default:  throw new SQLException("Failed to insert row into " + uri);
 		}
         
@@ -144,6 +178,18 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 				Log.v("SocietiesCP", "Set Table "+ServiceTable.TABLE_NAME);
 			break;
 			
+			case NODE_CSS: 
+				 qb.setTables(CssNodeTable.TABLE_NAME);
+				 Log.v("SocietiesCP", "Set Table "+CssNodeTable.TABLE_NAME);
+			break;
+			
+			case PROFILE_CSS:
+				
+				 qb.setTables(CssProfileTable.TABLE_NAME);
+				 Log.v("SocietiesCP", "Set Table "+CssProfileTable.TABLE_NAME);
+			break;
+			
+			
 		}
 		
 		// Apply the query to the underlying database. 
@@ -168,6 +214,12 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 			break;
 		case SERVICE_ID:
 			count = db.update(ServiceTable.TABLE_NAME, values, where, whereArgs);
+			
+		case NODE_CSS:
+			count = db.update(CssNodeTable.TABLE_NAME, values, where, whereArgs);
+			
+		case PROFILE_CSS:
+			count = db.update(CssProfileTable.TABLE_NAME, values, where, whereArgs);
 		
 		default:
 			throw new IllegalArgumentException("Unknown URI: " + uri);
@@ -203,6 +255,9 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 	    	
 	        db.execSQL(CredentialTable.CREATE_CREDENTIAL_TABLE);
 	        db.execSQL(ServiceTable.CREATE_SERVICE_TABLE);
+	        db.execSQL(CssNodeTable.CREATE_CSS_NODE_TABLE);
+	        db.execSQL(CssProfileTable.CREATE_CSS_PROFILE_TABLE);
+	        
 	    }
 
 		@Override
@@ -212,6 +267,12 @@ public class SocietiesCP extends ContentProvider implements IContentProvider {
 			
 			onCreate(db);
 		}
+		
+		
+		
 	}
+	
+	
+	
 
 }
