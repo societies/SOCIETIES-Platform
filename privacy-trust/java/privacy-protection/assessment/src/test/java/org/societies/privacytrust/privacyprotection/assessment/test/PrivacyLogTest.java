04707eea1728a6ee3a801bd9e7610a2d694e3146
Mitja Vardjan <mitja.vardjan@setcce.si>
2012-05-22 18:06:51 +0200

    junit test for search engine

--- privacy-trust/java/privacy-protection/assessment/src/test/java/org/societies/privacytrust/privacyprotection/assessment/test/PrivacyLogTest.java
+++ privacy-trust/java/privacy-protection/assessment/src/test/java/org/societies/privacytrust/privacyprotection/assessment/test/PrivacyLogTest.java
@@ -28,6 +28,7 @@ package org.societies.privacytrust.privacyprotection.assessment.test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -35,6 +36,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.societies.api.identity.IIdentity;
+import org.societies.api.internal.privacytrust.privacyprotection.model.privacyassessment.ChannelType;
+import org.societies.api.internal.privacytrust.privacyprotection.model.privacyassessment.DataAccessLogEntry;
 import org.societies.api.internal.privacytrust.privacyprotection.model.privacyassessment.DataTransmissionLogEntry;
 import org.societies.api.internal.privacytrust.privacyprotection.model.privacyassessment.PrivacyLogFilter;
 import org.societies.privacytrust.privacyprotection.assessment.log.PrivacyLog;
@@ -75,17 +79,113 @@ public class PrivacyLogTest {
 		List<DataTransmissionLogEntry> result;
 		
 		result = privacyLog.getAll();
-		//assertNotNull(result);
+		assertNotNull(result);
+		result = privacyLog.getDataTransmission();
+		assertNotNull(result);
+	}
+
+	@Test
+	public void testGetDataAccess() {
+		
+		LOG.debug("testGetDataAccess()");
+		
+		int size1;
+		int size2;
+		
+		DataAccessLogEntry entry = new DataAccessLogEntry(new Date(), null, "", null, 100);
+		
+		size1 = privacyLog.getDataAccess().size();
+		privacyLog.append(entry);
+		size2 = privacyLog.getDataAccess().size();
+		assertEquals(size1 + 1, size2);
 	}
 
 	@Test
 	public void testSearch() {
 		
-		PrivacyLogFilter filter = new PrivacyLogFilter();
-		
+		PrivacyLogFilter filter;
 		List<DataTransmissionLogEntry> result;
+
+		int initialSize = privacyLog.getAll().size();
 		
+		// TODO: use different implementations when available (Redmine #1124)
+		IIdentity receiver1 = mock(IIdentity.class);
+		IIdentity sender1 = mock(IIdentity.class);
+		IIdentity receiver2 = mock(IIdentity.class);
+		IIdentity sender2 = mock(IIdentity.class);
+		
+		Date date1 = new Date(1000000);
+		Date date2 = new Date(2000000);
+		Date date3 = new Date(3000000);
+		
+		DataTransmissionLogEntry entry1 = new DataTransmissionLogEntry(
+				"dataType1", date1, receiver1, sender1, "senderClass1", 861, ChannelType.XMPP);
+		DataTransmissionLogEntry entry2 = new DataTransmissionLogEntry(
+				"dataType1", date2, receiver2, sender1, "senderClass1", 691, ChannelType.XMPP);
+		DataTransmissionLogEntry entry3 = new DataTransmissionLogEntry(
+				"dataType2", date3, receiver2, sender2, "senderClass2", 121, ChannelType.FACEBOOK);
+		
+		privacyLog.append(entry1);
+		privacyLog.append(entry2);
+		privacyLog.append(entry3);
+		
+		// Filters with one single parameter set to non-null
+		
+		filter = new PrivacyLogFilter();
 		result = privacyLog.search(filter);
-		//assertNotNull(result);
+		assertEquals(initialSize + 3, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setChannelId(new ChannelType[] {ChannelType.XMPP} );
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 2, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setDataType(new String[] {"dataType1"});
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 2, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setEnd(new Date(1500000));
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 1, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setReceiver(receiver1);
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 1, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setSender(sender1);
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 2, result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setSenderClass("senderClass1");
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 2, result.size());
+		
+//		filter = new PrivacyLogFilter();
+//		filter.setSentToGroup(true);
+//		result = privacyLog.search(filter);
+//		assertEquals(initialSize + , result.size());
+		
+//		filter = new PrivacyLogFilter();
+//		filter.setSentToLocalCss(false);
+//		result = privacyLog.search(filter);
+//		assertEquals(initialSize + , result.size());
+		
+		filter = new PrivacyLogFilter();
+		filter.setStart(new Date(1500000));
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 2, result.size());
+		
+		// Filters with 2 or more parameters
+		
+		filter = new PrivacyLogFilter();
+		filter.setSenderClass("senderClass1");
+		filter.setStart(new Date(1500000));
+		result = privacyLog.search(filter);
+		assertEquals(initialSize + 1, result.size());
 	}
 }
