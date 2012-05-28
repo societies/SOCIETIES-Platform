e152fa0a423aa7008971aa1be871bcd853b12b9f
Mitja Vardjan <mitja.vardjan@setcce.si>
2012-05-28 15:50:09 +0200

    javadoc

--- api/java/internal/src/main/java/org/societies/api/internal/security/policynegotiator/INegotiation.java
+++ api/java/internal/src/main/java/org/societies/api/internal/security/policynegotiator/INegotiation.java
@@ -25,6 +25,8 @@
 package org.societies.api.internal.security.policynegotiator;
 
 import org.societies.api.identity.Requestor;
+import org.societies.api.identity.RequestorCis;
+import org.societies.api.identity.RequestorService;
 import org.societies.api.internal.privacytrust.privacyprotection.IPrivacyPolicyNegotiationManager;
 
 /**
@@ -40,7 +42,10 @@ public interface INegotiation {
 	/**
 	 * Start policy negotiation procedure.
 	 * 
-	 * @param provider Includes identity of the service provider and service ID comprehendable by the provider
+	 * @param provider Includes identity of the service provider and service ID comprehendable by the provider.
+	 * This should be an instance of either:<br/>
+	 * - {@link RequestorCis} if this negotiation is about joining a CIS, or<br/>
+	 * - {@link RequestorService} if this negotiation is about using a new service
 	 * 
 	 * @param includePrivacyPolicyNegotiation True to perform also Privacy Policy Negotiation using
 	 * {@link IPrivacyPolicyNegotiationManager}.
