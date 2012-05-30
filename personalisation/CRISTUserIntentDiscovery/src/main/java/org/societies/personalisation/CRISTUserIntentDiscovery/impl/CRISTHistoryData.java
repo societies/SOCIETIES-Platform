4b5d07ecda43c30f191c7ec52b87aa198566ca0b
Zhiyong <Zhiyong.Yu@it-sudparis.eu>
2012-05-30 08:27:54 +0200

    update CRISTUserIntentTaskManager, especially inferUserSituation method

--- personalisation/CRISTUserIntentDiscovery/src/main/java/org/societies/personalisation/CRISTUserIntentDiscovery/impl/CRISTHistoryData.java
+++ personalisation/CRISTUserIntentDiscovery/src/main/java/org/societies/personalisation/CRISTUserIntentDiscovery/impl/CRISTHistoryData.java
@@ -18,6 +18,7 @@ import org.societies.api.internal.context.broker.ICtxBroker;
 import org.societies.api.personalisation.model.Action;
 import org.societies.api.personalisation.model.IAction;
 import org.societies.api.schema.servicelifecycle.model.ServiceResourceIdentifier;
+import org.societies.personalisation.CRIST.api.model.CRISTUserSituation;
 
 
 /**
@@ -30,97 +31,34 @@ public class CRISTHistoryData {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CRISTHistoryData.class);
 	
-	public static ServiceResourceIdentifier serviceId_music;
-	public static ServiceResourceIdentifier serviceId_checkin;
-	
-	private ArrayList<String> context = new ArrayList<String>(); //context = ["100","30","22","N/A"]
 	private IAction action = null;
-	private String situationValue = null;
+	private CRISTUserSituation situation = null;
 	
 
-	public CRISTHistoryData(IAction action, String situation, ArrayList<String> context) {
+	public CRISTHistoryData(IAction action, CRISTUserSituation situation) {
 			
 		this.action = action;
-		this.situationValue = situation;
-		this.context = context;
+		this.situation = situation;
 		LOG.info("New CRISTHistoryData: " + this.toString());
-	}
-
-	public ArrayList<String> getContext() {
-		return context;
-	}
-
-	public void setContext(ArrayList<String> context) {
-		this.context = context;
-	}
-	
-	public String getActionValue() {
-		return action.getparameterName()+ " " + action.getvalue();
 	}
 
 	public IAction getAction() {
 		return action;
 	}
 
-	public void setActionValue(IAction action) {
+	public void setAction(IAction action) {
 		this.action = action;
 	}
 
-	public String getSituationValue() {
-		return situationValue;
+	public CRISTUserSituation getSituation() {
+		return situation;
 	}
 	
-	public void setSituationValue(String situationValue) {
-		this.situationValue = situationValue;
+	public void setSituation(CRISTUserSituation situation) {
+		this.situation = situation;
 	}
 	
-	public String toString() {
-		String string = getActionValue() + " " + this.situationValue + " " + this.context;
-		return string;
-	}
-	
-	public static ServiceResourceIdentifier getServiceId_music()
-	{
-		if (serviceId_music != null)
-		{
-			return serviceId_music;
-		}
-		serviceId_music = new ServiceResourceIdentifier();
-		try {
-			serviceId_music.setIdentifier(new URI("http://testService_music"));
-			return serviceId_music;
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public static ServiceResourceIdentifier getServiceId_checkin()
-	{
-		if (serviceId_checkin != null)
-		{
-			return serviceId_checkin;
-		}
-		serviceId_checkin = new ServiceResourceIdentifier();
-		try {
-			serviceId_checkin.setIdentifier(new URI("http://testService_checkin"));
-			return serviceId_checkin;
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public static ArrayList<String> getRegisteredContext()
-	{
-		ArrayList<String> registeredContext = new ArrayList<String>();
-		registeredContext.add("Light");
-		registeredContext.add("Sound");
-		registeredContext.add("Temperature");
-		registeredContext.add("GPS");
-		return registeredContext;
 
-	}
 	
 	
 	
