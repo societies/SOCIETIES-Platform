ab6573ef7b61d4e102f6bb97598455b2eeffbd16
nikosk <nikosk@cn.ntua.gr>
2012-05-31 18:17:57 +0300

    test 1109 updates caui prediction improvements

--- integration-tests/BIT/proactive-personalisation-test-suite/trigger-caui-prediction#1109/src/main/java/org/societies/integration/test/bit/caui_prediction/RetrieveLearnedModelTest.java
+++ integration-tests/BIT/proactive-personalisation-test-suite/trigger-caui-prediction#1109/src/main/java/org/societies/integration/test/bit/caui_prediction/RetrieveLearnedModelTest.java
@@ -1,49 +1,40 @@
 package org.societies.integration.test.bit.caui_prediction;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import junit.framework.Assert;
 
-import org.junit.Ignore;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.societies.api.context.CtxException;
 import org.societies.api.context.model.CtxAttribute;
-import org.societies.api.context.model.CtxAttributeIdentifier;
-import org.societies.api.context.model.CtxAttributeTypes;
+import org.societies.api.internal.context.model.CtxAttributeTypes;
 import org.societies.api.context.model.IndividualCtxEntity;
-import org.societies.api.context.model.util.SerialisationHelper;
-import org.societies.api.internal.context.broker.ICtxBroker;
-import org.societies.api.personalisation.model.Action;
-import org.societies.api.personalisation.model.IAction;
-
 
 public class RetrieveLearnedModelTest {
 
 	private static Logger LOG = LoggerFactory.getLogger(TestCase1109.class);
-	
-	public void setUp(){
 
+	public void setUp(){
 	}
 
 	@Test
-	public void test() {
+	public void TestRetrieveModel() {
 		try {
 			IndividualCtxEntity operator = TestCase1109.getCtxBroker().retrieveCssOperator().get();
 			Set<CtxAttribute> setAttr = operator.getAttributes(CtxAttributeTypes.CAUI_MODEL);
 			
+			Assert.assertNotNull(setAttr);
+			Assert.assertEquals(1, setAttr.size());
 			
+			LOG.debug("attributes size refering to caui model "+setAttr.size());
+			LOG.debug("attributes refering to caui model "+setAttr);
+
 			for(CtxAttribute attrRetr : setAttr){
-					Assert.assertNotNull(setAttr);
-					Assert.assertEquals(1, setAttr.size());
-					Assert.assertNotNull(attrRetr.getBinaryValue());
-					LOG.info("attributes refering to caui model "+attrRetr  );
-					//System.out.println("Test 749 started : "+   );	
+				Assert.assertNotNull(attrRetr.getBinaryValue());
 			}
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
