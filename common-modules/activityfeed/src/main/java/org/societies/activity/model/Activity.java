7d5b1468ed83509ab446288fcd720a09d0edb704
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2012-06-12 14:33:02 +0200

    changed to hashmap to ease adding new parameters and make the implementation of the search function better

--- common-modules/activityfeed/src/main/java/org/societies/activity/model/Activity.java
+++ common-modules/activityfeed/src/main/java/org/societies/activity/model/Activity.java
@@ -1,5 +1,7 @@
 package org.societies.activity.model;
 
+import java.util.HashMap;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.apache.shindig.social.core.model.ActivityEntryImpl;
 import org.apache.shindig.social.opensocial.model.ActivityObject;
@@ -21,11 +24,8 @@ public class Activity implements IActivity {
 	 */
 	private static final long serialVersionUID = 1L;
 	private ActivityEntryImpl act;
-	private String verb;
-	private String actor;
-	private String object;
-	private String subject;
-	private String target;
+	@Transient
+	private HashMap<String,String> data = null;
 	private long time;
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -33,59 +33,58 @@ public class Activity implements IActivity {
 	
 	@ManyToOne
 	private ActivityFeed feed;
-	public Activity(){}
+	public Activity(){data = new HashMap<String,String>();}
 	public Activity(IActivity icis)
 	{
+		data = new HashMap<String,String>();
 		this.setActor(icis.getActor());
 		this.setObject(icis.getObject());
 		this.setTarget(icis.getTarget());
 		this.setVerb(icis.getVerb());
 	}
 	
-	@Column(name = "Verb")
+	@Column(name="verb")
 	@Override
 	public String getVerb() {
-		return verb;
+		return data.get("verb");
 	}
 
+	@Column(name="verb")
 	@Override
 	public void setVerb(String verb) {
-		this.verb = verb;
+		data.put("verb", verb);
 		
 	}
-	
-	@Column(name = "Actor")
+	@Column(name="actor")
 	@Override
 	public String getActor() {
-		return actor;
+		return data.get("actor");
 	}
-
+	@Column(name="actor")
 	@Override
 	public void setActor(String actor) {
-		this.actor = actor;
+		data.put("actor", actor);
 	}
-
-	@Column(name = "Object")
+	@Column(name="object")
 	@Override
 	public String getObject() {
-		return object;
+		return data.get("object");
 	}
-
+	@Column(name="object")
 	@Override
 	public void setObject(String object) {
-		this.object = object;
+		data.put("object", object);
 		
 	}
-
-	@Column(name = "Target")
+	@Column(name="target")
 	@Override
 	public String getTarget() {
-		return target;
+		return data.get("target");
 	}
-
+	@Column(name="target")
 	@Override
 	public void setTarget(String target) {
-		this.target = target;
+		data.put("target", target);
 	}
 	public ActivityFeed getFeed() {
 		return feed;
