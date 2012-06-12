f13bc4bba819581d8c23fc776697758bf0d88985
Sarah Gallacher <s.gallacher@hw.ac.uk>
2012-06-12 12:57:38 +0100

    Moved IAndroidUserAgent api into api/android/external

--- api/android/external/src/main/java/org/societies/android/api/external/useragent/IAndroidUserAgent.java
+++ api/android/external/src/main/java/org/societies/android/api/external/useragent/IAndroidUserAgent.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.societies.android.platform.useragent.api;
+package org.societies.android.api.external.useragent;
 
 import org.societies.api.identity.IIdentity;
 import org.societies.api.personalisation.model.IAction;
