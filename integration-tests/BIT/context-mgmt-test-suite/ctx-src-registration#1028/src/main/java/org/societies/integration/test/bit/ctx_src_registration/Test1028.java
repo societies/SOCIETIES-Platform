b04fd769b9544a30d3303f2cda347578933e6d3a
fran_ko <fran_ko@KN-000120.intra.dlr.de>
2012-06-26 11:03:35 +0200

    adapted identity integration test

--- integration-tests/BIT/context-mgmt-test-suite/ctx-src-registration#1028/src/main/java/org/societies/integration/test/bit/ctx_src_registration/Test1028.java
+++ integration-tests/BIT/context-mgmt-test-suite/ctx-src-registration#1028/src/main/java/org/societies/integration/test/bit/ctx_src_registration/Test1028.java
@@ -22,7 +22,7 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.societies.integration.test.bit.ctx_identity;
+package org.societies.integration.test.bit.ctx_src_registration;
 
 
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
--- integration-tests/BIT/context-mgmt-test-suite/ctx-src-registration#1028/src/main/java/org/societies/integration/test/bit/ctx_src_registration/Tester.java
+++ integration-tests/BIT/context-mgmt-test-suite/ctx-src-registration#1028/src/main/java/org/societies/integration/test/bit/ctx_src_registration/Tester.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.societies.integration.test.bit.ctx_identity;
+package org.societies.integration.test.bit.ctx_src_registration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
