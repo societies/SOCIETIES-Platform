3f3e1b0c1b0698673eb365083df99737a2600e22
Joao M. Goncalves <jmgonc@gmail.com>
2012-06-29 11:28:41 +0100

    minimal javadoc added

--- platform-infrastructure/client-frwk/AndroidAgentApi/src/main/java/org/societies/interfaces/XMPPAgent.java
+++ platform-infrastructure/client-frwk/AndroidAgentApi/src/main/java/org/societies/interfaces/XMPPAgent.java
@@ -2,6 +2,12 @@ package org.societies.interfaces;
 
 import org.societies.api.comm.xmpp.exceptions.CommunicationException;
 
+/**
+ * Mirror interface of org.societies.api.comm.xmpp.interfaces.ICommManager, adapted for Android. Check javadoc there.
+ * 
+ * @author Edgar Domingues
+ * @author Joao M. Goncalves
+ */
 public interface XMPPAgent {
 	
 	public void register(String[] elementNames, String[] namespaces, Callback callback);
