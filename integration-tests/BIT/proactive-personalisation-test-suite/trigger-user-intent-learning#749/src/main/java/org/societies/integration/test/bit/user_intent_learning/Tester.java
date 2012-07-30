a932ef4bc6a9249b9ca3e5b2085f772549d74643
nikosk <nikosk@cn.ntua.gr>
2012-07-30 22:56:10 +0300

    user intent learning triggering test updated

--- integration-tests/BIT/proactive-personalisation-test-suite/trigger-user-intent-learning#749/src/main/java/org/societies/integration/test/bit/user_intent_learning/Tester.java
+++ integration-tests/BIT/proactive-personalisation-test-suite/trigger-user-intent-learning#749/src/main/java/org/societies/integration/test/bit/user_intent_learning/Tester.java
@@ -30,70 +30,75 @@ import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import junit.framework.Assert;
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.societies.api.context.CtxException;
-import org.societies.api.context.model.CtxAssociation;
-import org.societies.api.context.model.CtxAssociationIdentifier;
-import org.societies.api.context.model.CtxAssociationTypes;
+import org.societies.api.context.event.CtxChangeEvent;
+import org.societies.api.context.event.CtxChangeEventListener;
 import org.societies.api.context.model.CtxAttribute;
 import org.societies.api.context.model.CtxAttributeIdentifier;
-import org.societies.api.context.model.CtxAttributeTypes;
+import org.societies.api.internal.context.model.CtxAttributeTypes;
 import org.societies.api.context.model.CtxEntity;
 import org.societies.api.context.model.CtxEntityIdentifier;
-import org.societies.api.context.model.CtxEntityTypes;
 import org.societies.api.context.model.CtxHistoryAttribute;
 import org.societies.api.context.model.CtxIdentifier;
-import org.societies.api.context.model.CtxModelObject;
 import org.societies.api.context.model.CtxModelType;
 import org.societies.api.context.model.IndividualCtxEntity;
 import org.societies.api.context.model.util.SerialisationHelper;
 import org.societies.api.identity.IIdentity;
-import org.societies.api.identity.IdentityType;
-import org.societies.api.internal.context.broker.ICtxBroker;
+import org.societies.api.identity.INetworkNode;
+import org.societies.api.identity.InvalidFormatException;
 import org.societies.api.personalisation.model.Action;
 import org.societies.api.personalisation.model.IAction;
 import org.societies.api.schema.servicelifecycle.model.ServiceResourceIdentifier;
-import org.societies.api.useragent.monitoring.IUserActionMonitor;
 
-public class ContextStorageTest {
+
+public class Tester {
 
 	private static Logger LOG = LoggerFactory.getLogger(TestCase749.class);
 
+	private INetworkNode cssNodeId;
+	private IIdentity cssOwnerId;
+	public CtxAttribute uiModelAttr = null;
+
+
 	public void setUp(){
-		System.out.println("Test 749 started : ContextStorageTest");
+
 	}
+
 
 	@Test
 	public void monitorActionsContext() {
 
 		//create actions
-		IIdentity identity = new MockIdentity(IdentityType.CSS, "user", "societies.org");
+		IIdentity identity = TestCase749.commsMgr.getIdManager().getThisNetworkNode();
+
+
+		registerForNewUiModelEvent();
+
+
+
 		ServiceResourceIdentifier serviceId1 = new ServiceResourceIdentifier();
-		ServiceResourceIdentifier serviceId2 = new ServiceResourceIdentifier();
 		try {
-			serviceId1.setIdentifier(new URI("http://testService1"));
-			serviceId2.setIdentifier(new URI("http://testService2"));
+			serviceId1.setIdentifier(new URI("css://nikosk@societies.org/radioService"));
+			serviceId1.setServiceInstanceIdentifier("css://nikosk@societies.org/radioService");
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
 
 		//create actions
-		IAction action1 = new Action(serviceId1, "testService", "volume", "high");
-		IAction action2 = new Action(serviceId1, "testService", "volume", "medium");
-		IAction action3 = new Action(serviceId1, "testService", "volume", "low");
-		IAction action4 = new Action(serviceId1, "testService", "colour", "blue");
+		IAction action1 = new Action(serviceId1, "ServiceType", "volume", "high");
+		IAction action2 = new Action(serviceId1, "ServiceType", "volume", "medium");
+		IAction action3 = new Action(serviceId1, "ServiceType", "volume", "low");
+		IAction action4 = new Action(serviceId1, "ServiceType", "colour", "blue");
 		//IAction action5 = new Action(serviceId1, "testService", "colour", "green");
 		//{volume=medium/2=null, volume=low/0={volume=high/1=1.0}, volume=high/1={volume=medium/2=1.0}} 
 		//set context data
@@ -101,96 +106,88 @@ public class ContextStorageTest {
 		setContext(CtxAttributeTypes.TEMPERATURE, 25);
 		setContext(CtxAttributeTypes.STATUS, "free");
 
-
-		
-
-			//send actions - 1 second apart
-			LOG.info("Monitor services #749 - sending mock actions for storage");
-			TestCase749.uam.monitor(identity, action1);
+		for (int i=0; i<2; i++){
 			try {
+				//send actions - 1 second apart
+				LOG.info("Monitor services #749 - sending mock actions for storage");
+				TestCase749.uam.monitor(identity, action1);
+
+				Thread.sleep(1000);
+
+				setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "office");
+				setContext(CtxAttributeTypes.TEMPERATURE, 45);
+				setContext(CtxAttributeTypes.STATUS, "busy");
+
+				TestCase749.uam.monitor(identity, action2);
+
+				Thread.sleep(1000);
+
+
+				setContext(CtxAttributeTypes.LOCATION_SYMBOLIC,"park");
+				setContext(CtxAttributeTypes.TEMPERATURE, 45);
+				setContext(CtxAttributeTypes.STATUS, "busy");
+
+				TestCase749.uam.monitor(identity, action3);
+
+				Thread.sleep(1000);
+
+
+				setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "zoo");
+				setContext(CtxAttributeTypes.TEMPERATURE, 45);
+				setContext(CtxAttributeTypes.STATUS, "busy");
+
+				TestCase749.uam.monitor(identity, action4);
+
+				Thread.sleep(1000);
+
+
+				setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "university");
+				setContext(CtxAttributeTypes.TEMPERATURE, 45);
+				setContext(CtxAttributeTypes.STATUS, "busy");
+
+				TestCase749.uam.monitor(identity, action1);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action2);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action3);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action4);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action1);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action2);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action2);
+
+				Thread.sleep(1000);
+
+
+				TestCase749.uam.monitor(identity, action2);
+
 				Thread.sleep(1000);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-			setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "office");
-			setContext(CtxAttributeTypes.TEMPERATURE, 45);
-			setContext(CtxAttributeTypes.STATUS, "busy");
-
-			TestCase749.uam.monitor(identity, action2);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-
-			setContext(CtxAttributeTypes.LOCATION_SYMBOLIC,"park");
-			setContext(CtxAttributeTypes.TEMPERATURE, 45);
-			setContext(CtxAttributeTypes.STATUS, "busy");
-
-			TestCase749.uam.monitor(identity, action3);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-
-			setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "zoo");
-			setContext(CtxAttributeTypes.TEMPERATURE, 45);
-			setContext(CtxAttributeTypes.STATUS, "busy");
-
-			TestCase749.uam.monitor(identity, action4);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-
-			setContext(CtxAttributeTypes.LOCATION_SYMBOLIC, "university");
-			setContext(CtxAttributeTypes.TEMPERATURE, 45);
-			setContext(CtxAttributeTypes.STATUS, "busy");
-
-			TestCase749.uam.monitor(identity, action1);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-
-			TestCase749.uam.monitor(identity, action2);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-		
-			TestCase749.uam.monitor(identity, action3);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-		
-			TestCase749.uam.monitor(identity, action4);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			
-			TestCase749.uam.monitor(identity, action1);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			
-			TestCase749.uam.monitor(identity, action2);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			/*
+		}
+		/*
 		 * CHECK HISTORY DATA
 		 */
 		LOG.info("*********** CHECK HISTORY DATA ************");
@@ -198,10 +195,25 @@ public class ContextStorageTest {
 		Map<CtxHistoryAttribute, List<CtxHistoryAttribute>> tupleResults = new HashMap<CtxHistoryAttribute, List<CtxHistoryAttribute>>();
 		try {
 			tupleResults  = TestCase749.ctxBroker.retrieveHistoryTuples(CtxAttributeTypes.LAST_ACTION, listOfEscortingAttributeIds, null, null).get();
-			//Assert.assertTrue(tupleResults.size() == 5);
+			Assert.assertTrue(tupleResults.size() == 24);
 			LOG.info("hoc size:"+ tupleResults.size());
 			printHocTuplesDB(tupleResults);
 
+			LOG.info("*********** ACTIONS SEND WAITING FOR MODEL CREATION ************ ");
+		
+			LOG.info("******start sleeping time: "+System.currentTimeMillis());
+
+			for(int i=0; i < 5; i++){
+				waiting(10);
+				LOG.info("******time :"+i+" "+System.currentTimeMillis() );
+			
+				if(uiModelAttr.getBinaryValue() != null) {
+					retrieveModel();
+					break;
+				}
+			}
+			LOG.info("******end of testing : ");
+			//Assert.assertEquals(24,tupleResults.size());
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -209,52 +221,55 @@ public class ContextStorageTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
-		/*	
-
-		try {
-			List<CtxIdentifier> ctxAttrListIds = TestCase749.ctxBroker.lookup(CtxModelType.ATTRIBUTE, CtxAttributeTypes.LAST_ACTION).get();
-			LOG.info("ctxAttribute list "+ctxAttrListIds);
-			CtxAttributeIdentifier primaryAttrId = null;
-			Map<CtxHistoryAttribute, List<CtxHistoryAttribute>> tupleResults = new HashMap<CtxHistoryAttribute, List<CtxHistoryAttribute>>();
-			for(int i=0; i< ctxAttrListIds.size(); i++){
-				primaryAttrId = (CtxAttributeIdentifier) ctxAttrListIds.get(i);
-
-
-				Map<CtxHistoryAttribute, List<CtxHistoryAttribute>> tempTupleResults = new HashMap<CtxHistoryAttribute, List<CtxHistoryAttribute>>(); 
-
-				tempTupleResults =	TestCase749.ctxBroker.retrieveHistoryTuples(primaryAttrId, listOfEscortingAttributeIds, null, null).get();
-				//LOG.info("*********** print tempTupleResults data ************"+ tempTupleResults);
-				Assert.assertNotNull(tempTupleResults);
-				tupleResults.putAll(tempTupleResults);
-				//LOG.info("*********** print tupleResults data ************"+ tupleResults);
-			}			
-			//LOG.info("*********** final tupleResults data ************"+ tupleResults);
-			Assert.assertTrue(tupleResults.size() == 5);
-			printHocTuplesDB(tupleResults);
-			//			LOG.info("*********** print all history data ************");
-			//			TestCase749.ctxBroker.retrieveHistory(primaryAttrId, 0);
-	} catch (CtxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 */
-
 	}
 
+	public void retrieveModel(){
+
+		try {
+			cssNodeId = TestCase749.commsMgr.getIdManager().getThisNetworkNode();
+			final String cssOwnerStr = cssNodeId.getBareJid();
+			cssOwnerId = TestCase749.commsMgr.getIdManager().fromJid(cssOwnerStr);
+			IndividualCtxEntity operator = TestCase749.getCtxBroker().retrieveIndividualEntity(cssOwnerId).get();
+
+			LOG.info("operator retrieved: "+ operator  );
+			LOG.info("operator retrieved: "+ operator.getId());
+			//IndividualCtxEntity operator = TestCase749.getCtxBroker().retrieveIndividualEntity().get();
+			Set<CtxAttribute> setAttr = operator.getAttributes(CtxAttributeTypes.CAUI_MODEL);
+			//	LOG.info("attributes refering to caui model "+ setAttr  );
+
+			for(CtxAttribute attrRetr : setAttr){
+				Assert.assertNotNull(setAttr);
+				Assert.assertEquals(1, setAttr.size());
+				Assert.assertNotNull(attrRetr.getBinaryValue());
+				LOG.info("attributes refering to caui model "+attrRetr  );
+
+				LOG.info("attribute to caui model binary "+attrRetr.getBinaryValue());
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CtxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	private CtxAttribute setContext(String type, Serializable value){
 
 		CtxAttribute attr = null; 
 		try {
-			IndividualCtxEntity operator = TestCase749.ctxBroker.retrieveCssOperator().get();
+			//IndividualCtxEntity operator = TestCase749.ctxBroker.retrieveCssOperator().get();
+
+			cssNodeId = TestCase749.commsMgr.getIdManager().getThisNetworkNode();
+			final String cssOwnerStr = cssNodeId.getBareJid();
+			cssOwnerId = TestCase749.commsMgr.getIdManager().fromJid(cssOwnerStr);
+			IndividualCtxEntity operator = TestCase749.getCtxBroker().retrieveIndividualEntity(cssOwnerId).get();
 
 			Set<CtxAttribute> ctxAttrSet = operator.getAttributes(type);
 			if(ctxAttrSet.size()>0 ){
@@ -275,9 +290,67 @@ public class ContextStorageTest {
 		} catch (CtxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return attr;
 	}
+
+
+
+
+	private void registerForNewUiModelEvent(){
+
+		if (TestCase749.ctxBroker == null) {
+			LOG.error("Could not register context event listener: ctxBroker is not available");
+			return;
+		}
+
+		CtxAttributeIdentifier uiModelAttributeId = null;
+		IndividualCtxEntity operator;
+		try {
+			//operator = this.ctxBroker.retrieveCssOperator().get();
+			final INetworkNode cssNodeId = TestCase749.commsMgr.getIdManager().getThisNetworkNode();
+			final String cssOwnerStr = cssNodeId.getBareJid();
+			this.cssOwnerId = TestCase749.commsMgr.getIdManager().fromJid(cssOwnerStr);
+
+			operator = TestCase749.ctxBroker.retrieveIndividualEntity(cssOwnerId).get();
+			//LOG.info("operator retrieved "+operator);
+
+			List<CtxIdentifier> ls = TestCase749.ctxBroker.lookup(CtxModelType.ATTRIBUTE, CtxAttributeTypes.CAUI_MODEL).get();
+			if (ls.size()>0) {
+				uiModelAttributeId = (CtxAttributeIdentifier) ls.get(0);
+			} else {
+				CtxAttribute attr = TestCase749.ctxBroker.createAttribute(operator.getId(), CtxAttributeTypes.CAUI_MODEL).get();
+				uiModelAttributeId = attr.getId();
+			}
+			if (uiModelAttributeId != null){
+				TestCase749.ctxBroker.registerForChanges(new MyCtxUIModelChangeEventListener(),uiModelAttributeId);	
+			}		
+
+			//LOG.info("registration for context attribute updates of type CAUI: "+uiModelAttributeId);
+		} catch (InterruptedException e) {
+			// 
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// 
+			e.printStackTrace();
+		} catch (CtxException e) {
+			// 
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+
+			e.printStackTrace();
+		}			
+	}
+
+
+
+
+
+
+
 
 
 	private void printAttr(CtxEntity entity) throws InterruptedException, ExecutionException, CtxException{
@@ -357,4 +430,66 @@ public class ContextStorageTest {
 		} 
 		return result; 
 	}
+
+
+
+	private class MyCtxUIModelChangeEventListener implements CtxChangeEventListener {
+
+
+		MyCtxUIModelChangeEventListener(){
+		}
+
+		@Override
+		public void onCreation(CtxChangeEvent event) {
+
+		}
+
+		@Override
+		public void onUpdate(CtxChangeEvent event) {
+			LOG.info(event.getId() + ": *** Update event *** new User Intent model stored in ctxDB");
+
+			CtxIdentifier uiModelAttrID = event.getId();
+
+			if(uiModelAttrID instanceof CtxAttributeIdentifier){
+
+				try {
+					uiModelAttr = (CtxAttribute) TestCase749.ctxBroker.retrieve(uiModelAttrID).get();
+					LOG.info("uiModelAttr "+uiModelAttr);
+
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+
+					e.printStackTrace();
+				} catch (CtxException e) {
+
+					e.printStackTrace();
+				}	
+			}
+		}
+
+		@Override
+		public void onModification(CtxChangeEvent event) {
+
+		}
+
+		@Override
+		public void onRemoval(CtxChangeEvent event) {
+
+		}
+	}
+
+
+	public static void waiting (int n){
+		
+		LOG.info("inside waiting "+n);
+		long t0, t1;
+		t0 =  System.currentTimeMillis();
+		do{
+			t1 = System.currentTimeMillis();
+		}
+		while (t1 - t0 < n * 1000);
+	}
+
 }
