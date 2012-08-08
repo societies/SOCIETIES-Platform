3c6c9a49ff54eafbe38ae9799ca9019b61a62093
nikosk <nikosk@cn.ntua.gr>
2012-08-08 17:38:53 +0300

    issue with ctxbroker service injection fixed

--- api/java/internal/src/main/java/org/societies/api/internal/context/broker/ICtxBroker.java
+++ api/java/internal/src/main/java/org/societies/api/internal/context/broker/ICtxBroker.java
@@ -62,7 +62,7 @@ import org.societies.api.identity.IIdentity;
  * @author <a href="mailto:nicolas.liampotis@cn.ntua.gr">Nicolas Liampotis</a> (ICCS)
  * @since 0.0.2
  */
-public interface ICtxBroker extends org.societies.api.context.broker.ICtxBroker{
+public interface ICtxBroker {
 	
 	/**
 	 * Creates a CtxAssociation
