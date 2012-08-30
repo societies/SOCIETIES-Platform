a6da237fe3ce339c842a60d98082bf06c4980d01
tcarlyle <tcarlyle@gmail.com>
2012-08-30 17:07:39 +0200

    first step towards cis on new main gui

---
+++ platform-infrastructure/client-frwk/SocietiesAndroidApp/assets/www/javascript/pagespecific/cismanager.js
@@ -0,0 +1,51 @@
+/**
+ * Societies Android app CIS Manager function(s) namespace
+ * 
+ * @namespace SocietiesCISManagerService
+ */
+
+var	SocietiesCISManagerService = {
+			
+		/**
+		 * @methodOf SocietiesCISManagerService#
+		 * @description create a CIS
+		 * @param {Object} successCallback The callback which will be called when result is successful
+		 * @param {Object} failureCallback The callback which will be called when result is unsuccessful
+		 * @returns CIS record
+		 */
+	CreateCIS: function() {
+		console.log("create CIS");
+
+		function success(data) {
+			
+			SocietiesGUI.populateCISRecordpage(data);
+			
+			console.log("Current page: " + $.mobile.activePage[0].id);
+
+			
+			$.mobile.changePage( ($("#menu")), { transition: "slideup"} );
+		}
+		
+		function failure(data) {
+			alert("createCIS - failure: " + data);
+		}
+	    window.plugins.LocalCISManagerService.createCIS(success, failure);
+
+	}
+			
+}
+
+/**
+ * JQuery boilerplate to attach JS functions to relevant HTML elements
+ * 
+ * @description Add Javascript functions to various HTML tags using JQuery
+ * @returns null
+ */
+
+jQuery(function() {
+	console.log("Create CIS jQuery calls");
+
+	$('#createCISbutton').click(function() {
+		SocietiesCISManagerService.CreateCIS();
+	});
+});
