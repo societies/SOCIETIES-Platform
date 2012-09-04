cbc823602173fcc49ba510c1d6762d5bce689715
Luca Lamorte <lucasimone@gmail.com>
2012-09-04 12:43:45 +0200

    Android SNS Connector enabler
    This Native component implements a view with WebKIT to read the HMLT
    and grab the token to fix the restriction of different domains of the
    SN Oauth Web page  and the Client (Android)

---
+++ platform-infrastructure/sns/AndroidSNApp/src/org/societies/platfrom/sns/android/socialapp/Constants.java
@@ -0,0 +1,28 @@
+package org.societies.platfrom.sns.android.socialapp;
+
+public class Constants {
+	// APP CONSTANTS
+	public final static String DEBUG_TAG = "CASingleSignOn";
+	
+	// CA-SSO Library Constants
+	public final static String VERSION	= "1.0";
+	public final static String SSO_URL  = "https://beta.teamlife.it/SSO/msignup";
+	
+	public static final int FB_CODE =  1001;
+	public static final int TW_CODE =  1002;
+	public static final int FQ_CODE =  1003;
+	public static final int LK_CODE =  1004;
+	
+	public static final String FB_URL = "http://dev.lucasimone.eu/fbMobile.php";
+	public static final String LK_URL = "http://dev.lucasimone.eu/auth.php";
+	
+	public static final String TW_URL = "http://157.159.160.188:8080/examples/servlets/servlet/TwitterLoginServlet";
+	public static final String FQ_URL = "http://157.159.160.188:8080/examples/servlets/servlet/FoursquareLoginServlet";
+	
+	
+	public static final String ACCESS_TOKEN 		= "access_token";
+	public static final String TOKEN_EXPIRATION 	= "expires";
+
+	protected static final String FROM = "from";
+	
+}
