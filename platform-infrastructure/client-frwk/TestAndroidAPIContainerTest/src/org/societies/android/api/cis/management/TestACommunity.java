9c2bd350f685f089eb5015927646dc1feab75caf
tcarlyle <tcarlyle@gmail.com>
2012-09-21 12:40:15 +0200

    create CIS working on android, but not join yet

--- platform-infrastructure/client-frwk/TestAndroidAPIContainerTest/src/org/societies/android/api/cis/management/TestACommunity.java
+++ platform-infrastructure/client-frwk/TestAndroidAPIContainerTest/src/org/societies/android/api/cis/management/TestACommunity.java
@@ -50,24 +50,48 @@ public class TestACommunity extends AndroidTestCase{
 		
 	}
 	
-//	public testParcelableWithCrit() throws Exception {
-//		ACommunity aCommunity = new ACommunity();
-//		aCommunity.setCommunityName("name");
-//		aCommunity.setDescription("desc");
-//		aCommunity.setCommunityType("type");
-//		aCommunity.setCommunityJid("jid");
-//		aCommunity.setOwnerJid("ownerJid");
-//		AMembershipCrit amembershipCrit = new AMembershipCrit();
-//		List<ACriteria> l = new ArrayList<ACriteria>();
-//		ACriteria a = new ACriteria();
-//		a.setAttrib("location");
-//		a.setOperator("equals");
-//		a.setRank(1);
-//		a.setValue1("Paris");
-//		l.add(a);
-//		amembershipCrit.setACriteria(l);
-//		aCommunity.setMembershipCrit(amembershipCrit);
-//		
-//	}
+	public void testParcelableWithCrit() throws Exception {
+		ACommunity aCommunity = new ACommunity();
+		aCommunity.setCommunityName("name");
+		aCommunity.setDescription("desc");
+		aCommunity.setCommunityType("type");
+		aCommunity.setCommunityJid("jid");
+		aCommunity.setOwnerJid("ownerJid");
+		AMembershipCrit amembershipCrit = new AMembershipCrit();
+		List<ACriteria> l = new ArrayList<ACriteria>();
+		ACriteria a = new ACriteria();
+		a.setAttrib("location");
+		a.setOperator("equals");
+		a.setRank(1);
+		a.setValue1("Paris");
+		l.add(a);
+		amembershipCrit.setACriteria(l);
+		aCommunity.setMembershipCrit(amembershipCrit);
+
+		
+        Parcel parcel = Parcel.obtain();
+        aCommunity.writeToParcel(parcel, 0);
+        //done writing, now reset parcel for reading
+        parcel.setDataPosition(0);
+        //finish round trip
+        ACommunity createFromParcel = ACommunity.CREATOR.createFromParcel(parcel);
+       
+        assertEquals(aCommunity.getCommunityName(), createFromParcel.getCommunityName());
+        assertEquals(aCommunity.getDescription(), createFromParcel.getDescription());
+        assertEquals(aCommunity.getCommunityType(), createFromParcel.getCommunityType());
+        assertEquals(aCommunity.getCommunityJid(), createFromParcel.getCommunityJid());
+        assertEquals(aCommunity.getOwnerJid(), createFromParcel.getOwnerJid());
+
+        assertEquals(aCommunity.getMembershipCrit().getACriteria().size(), createFromParcel.getMembershipCrit().getACriteria().size());
+        assertEquals(aCommunity.getMembershipCrit().getACriteria().get(0).getAttrib(), createFromParcel.getMembershipCrit().getACriteria().get(0).getAttrib());
+        assertEquals(aCommunity.getMembershipCrit().getACriteria().get(0).getOperator(), createFromParcel.getMembershipCrit().getACriteria().get(0).getOperator());
+        assertEquals(aCommunity.getMembershipCrit().getACriteria().get(0).getValue1(), createFromParcel.getMembershipCrit().getACriteria().get(0).getValue1());
+        assertEquals(aCommunity.getMembershipCrit().getACriteria().get(0).getRank(), createFromParcel.getMembershipCrit().getACriteria().get(0).getRank());
+
+        
+        
+		
+		
+	}
 
 }
