d1fd6e87bdcf4d8f8891cc0b18c2dc79b3023374
Nicolas Liampotis <nicolas.liampotis@cn.ntua.gr>
2012-10-30 13:22:03 +0200

    Use InputStream to read XLS file

--- example/context-broker-evaluation/src/main/java/org/societies/context/evaluation/broker/CtxBrokerEvaluation.java
+++ example/context-broker-evaluation/src/main/java/org/societies/context/evaluation/broker/CtxBrokerEvaluation.java
@@ -24,63 +24,22 @@
  */
 package org.societies.context.evaluation.broker;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import javax.xml.bind.JAXBException;
 
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
-import jxl.read.biff.BiffException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.societies.api.comm.xmpp.exceptions.CommunicationException;
-import org.societies.api.comm.xmpp.exceptions.XMPPError;
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
-
-import org.societies.api.context.CtxException;
-import org.societies.api.context.event.CtxChangeEvent;
-import org.societies.api.context.event.CtxChangeEventListener;
-import org.societies.api.context.model.CommunityCtxEntity;
-import org.societies.api.context.model.CtxAssociation;
-import org.societies.api.context.model.CtxAssociationIdentifier;
-import org.societies.api.context.model.CtxAssociationTypes;
-import org.societies.api.context.model.CtxAttribute;
-import org.societies.api.context.model.CtxAttributeBond;
-import org.societies.api.context.model.CtxAttributeIdentifier;
-import org.societies.api.context.model.CtxAttributeTypes;
-import org.societies.api.context.model.CtxAttributeValueType;
-
-import org.societies.api.context.model.CtxEntity;
-import org.societies.api.context.model.CtxEntityIdentifier;
-import org.societies.api.context.model.CtxEntityTypes;
-import org.societies.api.context.model.CtxHistoryAttribute;
-import org.societies.api.context.model.CtxIdentifier;
-import org.societies.api.context.model.CtxModelObject;
-import org.societies.api.context.model.CtxModelType;
-import org.societies.api.context.model.IndividualCtxEntity;
-import org.societies.api.identity.IIdentity;
-import org.societies.api.identity.InvalidFormatException;
 import org.societies.api.internal.context.broker.ICtxBroker;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
 
 /**
  * This class provides examples for using the internal Context Broker in OSGi. 
@@ -90,6 +49,8 @@ public class CtxBrokerEvaluation {
 
 	/** The logging facility. */
 	private static final Logger LOG = LoggerFactory.getLogger(CtxBrokerEvaluation.class);
+	
+	private static final String XLS_INPUT_FILE = "realityMining.xls";
 
 	/** The Internal Context Broker service reference. */
 	private ICtxBroker internalCtxBroker;
@@ -100,82 +61,66 @@ public class CtxBrokerEvaluation {
 	}
 	
 	@Autowired(required=true)
-	public CtxBrokerEvaluation(ICtxBroker internalCtxBroker, ICommManager commMgr) {
+	public CtxBrokerEvaluation(ICtxBroker internalCtxBroker, ICommManager commMgr) throws Exception {
 
-
+		if (LOG.isInfoEnabled())
+			LOG.info(this.getClass() + " instantiated");
 		
 		this.internalCtxBroker = internalCtxBroker;
 		LOG.info("*** CtxBroker service "+this.internalCtxBroker);
 
 		this.commMgrService = commMgr;
 		LOG.info("*** commMgrService instantiated "+this.commMgrService);
-
-		System.out.println("CtxBrokerEvaluation");
-		this.readData("./realityMining.xls");
+		
+		try {
+			this.readData(XLS_INPUT_FILE);
+		} catch (Exception e) {
+			LOG.error("Could not read " + XLS_INPUT_FILE 
+					+ ": " + e.getLocalizedMessage(), e);
+			throw e;
+		}
 
 	}
 
-	private void readData(String file){
-		
+	private void readData(String file) throws Exception {
+
 		//ReadExcel test = new ReadExcel();
 
 		//test.setInputFile("./realityMining.xls");
 
-		try {
-			HashMap<Integer, List<Integer>> data = xlsReader(file);
+		HashMap<Integer, List<Integer>> data = xlsReader(file);
 
-			System.out.println("CtxBrokerEvaluation data "+data);	
-			
-			for( Integer i : data.keySet()){
-				String identityString = "identity_"+i+"@societies.local";
-				
+		if (LOG.isInfoEnabled())
+			LOG.info("CtxBrokerEvaluation data "+data);	
+
+		for( Integer i : data.keySet()){
+			String identityString = "identity_"+i+"@societies.local";
+
 			//	IIdentity cssIDx =  this.commMgrService.getIdManager().fromJid(identityString);
 			//	IndividualCtxEntity indiEnt = (IndividualCtxEntity) this.internalCtxBroker.createIndividualEntity(cssIDx, CtxEntityTypes.PERSON);
 			//	System.out.println(indiEnt.getId());
-			}
-			
-			
-			
-		} catch (BiffException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		/*
-		catch (CtxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
 	}
+	
+	public HashMap<Integer, List<Integer>> xlsReader(String inputFile) throws Exception {
 
-	
-	
-	
-	public HashMap<Integer, List<Integer>> xlsReader(String inputFile) throws IOException, BiffException {
-
-		HashMap<Integer, List<Integer>> mapOfContextData = new HashMap<Integer, List<Integer>>();
+		final HashMap<Integer, List<Integer>> mapOfContextData = new HashMap<Integer, List<Integer>>();
 		
-		File inputWorkbook = new File(inputFile);
-		Workbook w;
+		final InputStream is = this.getClass().getResourceAsStream("/" + inputFile);
+		if (is == null)
+			throw new FileNotFoundException(inputFile + " (No such file in resources)");
+		final Workbook w;
 		try {
-			w = Workbook.getWorkbook(inputWorkbook);
+			w = Workbook.getWorkbook(is);
 			// Get the first sheet
 			Sheet sheet = w.getSheet(0);
 			Integer key = 0;
 			// Loop over column and lines
 			// get the labels of the columns
 
-
 			for (int j = 0; j < sheet.getColumns(); j++) {
 				List<Integer> data = new ArrayList<Integer>();
 				key=j;
-
 
 				// Cell cell = sheet.getCell(j, 0); //the first row only == the label
 				// if (cell.getType() == CellType.LABEL) {
@@ -196,25 +141,15 @@ public class CtxBrokerEvaluation {
 				mapOfContextData.put(key,data);
 
 			}
-			System.out.println("sheet.getColumns():"+sheet.getColumns());
-			System.out.println("sheet.getRows():"+sheet.getRows());
-		} catch (BiffException e) {
-			e.printStackTrace();
+			if (LOG.isInfoEnabled()) {
+				LOG.info("sheet.getColumns():"+sheet.getColumns());
+				LOG.info("sheet.getRows():"+sheet.getRows());
+			}
+		} finally {
+			if (is != null)
+				is.close();
 		}
 
 		return mapOfContextData;
 	}
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		CtxBrokerEvaluation cmEval = new CtxBrokerEvaluation();
-		cmEval.readData("./realityMining.xls");
-	}
-	
-	
 }
