7bc685ec0d4e32953fe811ee4409d1c22bf61c9b
Alan Walsh <awalsh@awalsh-XPS-L412Z>
2012-11-20 17:20:07 +0000

    Test API Internal projects

---
+++ platform-infrastructure/client-frwk/SocietiesAPITests/Internal/ContainerAndroidInternalAPI/src/org/societies/android/api/internal/container/MainActivity.java
@@ -0,0 +1,14 @@
+package org.societies.android.api.internal.container;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
