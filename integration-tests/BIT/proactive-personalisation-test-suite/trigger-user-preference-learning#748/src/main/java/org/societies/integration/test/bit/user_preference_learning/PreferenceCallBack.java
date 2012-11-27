dea48eec4b0ebd8bc4205684c13468e3b84695b2
Sarah Gallacher <s.gallacher@hw.ac.uk>
2012-11-27 15:56:10 +0000

    Removed refs to IPersonalisationCallback from BIT #748

--- integration-tests/BIT/proactive-personalisation-test-suite/trigger-user-preference-learning#748/src/main/java/org/societies/integration/test/bit/user_preference_learning/PreferenceCallBack.java
+++ integration-tests/BIT/proactive-personalisation-test-suite/trigger-user-preference-learning#748/src/main/java/org/societies/integration/test/bit/user_preference_learning/PreferenceCallBack.java
@@ -1,15 +1,15 @@
 package org.societies.integration.test.bit.user_preference_learning;
 
 import org.societies.api.identity.IIdentity;
-import org.societies.api.internal.personalisation.IPersonalisationCallback;
+//import org.societies.api.internal.personalisation.IPersonalisationCallback;
 import org.societies.api.personalisation.model.IAction;
 import org.societies.api.schema.servicelifecycle.model.ServiceResourceIdentifier;
 
-public class PreferenceCallBack implements IPersonalisationCallback {
+public class PreferenceCallBack /*implements IPersonalisationCallback */{
 	public int flag = 0;
 	public Object lock = new Object();
 
-	@Override
+	//@Override
 	public void receiveIAction(IIdentity arg0, ServiceResourceIdentifier arg1,
 			IAction arg2) {
 		// TODO Auto-generated method stub
