7ea07439fd4324f0f2b999a2ce2df286d4b99a61
Nicolas Liampotis <nicolas.liampotis@cn.ntua.gr>
2012-11-27 15:44:35 +0200

    Revert CtxBrokerCommCallback returns empty packages/namespaces

--- context-management/ContextBroker/src/main/java/org/societies/context/broker/impl/comm/CtxBrokerClient.java
+++ context-management/ContextBroker/src/main/java/org/societies/context/broker/impl/comm/CtxBrokerClient.java
@@ -45,7 +45,6 @@ import org.societies.api.context.model.CtxModelBeanTranslator;
 import org.societies.api.context.model.CtxModelObject;
 import org.societies.api.context.model.CtxModelType;
 import org.societies.api.identity.IIdentity;
-import org.societies.api.identity.IIdentityManager;
 import org.societies.api.identity.Requestor;
 import org.societies.api.identity.RequestorCis;
 import org.societies.api.identity.RequestorService;
@@ -87,8 +86,6 @@ public class CtxBrokerClient implements ICommCallback {
 
 	private ICommManager commManager;
 
-	private IIdentityManager idMgr;
-
 	private CtxBrokerCommCallback ctxBrokerCommCallback = new CtxBrokerCommCallback();
 
 	public ICommManager getCommManager() {
@@ -112,7 +109,6 @@ public class CtxBrokerClient implements ICommCallback {
 		this.commManager = commManager;
 		this.commManager.register(this);
 		this.commManager.register(this.ctxBrokerCommCallback);
-		this.idMgr = this.commManager.getIdManager();
 	}
 
 	public void createEntity(Requestor requestor, IIdentity targetCss, 
