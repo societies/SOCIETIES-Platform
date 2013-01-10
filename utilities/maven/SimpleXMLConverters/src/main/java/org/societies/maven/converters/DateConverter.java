e56cac15898b1edce0bc8ac638ef5dc78fdf36b6
aleckey <alec.leckey@gmail.com>
2013-01-10 09:37:57 +0000

    fixed bug in how timezone info is handled

--- utilities/maven/SimpleXMLConverters/src/main/java/org/societies/maven/converters/DateConverter.java
+++ utilities/maven/SimpleXMLConverters/src/main/java/org/societies/maven/converters/DateConverter.java
@@ -33,22 +33,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateConverter implements Converter<Date> {
-	private final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss Z"; 
+	private final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"; 
 	
 	@Override
     public Date read(InputNode node) throws Exception {
         String dateString = node.getValue();
-        //Long dateLong = Long.parseLong(dateString);
-		//return new Date(dateLong);
+
         DateFormat df = new SimpleDateFormat(DATE_TIME_FORMAT);
         return df.parse(dateString);
     }
 
 	@Override
     public void write(OutputNode node, Date date) {
+		//http://weblogs.java.net/blog/felipegaucho/archive/2009/12/06/jaxb-customization-xsddatetime
+		//new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ") produces	2009-12-06T15:59:34+0100
+		//the expected format for the Schema xsd:dateTime type is	2009-12-06T15:59:34+01:00
+
 		DateFormat df = new SimpleDateFormat(DATE_TIME_FORMAT);
-		//String val = String.valueOf(date.getTime());
-		String val = df.format(date);
+        DateFormat tzDf = new SimpleDateFormat("Z");
+        String timezone = tzDf.format(date);
+        String val = df.format(date) + timezone.substring(0, 3) + ":" + timezone.substring(3);
         node.setValue(val);
     }
 }
