4614b39e87c0e174a9569039276cc961f9686d2d
pskillen <ilike69ers>
2013-02-06 02:12:46 +0000

    Added integration testing framework for the web app using Selenium

---
+++ platform-infrastructure/web-app/src/test/java/org/societies/webapp/integration/selenium/rules/BrowserControlRule.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2011, SOCIETIES Consortium (WATERFORD INSTITUTE OF TECHNOLOGY (TSSG), HERIOT-WATT UNIVERSITY (HWU), SOLUTA.NET
+ * (SN), GERMAN AEROSPACE CENTRE (Deutsches Zentrum fuer Luft- und Raumfahrt e.V.) (DLR), Zavod za varnostne tehnologije
+ * informacijske družbe in elektronsko poslovanje (SETCCE), INSTITUTE OF COMMUNICATION AND COMPUTER SYSTEMS (ICCS), LAKE
+ * COMMUNICATIONS (LAKE), INTEL PERFORMANCE LEARNING SOLUTIONS LTD (INTEL), PORTUGAL TELECOM INOVAÇÃO, SA (PTIN), IBM Corp.,
+ * INSTITUT TELECOM (ITSUD), AMITEC DIACHYTI EFYIA PLIROFORIKI KAI EPIKINONIES ETERIA PERIORISMENIS EFTHINIS (AMITEC), TELECOM
+ * ITALIA S.p.a.(TI),  TRIALOG (TRIALOG), Stiftelsen SINTEF (SINTEF), NEC EUROPE LTD (NEC))
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.societies.webapp.integration.selenium.rules;
+
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserControlRule implements MethodRule {
+
+    public static final boolean CLOSE_BROWSER_ON_FAILURE = false;
+//    public static final boolean CLOSE_BROWSER_ON_FAILURE = true;
+
+    private WebDriver driver;
+    private String url;
+
+    public BrowserControlRule(WebDriver driver, String url) {
+        this.driver = driver;
+        this.url = url;
+    }
+
+    @Override
+    public Statement apply(final Statement statement, final FrameworkMethod frameworkMethod, final Object o) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+
+                driver.get(url); // open browser
+                driver.manage().window().maximize();
+
+                try {
+
+                    statement.evaluate(); // perform the test
+
+                    driver.close(); // close on success
+
+                } catch (Throwable t) {
+
+                    if (CLOSE_BROWSER_ON_FAILURE)
+                        driver.close(); // close on failure
+
+                    throw t;
+                }
+            }
+        };
+    }
+}
