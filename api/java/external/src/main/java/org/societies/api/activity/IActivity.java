cb80a703846f0565b062529149f7f964872ccda7
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2013-02-12 15:14:52 +0100

    added javadoc explaining the different fields in IActivity

--- api/java/external/src/main/java/org/societies/api/activity/IActivity.java
+++ api/java/external/src/main/java/org/societies/api/activity/IActivity.java
@@ -32,6 +32,12 @@ import org.societies.utilities.annotations.SocietiesExternalInterface.SocietiesI
  * This interface is mainly a container for passing the parameter
  * Activity into the {@link IActivityFeed} calls. This interface
  * only provides getters and setters to the activity container.
+ *
+ * To illustrate the usage of this interface, I will use an example througout the documentation:
+ * "Jack says 'hi' to Jane" (and this activity was published 00:00:00 01 january 1970 UTC)
+ *
+ * For more information of the standard this interface represents a subset of, see:
+ * http://activitystrea.ms/specs/json/1.0/
  * 
  * @author      Babak.Farshchian@sintef.no
  * @author      Bjørn Magnus
@@ -42,16 +48,100 @@ import org.societies.utilities.annotations.SocietiesExternalInterface.SocietiesI
 
 @SocietiesExternalInterface(type = SocietiesInterfaceType.PROVIDED)
 public interface IActivity {
+    /**
+     * This method returns the ID of the activity
+     * @return {@link Long} the ID.
+     */
 	public Long getId();
+    /**
+     * This method returns the verb of the activity
+     *
+     * e.g. "says"
+     *
+     * @return {@link String} the ID.
+     */
 	public String getVerb();
+
+    /**
+     * This methods lets you set the verb of the activity
+     *
+     * e.g. "says"
+     *
+     * @param {@link String} verb
+     */
 	public void setVerb(String verb);
+
+    /**
+     * This method returns the actor of the activity
+     *
+     * e.g. "Jack"
+     *
+     * @return {@link String}
+     */
 	public String getActor();
+
+    /**
+     * This method lets you set the actor of the activity
+     *
+     * e.g. "Jack"
+     *
+     * @param {@link String} actor
+     */
 	public void setActor(String actor);
+
+    /**
+     * This method returns the object of the activity
+     *
+     * e.g. "hi"
+     *
+     * @return {@link String}
+     */
 	public String getObject();
+
+    /**
+     * This method lets you set the object of the activity
+     *
+     * e.g. "hi"
+     *
+     * @param {@link String} object
+     */
 	public void setObject(String object);
+
+    /**
+     * This method returns the target of the activity
+     *
+     * e.g. "Jane"
+     *
+     * @return {@link String}
+     */
 	public String getTarget();
+
+    /**
+     * This method lets you set the target of the activity
+     *
+     * e.g. "Jane"
+     *
+     * @param {@link String} target
+     */
 	public void setTarget(String target);
 
+    /**
+     * This method returns the time of publication of the activity in ms since unix epoch represented as a string.
+     *
+     * e.g. "0"
+     *
+     * @return {@link String}
+     */
 	public String getPublished();
+
+    /**
+     * This method is not of any practical use for API/3rd party service developers, this method is only for use within the activityfeed implementation, and may be refactored at any moment of time.
+     * This method lets you set the time of publication of the activity in ms since unix epoch represented as a string.
+     *
+     * e.g. "0"
+     *
+     * @return {@link String}
+     */
+    @Deprecated
 	public void setPublished(String published);
 }
