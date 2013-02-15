2e3d59ce32f439fad04edfc7351921651648a006
yboul <ybouloudis@amitec.gr>
2013-02-15 14:35:22 +0200

    change to integration test 1108

--- integration-tests/BIT/context-mgmt-test-suite/comm-ctx-estimation#1108/src/main/java/org/societies/integration/test/bit/comm_ctx_estimation/CreateCommunityCtx.java
+++ integration-tests/BIT/context-mgmt-test-suite/comm-ctx-estimation#1108/src/main/java/org/societies/integration/test/bit/comm_ctx_estimation/CreateCommunityCtx.java
@@ -1,65 +1,62 @@
 package org.societies.integration.test.bit.comm_ctx_estimation;
 
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.societies.api.cis.attributes.MembershipCriteria;
-import org.societies.api.cis.attributes.Rule;
 import org.societies.api.cis.management.ICisManager;
 import org.societies.api.cis.management.ICisOwned;
+import org.societies.api.comm.xmpp.interfaces.ICommManager;
 import org.societies.api.context.CtxException;
 import org.societies.api.context.model.CommunityCtxEntity;
+import org.societies.api.context.model.CtxAssociation;
+import org.societies.api.context.model.CtxAssociationIdentifier;
 import org.societies.api.context.model.CtxAttribute;
 import org.societies.api.context.model.CtxAttributeIdentifier;
 import org.societies.api.context.model.CtxAttributeTypes;
-import org.societies.api.context.model.CtxAttributeValueType;
 import org.societies.api.context.model.CtxEntityIdentifier;
-import org.societies.api.context.model.CtxEntityTypes;
+import org.societies.api.context.model.CtxIdentifier;
+import org.societies.api.context.model.CtxModelType;
 import org.societies.api.context.model.IndividualCtxEntity;
 import org.societies.api.identity.IIdentity;
-import org.societies.api.identity.INetworkNode;
 import org.societies.api.identity.InvalidFormatException;
+import org.societies.api.internal.context.broker.ICtxBroker;
+import org.societies.api.internal.context.model.CtxAssociationTypes;
 
 
 public class CreateCommunityCtx {
 
 	private static Logger LOG = LoggerFactory.getLogger(Test1108.class);
 
-
-	//	private IIdentity cisID;
-	private IIdentity cssID1; 
-	private IIdentity cssID2;
-	private IIdentity cssID3;
-
+	private IIdentity cssIDXCManager; 
+	private IIdentity cssIDJohn;
 
 	//private CommunityCtxEntity communityEntity;
-
-	private IndividualCtxEntity indiEnt1;
-	private IndividualCtxEntity indiEnt2;
-	private IndividualCtxEntity indiEnt3;
+	private String targetXCManager = "XCManager.societies.local";
+	private String targetJohn = "john.societies.local";
+	
+	private IndividualCtxEntity XCManager;
+	private IndividualCtxEntity John;
 
 	private CtxAttributeIdentifier communityAttrTemperatureId = null;
 	private CtxAttributeIdentifier communityAttrInterestsId = null;
 	private CtxEntityIdentifier communityCtxEntityID = null;
 
+	public ICtxBroker ctxBroker;
+	public ICisManager cisManager;
+	public ICommManager commManager;
+	//private INetworkNode cssNodeId;
 
-	private INetworkNode cssNodeId;
-	private ICisManager cisManager;
-
-	String cssPassword = "password.societies.local";
+	//String cssPassword = "password.societies.local";
 
 	public void setUp(){
 
 		LOG.info("CreateCommunityCtx started");
-		//	LOG.info("Context broker service: "+ Test1108.getCtxBroker());
-		//	LOG.info("comm manager service"+ Test1108.getCommManager());
-		//	LOG.info("cisManager service"+ Test1108.getCisManager());
 
 	}
 
@@ -67,102 +64,100 @@ public class CreateCommunityCtx {
 	public void TestCreateCommunityEntities() {
 
 		LOG.info("TestCreateCommunityEntities");
-		LOG.info("Context broker service: "+ Test1108.getCtxBroker());
-		LOG.info("comm manager service"+ Test1108.getCommManager());
-		LOG.info("cisManager service"+ Test1108.getCisManager());
+		this.ctxBroker=Test1108.getCtxBroker();
+		this.commManager= Test1108.getCommManager();
+		this.cisManager = Test1108.getCisManager();
+		
+		LOG.info("Context broker service: "+ this.ctxBroker);
+		LOG.info("comm manager service"+ this.commManager);
+		LOG.info("cisManager service"+ this.cisManager);
 
 		CommunityCtxEntity communityEntity = null;
 
 		try {
-			IIdentity cisID = createCISid(); //cis-843d9742-f7ff-4ba9-ae53-60b4b4bcd30d.societies.local
-			//IIdentity cisID = Test1108.getCommManager().getIdManager().fromJid("cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local");
-			//IIdentity cisID = ('cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local');
-			//LOG.info("#@#@# Cis id "+cisID);
-			LOG.info("#@#@# Test id "+cisID); //cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local
-			//communityEntity = Test1108.getCtxBroker().createCommunityEntity(cisID).get();
-			communityEntity = Test1108.getCtxBroker().createCommunityEntity(cisID).get();
-			LOG.info("gCommunity Entity Created");
-			LOG.info("g00");		
+			this.cssIDXCManager = this.commManager.getIdManager().fromJid(targetXCManager);
+			this.cssIDJohn = this.commManager.getIdManager().fromJid(targetJohn);
 			
+			XCManager = this.ctxBroker.retrieveIndividualEntity(this.cssIDXCManager).get();
+			List<CtxIdentifier> XCManagerInterestList = this.ctxBroker.lookup(XCManager.getId(), CtxModelType.ATTRIBUTE, CtxAttributeTypes.INTERESTS).get();
+			CtxAttribute XCManagerInterestAttr = null;
 			
+			if (XCManagerInterestList.size() ==0){
+				XCManagerInterestAttr = this.ctxBroker.createAttribute(XCManager.getId(), CtxAttributeTypes.INTERESTS).get();
+			} else {
+				XCManagerInterestAttr = (CtxAttribute) this.ctxBroker.retrieve(XCManagerInterestList.get(0)).get();
+			}
+			XCManagerInterestAttr.setStringValue("reading,socialnetworking,cinema,sports");
+			CtxAttribute XCManagerInterestsAttrUpdated = (CtxAttribute) this.ctxBroker.update(XCManagerInterestAttr).get();
 			
-			//Creation of the 1st entity XCManager with attribute interests reading,socialnetworking,cinema,sports
-			this.cssID1 =  Test1108.getCommManager().getIdManager().fromJid("XCManager.societies.local ");
-			LOG.info("trying to create attribute witth: "+this.cssID1 + "and "+ CtxEntityTypes.PERSON);
-			this.indiEnt1 = Test1108.getCtxBroker().createIndividualEntity(this.cssID1, CtxEntityTypes.PERSON).get();
-			LOG.info("g01");
-			LOG.info("xssID "+this.cssID1); //XCManager.societies.local 
-			LOG.info("indiEnt1 "+this.indiEnt1); //org.societies.api.context.model.IndividualCtxEntity@a1e977de
-			LOG.info("this.indiEnt1.getId() "+this.indiEnt1.getId()+"CtxAttributeTypes.INTERESTS "+CtxAttributeTypes.INTERESTS); //(context://XCManager@societies.local /ENTITY/person/950308,interests)
-			CtxAttribute individualAttr1 = Test1108.getCtxBroker().createAttribute(this.indiEnt1.getId() , CtxAttributeTypes.INTERESTS).get();
-			LOG.info("g011");
-			individualAttr1.setStringValue("reading,socialnetworking,cinema,sports");
-			LOG.info("g012");
-			individualAttr1.setValueType(CtxAttributeValueType.STRING);
-				
-			//Creation of attribute Temperature of the 1st entity XCManager with value 25
-			LOG.info("g02");
-			CtxAttribute individualAttr2 = Test1108.getCtxBroker().createAttribute(this.indiEnt1.getId() , CtxAttributeTypes.TEMPERATURE).get();
-			individualAttr2.setValueType(CtxAttributeValueType.INTEGER);
-			individualAttr2.setIntegerValue(25);
-			LOG.info("g03");
-			Test1108.getCtxBroker().update(individualAttr1);
-			Test1108.getCtxBroker().update(individualAttr2);
-			LOG.info("g04");
+			// john's interest (remote comm will initiate)
+			LOG.info("john identity : " + this.cssIDJohn.toString());
+			CtxEntityIdentifier johnEntityID = this.ctxBroker.retrieveIndividualEntityId(null, this.cssIDJohn).get();
+			LOG.info("john entity id : " + johnEntityID.toString());
+			//List<CtxIdentifier> johnInterestList = this.ctxBroker.lookup(johnEntityID, CtxModelType.ATTRIBUTE,CtxAttributeTypes.INTERESTS).get();
+			List<CtxIdentifier> johnInterestList = this.ctxBroker.lookup(null, this.cssIDJohn, CtxModelType.ATTRIBUTE,CtxAttributeTypes.INTERESTS).get();
 			
+			CtxAttribute johnInterestAttr = null;
 			
-			// Creation of the 2nd entity XCManager with attribute interests cooking,horseRiding,restaurants,cinema and Temperature 27
-			this.cssID2 =  Test1108.getCommManager().getIdManager().fromJid("XCManager.societies.local");
-			this.indiEnt2 = Test1108.getCtxBroker().createIndividualEntity(this.cssID2, CtxEntityTypes.PERSON).get();
-			LOG.info("g05");
-			CtxAttribute individualAttr3 = Test1108.getCtxBroker().createAttribute(this.indiEnt2.getId() , CtxAttributeTypes.INTERESTS).get();
-			individualAttr3.setStringValue("cooking,horseRiding,restaurants,cinema");
-			individualAttr3.setValueType(CtxAttributeValueType.STRING);
-			LOG.info("g06");
-			CtxAttribute individualAttr4 = Test1108.getCtxBroker().createAttribute(this.indiEnt2.getId() , CtxAttributeTypes.TEMPERATURE).get();
-			individualAttr4.setIntegerValue(27);
-			individualAttr4.setValueType(CtxAttributeValueType.INTEGER);
-			LOG.info("g07");
-			Test1108.getCtxBroker().update(individualAttr3);
-			Test1108.getCtxBroker().update(individualAttr4);
-			LOG.info("g08");
-			
-			
-			//Creation of the 3rd entity John with attributes Interests cooking,horseRiding,socialnetworking,restaurants,cinema and Temperature 27
-			this.cssID3 =  Test1108.getCommManager().getIdManager().fromJid("john.societies.local");
-			this.indiEnt3 = Test1108.getCtxBroker().createIndividualEntity(this.cssID3, CtxEntityTypes.PERSON).get();
-			LOG.info("g09");
-			CtxAttribute individualAttr5 = Test1108.getCtxBroker().createAttribute(this.indiEnt3.getId(), CtxAttributeTypes.INTERESTS).get();
-			individualAttr5.setStringValue("cooking,horseRiding,socialnetworking,restaurants,cinema");
-			individualAttr5.setValueType(CtxAttributeValueType.STRING);
-			LOG.info("g10");
-			CtxAttribute individualAttr6 = Test1108.getCtxBroker().createAttribute(this.indiEnt3.getId() , CtxAttributeTypes.TEMPERATURE).get();
-			individualAttr6.setIntegerValue(27);
-			individualAttr6.setValueType(CtxAttributeValueType.INTEGER);
-			LOG.info("g11");
-			Test1108.getCtxBroker().update(individualAttr5);
-			Test1108.getCtxBroker().update(individualAttr6);
-			LOG.info("g12");
-	//		communityEntity.addMember(this.indiEnt1.getId());
-	//		communityEntity.addMember(this.indiEnt2.getId());
-	//		communityEntity.addMember(this.indiEnt3.getId());
-			LOG.info("g13");
-			Test1108.getCtxBroker().update(communityEntity);
-			LOG.info("g14");
-			CtxAttribute ctxAttr = Test1108.getCtxBroker().createAttribute(communityEntity.getId(), CtxAttributeTypes.TEMPERATURE).get();
-			CtxAttribute ctxAttr1 = Test1108.getCtxBroker().createAttribute(communityEntity.getId(), CtxAttributeTypes.INTERESTS).get();
-			LOG.info("g15");
-			this.communityAttrTemperatureId = ctxAttr.getId();
-			this.communityCtxEntityID = communityEntity.getId();
-			this.communityAttrInterestsId = ctxAttr1.getId();
-			LOG.info("g16");
-			//this.communityAttrTemperatureId = ctxAttr1.getId();
+			if( johnInterestList.size() == 0){
+				johnInterestAttr = this.ctxBroker.createAttribute(johnEntityID, CtxAttributeTypes.INTERESTS).get();
+			} else {
+				johnInterestAttr = (CtxAttribute) this.ctxBroker.retrieve(johnInterestList.get(0)).get();
+			}
+			johnInterestAttr.setStringValue("cooking,horseRiding,restaurants,cinema");
 
-			// LOG.info("TestRetrieveCommunityEntities "+ctxAttr.getId() );
-			LOG.info("TestRetrieveCommunityEntities   this.communityAttrTemperatureId "+ this.communityAttrTemperatureId );
-			LOG.info("TestRetrieveCommunityEntities   this.communityInterestsId "+ this.communityAttrInterestsId);
-			LOG.info("g17");
-			LOG.info("we5uy34y5 before retrieveCommunityEntities");
+			CtxAttribute johnInterestAttrUpdated = (CtxAttribute) this.ctxBroker.update(johnInterestAttr).get();
+			
+			//Create CIS
+			
+			IIdentity cisID = this.createCIS();
+			LOG.info("wait until community entity is created for cisID"+ cisID  );
+			Thread.sleep(20000);
+			
+			LOG.info("try to retrieve ctxCommunityEntity of cisID : " + cisID );
+			
+//			//IIdentity cisID = Test1108.getCommManager().getIdManager().fromJid("cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local");
+//			//IIdentity cisID = ('cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local');
+//			//LOG.info("#@#@# Cis id "+cisID);
+//			LOG.info("#@#@# Test id "+cisID); //cis-05ecbe3d-9577-445d-a652-a3ea2beeb7f2.societies.local
+//			//communityEntity = Test1108.getCtxBroker().createCommunityEntity(cisID).get();
+//			communityEntity = Test1108.getCtxBroker().createCommunityEntity(cisID).get();
+//			LOG.info("gCommunity Entity Created");
+//			LOG.info("g00");		
+			
+			CtxEntityIdentifier ctxCommunityEntityIdentifier = this.ctxBroker.retrieveCommunityEntityId(cisID).get();
+			LOG.info("ctxCommunityEntity id : " + ctxCommunityEntityIdentifier.toString());
+			
+			communityEntity = (CommunityCtxEntity) this.ctxBroker.retrieve(ctxCommunityEntityIdentifier).get();
+			LOG.info("ctxCommunityEntity members : " + communityEntity.getMembers());
+			
+			// only xcManager is member of the cis community, manually add john
+			// this should be removed
+			
+			if (communityEntity.getMembers().size()==1){
+				Set<CtxAssociationIdentifier> comAssocIdSet = communityEntity.getAssociations(CtxAssociationTypes.HAS_MEMBERS);
+				CtxAssociation hasMembersAssoc = null;
+				if (comAssocIdSet.size() >0){
+					for (CtxAssociationIdentifier assocID:comAssocIdSet){
+						hasMembersAssoc = (CtxAssociation) this.ctxBroker.retrieve(assocID).get();
+					}
+					hasMembersAssoc.addChildEntity(johnEntityID);
+					hasMembersAssoc = (CtxAssociation) this.ctxBroker.update(hasMembersAssoc).get(); 
+				}
+			}
+			
+			CommunityCtxEntity communityEntityUpdated = (CommunityCtxEntity) this.ctxBroker.retrieve(ctxCommunityEntityIdentifier).get();
+			LOG.info("Updated ctxCommunityEntity members : " + communityEntityUpdated.getMembers());
+			
+			CtxAttribute communityInterests = this.ctxBroker.createAttribute(communityEntityUpdated.getId(), CtxAttributeTypes.INTERESTS).get();
+			LOG.info("**** communityInterests: " + communityInterests.getId());
+			
+			CtxAttribute communityInterestsValue = this.ctxBroker.estimateCommunityContext(communityEntityUpdated.getId(), communityInterests.getId());
+			LOG.info("**** communityInterests id : " + communityInterestsValue.getId());
+			LOG.info("**** communityInterests value : " + communityInterestsValue.getStringValue());
+			
+
+		
 			retrieveCommunityEntities();
 
 		} catch (InterruptedException e) {
@@ -231,56 +226,40 @@ public class CreateCommunityCtx {
 
 	// helper classes
 
-	protected  IIdentity createCISid() {
+	protected  IIdentity createCIS() {
 
 		IIdentity cisID = null;
-		IIdentity cssOwnerId = null;
-		ICisOwned cisOwned ;
 
 		try {
-			this.cssNodeId = Test1108.getCommManager().getIdManager().getThisNetworkNode();
-			LOG.info("*** cssNodeId = " + this.cssNodeId);
-			final String cssOwnerStr = this.cssNodeId.getBareJid();
-			cssOwnerId = Test1108.getCommManager().getIdManager().fromJid(cssOwnerStr);
-
-			//Hashtable<String,MembershipCriteria> cisCriteria = new Hashtable<String,MembershipCriteria>();
-
-
-			Hashtable<String, MembershipCriteria> cisCriteria = new Hashtable<String, MembershipCriteria> (); 
-			MembershipCriteria m = new MembershipCriteria();
-			try{
-				Rule r = new Rule("equals",new ArrayList<String>(Arrays.asList("married")));
-				m.setRule(r);
-				cisCriteria.put(CtxAttributeTypes.STATUS, m);
-				r = new Rule("equals",new ArrayList<String>(Arrays.asList("Brazil")));
-				m.setRule(r);
-				cisCriteria.put(CtxAttributeTypes.ADDRESS_HOME_COUNTRY, m);
-			}catch(InvalidParameterException e){
-				// TODO: treat expection
-				e.printStackTrace();
-			}
-
+			Hashtable<String, MembershipCriteria> cisCriteria = new Hashtable<String, MembershipCriteria> ();
 			LOG.info("*** trying to create cis : ");
-			cisOwned = Test1108.getCisManager().createCis("CIS_NAME", "password.societies.local" , cisCriteria,"stringArg3").get();
-
+			ICisOwned cisOwned = this.cisManager.createCis("testCIS", "cisType", cisCriteria, "nice CIS").get();
 			LOG.info("*** cis created: "+cisOwned.getCisId());
-
-			//LOG.info("*** cis list: "+cisManager.getCisList());
-			//			cisManager.createCis(arg0, arg1, arg2, arg3)
-
-			//CisOwned ciss =  (cisManager.createCis(cssOwnerStr, "password.societies.local","cisNAme", null , TEST_CIS_MODE)).get();
-
-
-			//.createCis("testCIS", "cisType", cisCriteria, "nice CIS").get();
-
-			//cisOwned = cisManager.getOwnedCis(arg0);
+			
+			cisOwned = cisManager.createCis("testCIS", "cisType", cisCriteria, "nice CIS").get();
 			LOG.info("*** cisOwned " +cisOwned);
+			LOG.info("*** cisOwned.getCisId() " +cisOwned.getCisId());
 			String cisIDString  = cisOwned.getCisId();
-			LOG.info("*** cisOwned.getCisId() " +cisIDString);
-			cisID = Test1108.getCommManager().getIdManager().fromJid(cisIDString);
+			
+			cisID = this.commManager.getIdManager().fromJid(cisIDString);
+			
+//			MembershipCriteria m = new MembershipCriteria();
+//			try{
+//				Rule r = new Rule("equals",new ArrayList<String>(Arrays.asList("married")));
+//				m.setRule(r);
+//				cisCriteria.put(CtxAttributeTypes.STATUS, m);
+//				r = new Rule("equals",new ArrayList<String>(Arrays.asList("Brazil")));
+//				m.setRule(r);
+//				cisCriteria.put(CtxAttributeTypes.ADDRESS_HOME_COUNTRY, m);
+//			}catch(InvalidParameterException e){
+//				// TODO: treat expection
+//				e.printStackTrace();
+//			}
+
 
 		} catch (InterruptedException e) {
-
+			// TODO Auto-generated catch block
+				e.printStackTrace();
 		} catch (ExecutionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
