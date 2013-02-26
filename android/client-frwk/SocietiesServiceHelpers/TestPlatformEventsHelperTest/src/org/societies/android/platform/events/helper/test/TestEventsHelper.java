90fa59a8052748198569596ea56ced5eae83b534
Alan Walsh <awalsh@awalsh-XPS-L412Z>
2013-02-26 14:13:23 +0000

    Added new test

--- android/client-frwk/SocietiesServiceHelpers/TestPlatformEventsHelperTest/src/org/societies/android/platform/events/helper/test/TestEventsHelper.java
+++ android/client-frwk/SocietiesServiceHelpers/TestPlatformEventsHelperTest/src/org/societies/android/platform/events/helper/test/TestEventsHelper.java
@@ -1,5 +1,6 @@
 package org.societies.android.platform.events.helper.test;
 
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +10,7 @@ import org.societies.android.api.events.IPlatformEventsCallback;
 import org.societies.android.api.events.PlatformEventsHelperNotConnectedException;
 import org.societies.android.platform.events.helper.EventsHelper;
 import org.societies.api.schema.cssmanagement.CssEvent;
+import org.societies.api.schema.useragent.feedback.ExpFeedbackResultBean;
 
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
@@ -117,6 +119,110 @@ public class TestEventsHelper extends AndroidTestCase {
 										assertEquals(1, result);
 										try {
 											helper.publishEvent(IAndroidSocietiesEvents.CSS_MANAGER_ADD_CSS_NODE_INTENT, getCssEvent(), new IPlatformEventsCallback() {
+												
+												@Override
+												public void returnAction(int arg0) {
+												}
+												
+												@Override
+												public void returnAction(boolean result) {
+													assertTrue(result);
+													try {
+														helper.unSubscribeFromEvent(IAndroidSocietiesEvents.CSS_MANAGER_ADD_CSS_NODE_INTENT, new IPlatformEventsCallback() {
+															
+															@Override
+															public void returnAction(int result) {
+																fail();
+															}
+															
+															@Override
+															public void returnAction(boolean resultFlag) {
+																assertTrue(resultFlag);
+																helper.tearDownService(new IMethodCallback() {
+																	
+																	@Override
+																	public void returnAction(String result) {
+																		fail();
+																	}
+																	
+																	@Override
+																	public void returnAction(boolean resultFlag) {
+																		assertTrue(resultFlag);
+																		TestEventsHelper.this.testCompleted = true;
+																		latch.countDown();
+																	}
+																});
+															}
+														});
+													} catch (PlatformEventsHelperNotConnectedException e) {
+														// TODO Auto-generated catch block
+														e.printStackTrace();
+													}
+													
+												}
+											});
+										} catch (PlatformEventsHelperNotConnectedException e1) {
+											// TODO Auto-generated catch block
+											e1.printStackTrace();
+										}
+									}
+									
+									@Override
+									public void returnAction(boolean resultFlag) {
+										fail();
+									}
+								});
+							} catch (PlatformEventsHelperNotConnectedException e) {
+								e.printStackTrace();
+								fail();
+							}
+						}
+					});
+				} catch (PlatformEventsHelperNotConnectedException e) {
+					e.printStackTrace();
+					fail();
+				}
+			}
+		});
+		
+		latch.await(LATCH_TIME_OUT, TimeUnit.MILLISECONDS);
+		assertTrue(this.testCompleted);
+	}
+	@MediumTest
+	public void testSubscribeToEventWithComplexPayload() throws Exception {
+		this.testCompleted = false;
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		final EventsHelper helper = new EventsHelper(getContext());
+		helper.setUpService(new IMethodCallback() {
+			
+			@Override
+			public void returnAction(String result) {
+				fail();
+			}
+			
+			@Override
+			public void returnAction(boolean resultFlag) {
+				assertTrue(resultFlag);
+				try {
+					helper.subscribeToEvent(IAndroidSocietiesEvents.CSS_MANAGER_ADD_CSS_NODE_INTENT, new IPlatformEventsCallback() {
+						
+						@Override
+						public void returnAction(int result) {
+							fail();
+						}
+						
+						@Override
+						public void returnAction(boolean resultFlag) {
+							assertTrue(resultFlag);
+							try {
+								helper.getNumSubscribedNodes(new IPlatformEventsCallback() {
+									
+									@Override
+									public void returnAction(int result) {
+										assertEquals(1, result);
+										try {
+											helper.publishEvent(IAndroidSocietiesEvents.CSS_MANAGER_ADD_CSS_NODE_INTENT, getExpFeedbackResultBean(), new IPlatformEventsCallback() {
 												
 												@Override
 												public void returnAction(int arg0) {
@@ -313,4 +419,10 @@ public class TestEventsHelper extends AndroidTestCase {
     	return event;
     }
 
+    private static ExpFeedbackResultBean getExpFeedbackResultBean() {
+    	ExpFeedbackResultBean bean  = new ExpFeedbackResultBean();
+    	bean.setFeedback(new ArrayList<String>());
+    	bean.setRequestId("requestID1111");
+    	return bean;
+    }
 }
