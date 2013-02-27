869b3be599b83672d7f561b73acf98818ba3769f
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2013-02-27 22:24:48 +0100

    checkpoint

---
+++ integration-tests/BIT/activityfeedmanager/src/main/java/org/societies/integration/test/bit/cpa/TestCase10961.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.societies.integration.test.bit.cpa;
+
+/**
+ * The test case 713 aims to test 3P service installation.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.societies.api.activity.IActivityFeedManager;
+import org.societies.api.cis.management.ICisManager;
+import org.societies.api.comm.xmpp.interfaces.ICommManager;
+import org.societies.integration.test.IntegrationTestCase;
+
+public class TestCase10961 extends IntegrationTestCase {
+	private static Logger LOG = LoggerFactory.getLogger(TestCase10961.class);
+    public static ICisManager cisManager;
+    public static IActivityFeedManager activityFeedManager;
+    public static ICommManager commManager;
+
+    /**
+	 * Privacy Log Appender (injected)
+	 */
+
+	public TestCase10961() {
+
+		super(10961, new Class[] {NominalTestCase.class});
+		NominalTestCase.testCaseNumber = testCaseNumber;
+	}
+
+    public  void setCisManager(ICisManager cisManager) {
+        LOG.debug("[TEST CASE #"+testCaseNumber+"] setCisManager()");
+        this.cisManager = cisManager;
+    }
+
+    public  void setActivityFeedManager(IActivityFeedManager activityFeedManager) {
+        LOG.debug("[TEST CASE #"+testCaseNumber+"] setActivityFeedManager()");
+        this.activityFeedManager = activityFeedManager;
+    }
+
+    public void setCommManager(ICommManager commManager) {
+        LOG.debug("[TEST CASE #"+testCaseNumber+"] setCommManager()");
+        this.commManager = commManager;
+    }
+
+    public void destroy(){
+
+    }
+
+
+
+}
