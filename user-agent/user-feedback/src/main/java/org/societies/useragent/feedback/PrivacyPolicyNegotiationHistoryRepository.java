b5f5f377a99e0d113de4cd01762299ab6ff8978c
Patrick Skillen <p.skillen@hw.ac.uk>
2013-05-03 17:18:03 +0100

    - Added code to UserFeedback component to store requests in a database before sending them via XMPP PubSub - Removed persistence.xml file - this was added temporarily but not used - Changed a lot of "requestID" variables to "requestId" to maintain consistency in capitalization

--- user-agent/user-feedback/src/main/java/org/societies/useragent/feedback/PrivacyPolicyNegotiationHistoryRepository.java
+++ user-agent/user-feedback/src/main/java/org/societies/useragent/feedback/PrivacyPolicyNegotiationHistoryRepository.java
@@ -3,9 +3,11 @@ package org.societies.useragent.feedback;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.societies.api.internal.schema.useragent.feedback.UserFeedbackPrivacyNegotiationEvent;
+import org.societies.api.schema.useragent.feedback.FeedbackStage;
 import org.societies.useragent.api.feedback.IPrivacyPolicyNegotiationHistoryRepository;
 
 import java.util.Date;
@@ -15,6 +17,7 @@ public class PrivacyPolicyNegotiationHistoryRepository implements IPrivacyPolicy
     private static final Logger log = LoggerFactory.getLogger(PrivacyPolicyNegotiationHistoryRepository.class);
 
     private SessionFactory sessionFactory;
+    private Session session;
 
     public SessionFactory getSessionFactory() {
         return sessionFactory;
@@ -24,10 +27,18 @@ public class PrivacyPolicyNegotiationHistoryRepository implements IPrivacyPolicy
         this.sessionFactory = sessionFactory;
     }
 
+    public void init() {
+        session = sessionFactory.openSession();
+    }
+
+    public void destroy() {
+        session.flush();
+        session.close();
+    }
+
+
     @Override
     public List<UserFeedbackPrivacyNegotiationEvent> listPrevious(int howMany) {
-        Session session = sessionFactory.openSession();
-
         Query query = session.createQuery("FROM UserFeedbackPrivacyNegotiationEvent uf ORDER BY uf.requestDate");
         query.setMaxResults(howMany);
 
@@ -36,11 +47,43 @@ public class PrivacyPolicyNegotiationHistoryRepository implements IPrivacyPolicy
 
     @Override
     public List<UserFeedbackPrivacyNegotiationEvent> listSince(Date sinceWhen) {
-        Session session = sessionFactory.openSession();
-
         Query query = session.createQuery("FROM UserFeedbackPrivacyNegotiationEvent uf WHERE uf.requestDate > :date ORDER BY uf.requestDate");
+        query.setDate("date", sinceWhen);
 
         return query.list();
+    }
+
+    @Override
+    public UserFeedbackPrivacyNegotiationEvent getByRequestId(String requestId) {
+        Query query = session.createQuery("FROM UserFeedbackPrivacyNegotiationEvent uf WHERE uf.requestId = :id");
+        query.setString("id", requestId);
+
+        List results = query.list();
+
+        if (results.size() == 0)
+            return null;
+
+        return (UserFeedbackPrivacyNegotiationEvent) results.get(0);
+    }
+
+    @Override
+    public void insert(UserFeedbackPrivacyNegotiationEvent event) {
+        Transaction transaction = session.beginTransaction();
+
+        session.save(event);
+
+        transaction.commit();
+    }
+
+    @Override
+    public void updateStage(String requestId, FeedbackStage newStage) {
+        Transaction transaction = session.beginTransaction();
+
+        UserFeedbackPrivacyNegotiationEvent item = getByRequestId(requestId);
+        item.setStage(newStage);
+        session.update(item);
+
+        transaction.commit();
     }
 
 }
