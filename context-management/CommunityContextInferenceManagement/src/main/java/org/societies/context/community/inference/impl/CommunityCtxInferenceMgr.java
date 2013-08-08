94daf5cf866143101095417ef1dbb98c9acac0a8
nikosk <nikosk@cn.ntua.gr>
2013-08-08 11:41:36 +0300

    unused imports removed

--- context-management/CommunityContextInferenceManagement/src/main/java/org/societies/context/community/inference/impl/CommunityCtxInferenceMgr.java
+++ context-management/CommunityContextInferenceManagement/src/main/java/org/societies/context/community/inference/impl/CommunityCtxInferenceMgr.java
@@ -29,12 +29,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
-import org.societies.api.context.CtxException;
 import org.societies.api.context.model.CtxAttribute;
 import org.societies.api.context.model.CtxAttributeIdentifier;
 import org.societies.api.context.model.CtxAttributeTypes;
