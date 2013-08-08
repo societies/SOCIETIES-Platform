c06e55bed793d960bc5f3b4b4ad6ac1c3824f24f
Nicolas Liampotis <nicolas.liampotis@cn.ntua.gr>
2013-08-08 16:20:43 +0300

    Update BIT #1962 to remove created test trust data upon completion

--- integration-tests/BIT/privacy-trust-test-suite/local-trust-eventing-1962/src/main/java/org/societies/integration/test/bit/privacytrust/TestByTrustorTrustee.java
+++ integration-tests/BIT/privacy-trust-test-suite/local-trust-eventing-1962/src/main/java/org/societies/integration/test/bit/privacytrust/TestByTrustorTrustee.java
@@ -137,15 +137,25 @@ public class TestByTrustorTrustee {
 	@After
 	public void tearDown() throws Exception {
 		
-		try {
-			this.internalTrustBroker.unregisterTrustUpdateListener(
-					this.listener, new TrustQuery(this.myTeid).setTrusteeId(this.teid1));
-		} catch (TrustException te) {
-			fail("Failed to unregister TrustUpdateEvent listener: "
-					+ te.getLocalizedMessage());
+		if (this.myTeid != null) {
+			if (this.teid1 != null) {
+				this.internalTrustBroker.removeTrustRelationships(
+						new TrustQuery(this.myTeid).setTrusteeId(this.teid1));
+			}
+			if (this.teid2 != null) {
+				this.internalTrustBroker.removeTrustRelationships(
+						new TrustQuery(this.myTeid).setTrusteeId(this.teid2));
+			}
+			if (this.listener != null) {
+				try {
+					this.internalTrustBroker.unregisterTrustUpdateListener(
+							this.listener, new TrustQuery(this.myTeid).setTrusteeId(this.teid1));
+				} catch (TrustException te) {
+					fail("Failed to unregister TrustUpdateEvent listener: "
+							+ te.getLocalizedMessage());
+				}
+			}
 		}
-		// TODO
-		// 1. remove test trust data db? currently not supported
 	}
 	
 	@Test
