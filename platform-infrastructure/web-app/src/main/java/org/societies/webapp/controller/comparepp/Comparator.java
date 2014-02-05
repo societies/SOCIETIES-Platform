e57514ac9b19f9c899953c549b5a486bdf6958d5
Stuart Whyte <sww2@hw.ac.uk>
2014-02-05 15:46:57 +0000

    compare policies updates

--- platform-infrastructure/web-app/src/main/java/org/societies/webapp/controller/comparepp/Comparator.java
+++ platform-infrastructure/web-app/src/main/java/org/societies/webapp/controller/comparepp/Comparator.java
@@ -1,21 +1,46 @@
 package org.societies.webapp.controller.comparepp;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.societies.api.internal.context.model.CtxAttributeTypes;
 import org.societies.api.internal.privacytrust.privacyprotection.model.privacypolicy.AgreementEnvelope;
+import org.societies.api.internal.schema.privacytrust.privacyprotection.preferences.PrivacyOutcomeConstantsBean;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.Condition;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.Decision;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.RequestItem;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.RequestPolicy;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.ResponseItem;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.IPrivacyPreference;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.PrivacyPreference;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.accesscontrol.AccessControlOutcome;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.accesscontrol.AccessControlPreferenceTreeModel;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.constants.PrivacyOutcomeConstants;
+import org.societies.privacytrust.privacyprotection.api.model.privacypreference.dobf.DObfOutcome;
 
 public final class Comparator {
-	
+
 	private static Logger log = LoggerFactory.getLogger(Comparator.class);
-	
+
+	private static String[] sensedDataTypes = new String[]{CtxAttributeTypes.TEMPERATURE, 
+		CtxAttributeTypes.STATUS,
+		CtxAttributeTypes.LOCATION_SYMBOLIC,
+		CtxAttributeTypes.LOCATION_COORDINATES,
+		CtxAttributeTypes.ACTION};
+
+
+	public static boolean isAttributeSensed(String type) {
+		for (String sensedType : sensedDataTypes){
+			if (sensedType.equalsIgnoreCase(type)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static HashMap<RequestItem, RequestItem> comparePolicyToAgreement(RequestPolicy policy, AgreementEnvelope agreement) {
 		HashMap<RequestItem, RequestItem> changes = new HashMap<RequestItem, RequestItem>();
 		for(RequestItem policyItem : policy.getRequestItems()) {
@@ -26,24 +51,48 @@ public final class Comparator {
 						changes.put(policyItem, agreementItem);
 					}
 					if(!checkConditions(agreementItem.getConditions(), policyItem.getConditions())) {						
-						log.debug("Conditions have changed");
 						changes.put(policyItem, agreementItem);
-					} else {
-						log.debug("Conditions are the same!");
-					}
+					} 
 					if(agreementResponse.getDecision().equals(Decision.DENY)) {
-						log.debug("Decision has been changed");
 						changes.put(policyItem, agreementItem);
 					}
 					break;
 				}
-				
+
 			}
-			
+
 		}
 		return changes;
 	}
-	
+
+	public static boolean checkAccessControl(AccessControlPreferenceTreeModel model) {
+		if(null!=model) {
+			IPrivacyPreference p = model.getRootPreference();
+			if(p!=null) {
+				Enumeration d = p.depthFirstEnumeration();
+				while(d.hasMoreElements()) {
+					PrivacyPreference nxt = (PrivacyPreference) d.nextElement();
+					if(nxt.getOutcome() instanceof AccessControlOutcome) {
+						AccessControlOutcome outcome = (AccessControlOutcome) nxt.getOutcome();
+						if(outcome.getEffect().equals(PrivacyOutcomeConstantsBean.BLOCK)) {
+							return true;						
+						} 
+					}
+					if (nxt.getOutcome() instanceof DObfOutcome) {
+						DObfOutcome outcome = (DObfOutcome) nxt.getOutcome();
+						if(outcome.getObfuscationLevel()!=1.0) {
+							return true;
+						}
+
+					}
+				}
+			} else {
+				log.debug("Model is null!");
+			}
+		}
+		return false;
+	}
+
 	private static boolean checkConditions(List<Condition> agreementConditions, List<Condition> policyConditions) {
 		if(agreementConditions.size()!=policyConditions.size()) {
 			return false;
--- platform-infrastructure/web-app/src/main/java/org/societies/webapp/controller/comparepp/ComparePrivacyPrefController.java
+++ platform-infrastructure/web-app/src/main/java/org/societies/webapp/controller/comparepp/ComparePrivacyPrefController.java
@@ -16,13 +16,16 @@ import org.slf4j.LoggerFactory;
 import org.societies.api.cis.management.ICis;
 import org.societies.api.cis.management.ICisManager;
 import org.societies.api.comm.xmpp.interfaces.ICommManager;
+import org.societies.api.identity.IIdentity;
 import org.societies.api.identity.InvalidFormatException;
 import org.societies.api.identity.RequestorCis;
 import org.societies.api.identity.RequestorService;
 import org.societies.api.internal.context.broker.ICtxBroker;
+import org.societies.api.internal.context.model.CtxAttributeTypes;
 import org.societies.api.internal.privacytrust.privacyprotection.IPrivacyAgreementManager;
 import org.societies.api.internal.privacytrust.privacyprotection.IPrivacyPolicyManager;
 import org.societies.api.internal.schema.privacytrust.privacyprotection.preferences.AccessControlPreferenceDetailsBean;
+import org.societies.api.internal.schema.privacytrust.privacyprotection.preferences.PrivacyOutcomeConstantsBean;
 import org.societies.api.internal.servicelifecycle.IServiceDiscovery;
 import org.societies.api.internal.servicelifecycle.ServiceDiscoveryException;
 import org.societies.api.privacytrust.privacy.model.PrivacyException;
@@ -32,6 +35,7 @@ import org.societies.api.schema.identity.RequestorServiceBean;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.RequestItem;
 import org.societies.api.schema.privacytrust.privacy.model.privacypolicy.RequestPolicy;
 import org.societies.api.schema.servicelifecycle.model.Service;
+import org.societies.api.schema.servicelifecycle.model.ServiceResourceIdentifier;
 import org.societies.api.services.IServices;
 import org.societies.personalisation.preference.api.model.IPreference;
 import org.societies.privacytrust.privacyprotection.api.IPrivacyPreferenceManager;
@@ -69,18 +73,18 @@ public class ComparePrivacyPrefController {
 
 	HashMap<ICis, HashMap<RequestItem, RequestItem>> changedCISPP;
 	HashMap<Service, HashMap<RequestItem, RequestItem>> changedServicePP;
-	List<AccessControlPreferenceTreeModel> changedAccCtrl;
+	List<AccessControlPreferenceDetailsBean> changedAccCtrl;
 
 	public ComparePrivacyPrefController() {
 		changedCISPP = new HashMap<ICis, HashMap<RequestItem, RequestItem>> ();
 		changedServicePP = new HashMap<Service, HashMap<RequestItem, RequestItem>> ();
-		changedAccCtrl = new ArrayList<AccessControlPreferenceTreeModel>();
+		changedAccCtrl = new ArrayList<AccessControlPreferenceDetailsBean>();
 	}
 
 	@PostConstruct
 	public void initController() {
 		compareCISPrivacyPolicies();
-		//compareServicePrivacyPolicies();
+		compareServicePrivacyPolicies();
 		compareAccessControl();
 
 		/*	try {
@@ -127,45 +131,59 @@ public class ComparePrivacyPrefController {
 	public void compareAccessControl() {
 		List<AccessControlPreferenceDetailsBean> accessControlList = this.privacyPrefMgr.getAccCtrlPreferenceDetails();
 		for(AccessControlPreferenceDetailsBean bean : accessControlList) {
-			AccessControlPreferenceTreeModel model = this.privacyPrefMgr.getAccCtrlPreference(bean);
-			if(null!=model) {
-				IPrivacyPreference p = model.getRootPreference();
-				if(p.getOutcome()!=null) {
-					log.debug(p.getOutcome().getOutcomeType().toString());
+			if(Comparator.isAttributeSensed(bean.getResource().getDataType())) {
+				AccessControlPreferenceTreeModel model = this.privacyPrefMgr.getAccCtrlPreference(bean);
+				if(Comparator.checkAccessControl(model)) {
+					log.debug("Adding a model to changed ACC!");
+					log.debug(bean.toString());
+					changedAccCtrl.add(bean);
+
 				}
-				Enumeration d = p.depthFirstEnumeration();
-				while(d.hasMoreElements()) {
-					PrivacyPreference nxt = (PrivacyPreference) d.nextElement();
-
-					if(nxt.getOutcome() instanceof AccessControlOutcome) {
-						log.debug("Outcome is of AccessControlOutome");
-						AccessControlOutcome outcome = (AccessControlOutcome) nxt.getOutcome();
-
-						log.debug("Outcome effect is: " + outcome.getEffect());
-						if(outcome.getEffect().equals(PrivacyOutcomeConstants.BLOCK.toString())) {
-							changedAccCtrl.add(model);			
-							log.debug("User has denied access! Adding to list!");
-							log.debug(bean.getResource().toString());
-							
-							break;
-						} 
-					}
-					if (nxt.getOutcome() instanceof DObfOutcome) {
-						log.debug("Access is of DObfOutcome");
-						DObfOutcome outcome = (DObfOutcome) nxt.getOutcome();
-						if(outcome.getObfuscationLevel()!=1.0) {
-							changedAccCtrl.add(model);
-							log.debug("Adding to list - obfsucation level is not at default!");
-							break;
-						}
-
-					}
-				}
-			} else {
-				log.debug("Model is null!");
 			}
-
 		}
+	}
+
+	public String getJidRequestor(AccessControlPreferenceDetailsBean bean) {
+		if(bean.getRequestor() instanceof RequestorCisBean) {
+			RequestorCisBean cBean = (RequestorCisBean) bean.getRequestor();
+			return cBean.getRequestorId();
+		}
+		if(bean.getRequestor() instanceof RequestorServiceBean) {
+			RequestorServiceBean sBean = (RequestorServiceBean) bean.getRequestor();
+			return sBean.getRequestorId();
+		}
+		return "";
+	}
+
+	public String getCisServiceRequestor(AccessControlPreferenceDetailsBean bean) {
+		if(bean.getRequestor() instanceof RequestorCisBean) {
+			RequestorCisBean cBean = (RequestorCisBean) bean.getRequestor();
+			return cBean.getCisRequestorId();
+		}
+		if(bean.getRequestor() instanceof RequestorServiceBean) {
+			RequestorServiceBean sBean = (RequestorServiceBean) bean.getRequestor();
+			return sBean.getRequestorServiceId().getServiceInstanceIdentifier();
+		}
+		return "";
+	}
+
+	public String getObfOrBlock(AccessControlPreferenceDetailsBean bean) {
+		AccessControlPreferenceTreeModel model = this.privacyPrefMgr.getAccCtrlPreference(bean);
+		IPrivacyPreference p = model.getRootPreference();
+		if(p!=null) {
+			Enumeration d = p.depthFirstEnumeration();
+			while(d.hasMoreElements()) {
+				PrivacyPreference nxt = (PrivacyPreference) d.nextElement();
+				if(nxt.getOutcome() instanceof AccessControlOutcome) {
+					return "BLOCK";
+				}
+				if(nxt.getOutcome() instanceof DObfOutcome) {
+					DObfOutcome outcome = (DObfOutcome) model.getRootPreference().getOutcome();
+					return String.valueOf(outcome.getObfuscationLevel());
+				}
+			}
+		}
+		return model.toString();
 	}
 
 	public void compareServicePrivacyPolicies() {
@@ -184,25 +202,33 @@ public class ComparePrivacyPrefController {
 		}
 		for(Service service : services) {
 			RequestorServiceBean bean = new RequestorServiceBean();
-			bean.setRequestorServiceId(service.getServiceIdentifier());
-			bean.setRequestorId(commManager.getIdManager().getThisNetworkNode().getBareJid());
+			ServiceResourceIdentifier serviceServerID = serviceMgmt.getServerServiceIdentifier(service.getServiceIdentifier());
+			IIdentity serverHostJID = serviceMgmt.getServer(serviceServerID);
+			log.debug(serverHostJID.getBareJid());
+			//WHEN DOING BELOW, REQUESTOR CIS IS OBV CIS ID
+			bean.setRequestorServiceId(serviceServerID);
+			//THIS IS THE CURRENT USER (NOT OWNER OF CIS) BUT IN CASE OF SERVICES, IT IS USER WITH SERVICE
+			//SERVER
+			bean.setRequestorId(serverHostJID.getBareJid());
 			try {
 				RequestPolicy p = this.privacyPolicyManager.getPrivacyPolicy(bean);
 				if(p!=null) {
-					RequestorService r = new RequestorService(this.commManager.getIdManager().getThisNetworkNode(), service.getServiceIdentifier());
+					//USING JANE's ID TO GET SERVICE AGREEMENT
+					RequestorService r = new RequestorService(serverHostJID, serviceServerID);
 					org.societies.api.internal.privacytrust.privacyprotection.model.privacypolicy.AgreementEnvelope a = this.privacyAgreementManager.getAgreement(r);
 					if(a!=null) {
 						log.debug("Policy Agree for sservice is not null!");
+						HashMap<RequestItem, RequestItem> changedArticles = Comparator.comparePolicyToAgreement(p, a);
+						if(changedArticles.size()==0) {
+							log.debug("The user did not change the policy!");
+						} else {
+							changedServicePP.put(service, changedArticles);
+							log.debug("The user has changed the policy!");
+						}
 					} else {
 						log.debug("Agreement is null!");
 					}
-					HashMap<RequestItem, RequestItem> changedArticles = Comparator.comparePolicyToAgreement(p, a);
-					if(changedArticles.size()==0) {
-						log.debug("The user did not change the policy!");
-					} else {
-						changedServicePP.put(service, changedArticles);
-						log.debug("The user has changed the policy!");
-					}
+					
 				}
 			} catch (PrivacyException e) {
 				// TODO Auto-generated catch block
@@ -222,6 +248,7 @@ public class ComparePrivacyPrefController {
 				RequestPolicy p = this.privacyPolicyManager.getPrivacyPolicy(bean);
 				RequestorCis c = null;
 				try {
+					//REQUESTOR CIS TO GET AGREEMENT USING MATTS ID
 					c = new RequestorCis(this.commManager.getIdManager().getThisNetworkNode(), this.commManager.getIdManager().fromJid(cis.getCisId()));
 				} catch (InvalidFormatException e) {
 					// TODO Auto-generated catch block
@@ -341,6 +368,15 @@ public class ComparePrivacyPrefController {
 
 	public void setPrivacyPrefMgr(IPrivacyPreferenceManager privacyPrefMgr) {
 		this.privacyPrefMgr = privacyPrefMgr;
+	}
+
+	public List<AccessControlPreferenceDetailsBean> getChangedAccCtrl() {
+		return changedAccCtrl;
+	}
+
+	public void setChangedAccCtrl(
+			List<AccessControlPreferenceDetailsBean> changedAccCtrl) {
+		this.changedAccCtrl = changedAccCtrl;
 	}
 
 
